73b7d32e4b07a3b0563a3682e26b3c24470ff589
Morgan Heggland <morganheggland@gmail.com>
2020-08-09 16:01:25 -0400

    add more basic front-end structure

---
+++ front-end/src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import { AppProps } from "next/app";
+import "../styles/global.css";
+
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+    return <Component {...pageProps} />;
+};
+
+export default App;
