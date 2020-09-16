176dfa5ff349b651959e299fb567f9118f29f0db
Morgan Heggland <morganheggland@gmail.com>
2020-09-16 16:06:57 +0200

    make it possible to create events in frontend

--- frontend/src/pages/events.tsx
+++ frontend/src/pages/events.tsx
@@ -1,8 +1,7 @@
-import { gql } from "apollo-boost";
 import { NextPage } from "next";
 import Link from "next/link";
 import React from "react";
-import { useQuery } from "react-apollo";
+import { gql, useMutation, useQuery } from "@apollo/client";
 
 const EventInfo: NextPage = () => {
     const QUERY_EVENT = gql`
@@ -14,41 +13,101 @@ const EventInfo: NextPage = () => {
         }
     `;
 
+    const CREATE_EVENT = gql`
+        mutation CreateEvent($title: String, $description: String, $starttime: DateTime) {
+            createEvent(title: $title, description: $description, starttime: $starttime) {
+                ok
+                event {
+                    id
+                    title
+                    description
+                    starttime
+                }
+            }
+        }
+    `;
+
     const QUERY_ALL_EVENTS = gql`
         query {
             allEvents {
                 id
                 title
+                description
+                starttime
             }
         }
     `;
 
-    const { data, loading } = useQuery(QUERY_ALL_EVENTS, {
-        pollInterval: 30000, // refetch the result every 30 second
-    });
+    const AllEvents = () => {
+        const { loading, error, data } = useQuery(QUERY_ALL_EVENTS, {
+            pollInterval: 30000, // refetch the result every 30 second
+        });
+        // should handle loading status
+        if (loading) return <p>Loading...</p>;
 
-    // should handle loading status
-    if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
 
-    if (data.event)
-        return (
-            <div>
+        return data.allEvents.map((event) => (
+            <div key={event.id}>
                 <p>
-                    {data.event.id}: {data.event.title}
+                    {event.id}: {event.title} - {event.starttime.slice(0, 19).replace("T", " ")} - {event.description}
                 </p>
             </div>
+        ));
+    };
+
+    const CreateEvent = () => {
+        let title: HTMLInputElement;
+        let description: HTMLInputElement;
+        let startTime: HTMLInputElement;
+        const [createEvent, { data }] = useMutation(CREATE_EVENT); //ideally we use this data to update the event list
+
+        return (
+            <div>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        createEvent({ variables: { title: title.value, description: description.value } });
+                        title.value = "";
+                        description.value = "";
+                    }}
+                >
+                    <div>
+                        <input
+                            placeholder="Title"
+                            ref={(node) => {
+                                title = node;
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            placeholder="Description"
+                            ref={(node) => {
+                                description = node;
+                            }}
+                        />
+                    </div>
+                    <div>
+                        <input
+                            type="datetime-local"
+                            placeholder="Start time"
+                            ref={(node) => {
+                                startTime = node;
+                            }}
+                        />
+                    </div>
+                    <button type="submit">Create Event</button>
+                </form>
+            </div>
         );
+    };
 
     return (
         <div>
             <Link href="/"> Go Home </Link>
-            {data.allEvents.map((event) => (
-                <div key={event.id}>
-                    <p>
-                        {event.id}: {event.title}
-                    </p>
-                </div>
-            ))}
+            <CreateEvent />
+            <AllEvents />
         </div>
     );
 };
