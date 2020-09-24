50613a43777100259df13611284089469984c1ab
Morgan Heggland <morganheggland@gmail.com>
2020-09-24 21:54:54 +0200

    run black

--- backend/apps/events/migrations/0001_initial.py
+++ backend/apps/events/migrations/0001_initial.py
@@ -7,17 +7,24 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Event',
+            name="Event",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=128)),
-                ('starttime', models.DateTimeField()),
-                ('description', models.TextField()),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=128)),
+                ("starttime", models.DateTimeField()),
+                ("description", models.TextField()),
             ],
         ),
     ]
