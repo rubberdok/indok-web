1a61a95740ee32c693211cd6c2113df3c33ee889
Herman Holmøy <herman.holmoy12@gmail.com>
2020-09-24 20:39:28 +0200

    create schema for cabin booking and mutation

---
+++ backend/cabin/schema.py
@@ -0,0 +1,26 @@
+import graphene
+
+from graphene import Argument
+from graphene_django import DjangoObjectType
+from datetime import datetime
+from django.shortcuts import get_object_or_404
+
+from .models import Cabin as CabinModel
+from .models import Booking as BookingModel
+
+
+class BookingType(DjangoObjectType):
+    class Meta:
+        model = BookingModel
+
+
+class CabinType(DjangoObjectType):
+    class Meta:
+        model = CabinModel
+
+
+class BookingQueries(graphene.ObjectType):
+    all_bookings = graphene.List(BookingType)
+
+    def resolve_all_bookings(root, info):
+        return BookingModel.objects.all()
