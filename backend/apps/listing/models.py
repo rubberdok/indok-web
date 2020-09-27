d17c343916ae36b3c11b656f146d39802ba5b90b
Lars Waage <46653859+larwaa@users.noreply.github.com>
2020-09-27 23:14:34 +0200

    :boom: feat: revert listing to numerical pk, add slug field

--- backend/apps/listing/models.py
+++ backend/apps/listing/models.py
@@ -4,14 +4,14 @@ import uuid
 
 class Listing(models.Model):
     description = models.CharField(max_length=2000)
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=250)
+
 
     start_date_time = models.DateTimeField()
     end_date_time = models.DateTimeField()
 
     url = models.URLField()
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    
+
     def __str__(self):
         return f"{self.title} (Open: {self.start_date_time} - {self.end_date_time}: {self.description}"
 
