daec939c4cc0979c0ea18d41a8b5eb1f0e5354ca
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-09-28 18:02:01 +0200

    started work on listing response frontend functionality, renamed application to listing response

--- frontend/src/components/application.tsx
+++ frontend/src/components/application.tsx
@@ -1,11 +1,7 @@
 import { NextPage } from "next";
 import Link from "next/link";
-import TextField from "./application_components/textfield"
+import TextField from "./listing_response_components/textfield";
 
-const Application = ({ children }: { children: React.ReactNode}) => (
-    <>
-        {children}
-    </>
-);
+const Application = ({ children }: { children: React.ReactNode }) => <>{children}</>;
 
 export default Application;
--- frontend/src/pages/applicationpage.tsx
+++ frontend/src/pages/applicationpage.tsx
@@ -1,25 +1,27 @@
 import { NextPage } from "next";
 import Link from "next/link";
 import Layout from "../components/Layout";
-import Application from "../components/application"
-import TextField from "../components/application_components/textfield"
-import Choice from "../components/application_components/choice"
-import Dropdown from "../components/application_components/dropdown"
-import Slider from "../components/application_components/slider"
+import Application from "../components/application";
+import TextField from "../components/listing_response_components/textfield";
+import Choice from "../components/listing_response_components/choice";
+import Dropdown from "../components/listing_response_components/dropdown";
+import Slider from "../components/listing_response_components/slider";
 
 const ApplicationPage: NextPage = () => (
     <Layout>
-        <Link href="/"><a>Back to index</a></Link>
+        <Link href="/">
+            <a>Back to index</a>
+        </Link>
         <Application>
-            <TextField title="Test input" placeholder="Type text here"/>
-            <br/>
-            <Choice title="Test radio buttons" radio options={["test1", "test2"]}/>
-            <br/>
-            <Choice title="Test checkbox" options={["test3", "test4"]}/>
-            <br/>
-            <Dropdown title="Test dropdown" options={["test5", "test6", "test7"]}/>
-            <br/>
-            <Slider title="Test slider" range={[0, 50]} placeholder={10}/>
+            <TextField title="Test input" placeholder="Type text here" />
+            <br />
+            <Choice title="Test radio buttons" radio options={["test1", "test2"]} />
+            <br />
+            <Choice title="Test checkbox" options={["test3", "test4"]} />
+            <br />
+            <Dropdown title="Test dropdown" options={["test5", "test6", "test7"]} />
+            <br />
+            <Slider title="Test slider" range={[0, 50]} placeholder={10} />
         </Application>
     </Layout>
 );
