9a1786c151b3195d114cb2c1ebd39357697585ae
Herman Holmøy <herman.holmoy12@gmail.com>
2020-09-28 16:04:10 +0200

    add new app cabins

---
+++ backend/cabins/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from cabins.models import Cabin, Booking
+
+# Register your models here.
+
+admin.site.register(Cabin)
+admin.site.register(Booking)
---
+++ backend/cabins/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Cabin(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Booking(models.Model):
+    contact_num = models.IntegerField()
+    contact_person = models.CharField(max_length=100)
+    start_day = models.DateField()  # python datetime.date
+    end_day = models.DateField()
+
+    def __str__(self):
+        return f"Booking {self.id}, {self.contact_person}"
