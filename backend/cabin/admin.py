7b410ed99ccdbf866210ed9814fa397a07defc62
Herman Holmøy <herman.holmoy12@gmail.com>
2020-09-28 16:06:14 +0200

    remove cabin app, changed to cabins

--- backend/cabin/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from cabin.models import Cabin, Booking
-
-# Register your models here.
-
-admin.site.register(Cabin)
-admin.site.register(Booking)
--- backend/cabin/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CabinConfig(AppConfig):
-    name = 'cabin'
