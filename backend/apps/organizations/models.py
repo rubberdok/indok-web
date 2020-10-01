99f3f1c33b79107f2ea32762ec4cfdc133a82ec3
Lars Waage <46653859+larwaa@users.noreply.github.com>
2020-10-01 18:07:37 +0200

    :construction: dev: remove unique constraint on name

--- backend/apps/organizations/models.py
+++ backend/apps/organizations/models.py
@@ -1,14 +1,18 @@
 from django.db import models
+from django.db.models import UniqueConstraint
 
 # Create your models here.
 class Organization(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100)
     description = models.CharField(max_length=4000, blank=True)
 
     parent = models.ForeignKey('Organization', on_delete=models.CASCADE, null=True)
 
     logo = models.ImageField(upload_to='organizations', null=True)
+
+    class Meta:
+        UniqueConstraint(fields=['parent', 'name'], name='unique_child_organization_name')
 
     def __str__(self):
         return f"{self.name}"
