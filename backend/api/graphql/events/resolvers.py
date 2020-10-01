06cdda5c006bb0157e3dfdf6aa214dee79e4a231
Sverre Spetalen <sverress@stud.ntnu.no>
2020-10-01 17:18:08 +0200

    la til genial løsning

--- backend/api/graphql/events/resolvers.py
+++ backend/api/graphql/events/resolvers.py
@@ -1,13 +1,12 @@
-import graphene
 from apps.events.models import Event
 
 
-def resolve_all_events(info):
-    return Event.objects.all()
+class EventResolvers:
+    def resolve_all_events(self, info):
+        return Event.objects.all()
 
-
-def resolve_event(info, id):
-    try:
-        return Event.objects.get(id=id)
-    except Event.DoesNotExist:
-        return None
+    def resolve_event(self, info, id):
+        try:
+            return Event.objects.get(id=id)
+        except Event.DoesNotExist:
+            return None
