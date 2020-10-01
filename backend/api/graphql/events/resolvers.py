bc71348561f37a4b757a90fff1303f3959838cbf
Sverre Spetalen <sverress@stud.ntnu.no>
2020-10-01 17:27:08 +0200

    change name of resolver parameter

--- backend/api/graphql/events/resolvers.py
+++ backend/api/graphql/events/resolvers.py
@@ -2,10 +2,10 @@ from apps.events.models import Event
 
 
 class EventResolvers:
-    def resolve_all_events(self, info):
+    def resolve_all_events(parent, info):
         return Event.objects.all()
 
-    def resolve_event(self, info, id):
+    def resolve_event(parent, info, id):
         try:
             return Event.objects.get(id=id)
         except Event.DoesNotExist:
