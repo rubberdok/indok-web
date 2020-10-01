4532b5f807686798cc724839026e02065f47afb9
Hermann Morkrid <hermann.morkrid@gmail.com>
2020-10-01 18:28:48 +0200

    changed file structure for listing components

--- frontend/src/pages/applicationpage.tsx
+++ frontend/src/pages/applicationpage.tsx
@@ -1,19 +1,19 @@
 import { NextPage } from "next";
 import Link from "next/link";
 import Layout from "../components/Layout";
-import Application from "../components/application";
-import TextField from "../components/listing_response_components/textfield";
-import Choice from "../components/listing_response_components/choice";
-import Dropdown from "../components/listing_response_components/dropdown";
-import Slider from "../components/listing_response_components/slider";
+import ListingResponse from "../components/ListingResponse";
+import TextField from "../components/listing_components/textfield";
+import Choice from "../components/listing_components/choice";
+import Dropdown from "../components/listing_components/dropdown";
+import Slider from "../components/listing_components/slider";
 
 const ApplicationPage: NextPage = () => (
     <Layout>
         <Link href="/">
             <a>Back to index</a>
         </Link>
-        <Application>
-            <TextField title="Test input" placeholder="Type text here" />
+        <ListingResponse>
+            <TextField title="Test input" placeholder="Type text here" size="short" />
             <br />
             <Choice title="Test radio buttons" radio options={["test1", "test2"]} />
             <br />
@@ -22,7 +22,7 @@ const ApplicationPage: NextPage = () => (
             <Dropdown title="Test dropdown" options={["test5", "test6", "test7"]} />
             <br />
             <Slider title="Test slider" range={[0, 50]} placeholder={10} />
-        </Application>
+        </ListingResponse>
     </Layout>
 );
 
