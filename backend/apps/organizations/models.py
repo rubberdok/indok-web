5d392355aca9ff77f1516501a404eb042d86ab5d
Lars Waage <46653859+larwaa@users.noreply.github.com>
2020-10-01 17:31:02 +0200

    :sparkles: feat: add description, logo, and parent field to organizations

--- backend/apps/organizations/models.py
+++ backend/apps/organizations/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class Organization(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100)
+    description = models.CharField(max_length=4000, blank=True)
+
+    parent = models.ForeignKey('Organization', on_delete=models.CASCADE, null=True)
+
+    logo = models.ImageField(upload_to='organizations', null=True)
 
     def __str__(self):
         return f"{self.name}"
