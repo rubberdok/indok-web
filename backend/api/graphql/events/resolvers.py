01b111824493d9fad10b8132fde829346ddc376c
mathildesolberg <mathilde.solberg@hotmail.com>
2020-10-05 16:08:54 +0200

    fixed merge conflicts

--- backend/api/graphql/events/resolvers.py
+++ backend/api/graphql/events/resolvers.py
@@ -5,29 +5,18 @@ class EventResolvers:
     def resolve_all_events(parent, info):
         return Event.objects.all()
 
-<<<<<<< HEAD
-    def resolve_event(info, id):
-=======
     def resolve_event(parent, info, id):
->>>>>>> 94a756da0bddd3b620e0c64e685e4595b646076d
         try:
             return Event.objects.get(id=id)
         except Event.DoesNotExist:
             return None
 
-<<<<<<< HEAD
-    def resolve_all_categories(info):
-        return Category.objects.all()
-
-    def resolve_category(info, id):
-=======
 
 class CategoryResolvers:
     def resolve_all_categories(parent, info):
         return Category.objects.all()
 
     def resolve_category(parent, info, id):
->>>>>>> 94a756da0bddd3b620e0c64e685e4595b646076d
         try:
             return Category.objects.get(id=id)
         except Category.DoesNotExist:
--- backend/api/graphql/events/schema.py
+++ backend/api/graphql/events/schema.py
@@ -2,7 +2,6 @@ import graphene
 
 from .mutations import CreateCategory, DeleteCategory, UpdateCategory
 from .mutations import CreateEvent, DeleteEvent, UpdateEvent
-<<<<<<< HEAD
 from .resolvers import (
     resolve_all_categories,
     resolve_all_events,
@@ -10,12 +9,7 @@ from .resolvers import (
     resolve_event,
 )
 from .types import CategoryType, EventType
-from .resolvers import EventResolvers
-=======
-from .types import CategoryType, EventType
 from .resolvers import EventResolvers, CategoryResolvers
->>>>>>> 94a756da0bddd3b620e0c64e685e4595b646076d
-from .types import EventType
 
 
 class EventMutations(graphene.ObjectType):
@@ -27,15 +21,12 @@ class EventMutations(graphene.ObjectType):
 class EventQueries(graphene.ObjectType, EventResolvers):
     all_events = graphene.List(EventType)
     event = graphene.Field(EventType, id=graphene.ID(required=True))
-<<<<<<< HEAD
 
     def resolve_all_events(self, info):
         return resolve_all_events(info)
 
     def resolve_event(self, info, id):
         return resolve_event(info, id)
-=======
->>>>>>> 94a756da0bddd3b620e0c64e685e4595b646076d
 
 
 class CategoryMutations(graphene.ObjectType):
@@ -47,12 +38,9 @@ class CategoryMutations(graphene.ObjectType):
 class CategoryQueries(graphene.ObjectType, CategoryResolvers):
     all_categories = graphene.List(CategoryType)
     category = graphene.Field(CategoryType, id=graphene.ID(required=True))
-<<<<<<< HEAD
 
     def resolve_all_categories(self, info):
         return resolve_all_categories(info)
 
     def resolve_category(self, info, id):
         return resolve_category(info, id)
-=======
->>>>>>> 94a756da0bddd3b620e0c64e685e4595b646076d
