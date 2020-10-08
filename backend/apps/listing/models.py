6f89de1f6a626125426cac21a6e1857cad72beb4
Lars Waage <46653859+larwaa@users.noreply.github.com>
2020-10-08 17:52:06 +0200

    :recycle: refactor: make listing responses a model instead of an app, add migrations

--- backend/apps/listing/models.py
+++ backend/apps/listing/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.db.models import UniqueConstraint
+
 from apps.organizations.models import Organization
+from apps.users.models import User
 # Create your models here.
 
 class Listing(models.Model):
@@ -21,3 +24,10 @@ class Listing(models.Model):
     def __repl__(self):
         return self.__str__()
 
+class Response(models.Model):
+    applicant = models.ForeignKey(User, blank=False, null=False, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, blank=False, null=False, on_delete=models.CASCADE)
+    response = models.CharField(max_length=5000)
+
+    class Meta:
+        UniqueConstraint(fields=['applicant', 'listing'], name="unique_listing_response_for_listing")
