6fcdf71a0bde3ad221706d36587544de8b1319c4
Lars Waage <46653859+larwaa@users.noreply.github.com>
2020-10-10 12:28:33 +0200

    :recycle: refactor: move resolvers to resolvers.py, add children field to organization type

---
+++ backend/api/graphql/organizations/resolvers.py
@@ -0,0 +1,18 @@
+from apps.organizations.models import Organization
+from django.db.models import Q
+
+
+class OrganizationResolvers:
+    def resolve_all_organizations(root, info, search=None):
+        if search:
+            filter = (
+                Q(name__icontains=search)
+            )
+            return Organization.objects.filter(filter)
+        return Organization.objects.all()
+
+    def resolve_organization(root, info, id):
+        try:
+            return Organization.objects.get(pk=id)
+        except Organization.DoesNotExist:
+            return None
