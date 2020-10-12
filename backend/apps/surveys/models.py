82795fac2a5b5416182ef7f30122fba1e38faa5e
Lars Waage <46653859+larwaa@users.noreply.github.com>
2020-10-12 17:09:27 +0200

    :construction: wip: add through model to many-to-many fields

--- backend/apps/surveys/models.py
+++ backend/apps/surveys/models.py
@@ -8,17 +8,21 @@ class Survey(models.Model):
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE, null=True)
     descriptive_name = models.CharField(max_length=100)
     description = models.CharField(max_length=3000, blank=True, default="")
-    question = models.ManyToManyField("Question", related_name="surveys")
+    question = models.ManyToManyField("Question", through="SurveyQuestion", related_name="surveys")
 
     def __str__(self):
         return self.descriptive_name
+
+class SurveyQuestion(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    question = models.ForeignKey("Question", on_delete=models.CASCADE)
+    offered_answer = models.ManyToManyField("OfferedAnswer", related_name="surveyquestions")
+    question_type = models.ForeignKey("QuestionType", on_delete=models.SET_NULL, null=True)
 
 class Question(models.Model):
     question = models.CharField(max_length=300)
     description = models.CharField(max_length=1000, blank=True, default="")
     slug = models.CharField(max_length=300, blank=True, default="")
-    offered_answer = models.ManyToManyField("OfferedAnswer", related_name="questions")
-    question_type = models.ForeignKey("QuestionType", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"Question: {self.question}"
