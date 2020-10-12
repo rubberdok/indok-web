e109560952d5c18ed5d34fbe0dc230d21e251224
Patrik Kjærran <patrikkja@gmail.com>
2020-10-12 16:27:12 +0200

    Update event detail page.

--- frontend/src/components/pages/events/eventDetailPage.tsx
+++ frontend/src/components/pages/events/eventDetailPage.tsx
@@ -5,6 +5,14 @@ interface Props {
     eventId: number;
 }
 
+function parseDate(date: string) {
+    return date != null ? date.replace("T", " ").split("+")[0] : "null";
+}
+
+function getName(obj: any) {
+    return obj != null ? obj.name : "null";
+}
+
 const EventDetailPage: React.FC<Props> = ({ eventId }) => {
     const { loading, error, data } = useQuery(GET_EVENT, {
         pollInterval: 30000, // refetch the result every 30 seconds
@@ -18,11 +26,20 @@ const EventDetailPage: React.FC<Props> = ({ eventId }) => {
     if (data.event)
         return (
             <div>
+                <h3>Event: {data.event.title}</h3>
+
+                <p>Id: {data.event.id}</p>
                 <p>Title: {data.event.title}</p>
-                <div>
-                    <p>Time: {data.event.starttime.slice(0, 19).replace("T", " ")}</p>
-                </div>
+                <p>Starttime: {parseDate(data.event.starttime)}</p>
+                <p>Endtime: {parseDate(data.event.endtime)}</p>
+                <p>Location: {data.event.location}</p>
                 <p>Description: {data.event.description}</p>
+                <p>Organization: {getName(data.event.organization)}</p>
+                <p>Category: {getName(data.event.category)}</p>
+                <p>Image URL: {data.event.image}</p>
+                <p>Is attendable: {data.event.isAttendable}</p>
+                <p>Deadline: {parseDate(data.event.deadline)}</p>
+                <p>Publisher: {data.event.publisher}</p>
             </div>
         );
     else return null;
--- frontend/src/interfaces/events.ts
+++ frontend/src/interfaces/events.ts
@@ -1,6 +1,19 @@
 export interface Event {
     id: string;
     title: string;
-    description: string;
     starttime: string;
+    endtime: string;
+    location: string;
+    description: string;
+    organization: { id: string; name: string };
+    category: { id: string; name: string };
+    image: string;
+    isAttendable: string;
+    deadline: string;
+    publisher: string;
+}
+
+export interface Category {
+    id: string;
+    name: string;
 }
