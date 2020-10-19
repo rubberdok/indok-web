92a13d768fb17f172b236f5ecbef8f5f847742ed
Sverre Spetalen <sverress@stud.ntnu.no>
2020-10-19 17:39:15 +0200

    moved out constants

--- frontend/src/components/Calendar/index.tsx
+++ frontend/src/components/Calendar/index.tsx
@@ -14,10 +14,11 @@ import {
     TwoCalendarsContainer,
 } from "./styles";
 import _ from "lodash";
-import { getDateRange, getCurrentDate } from "./helpers";
+import { getDateRange } from "./helpers";
+import { NORWEGIAN_SHORT_DAY_NAMES, DATE_FORMAT } from "./constants";
 
 moment.updateLocale("nb", {
-    weekdaysShort: ["søn", "man", "tir", "ons", "tor", "fre", "lør"],
+    weekdaysShort: NORWEGIAN_SHORT_DAY_NAMES,
 });
 
 export interface CalendarEvent {
@@ -43,14 +44,14 @@ const Calendar = ({ events, rangeChanged }: CalendarProps) => {
         for (let i = 1; i <= month.daysInMonth(); i++) {
             const date = isCurrentMonth ? moment(selectedMonth) : moment(selectedMonth).clone().add(1, "month");
             date.set("date", i);
-            const dateEvents = events.filter((event) => event.date === date.format("YYYY-MM-DD"));
+            const dateEvents = events.filter((event) => event.date === date.format(DATE_FORMAT));
             daysOfMonth.push(
                 <DayCell
                     onMouseOver={() => (isRangeFreezed ? null : setRange(getDateRange(selectedDay, date)))}
-                    isInRange={_.includes(range, date.format("YYYY-MM-DD"))}
+                    isInRange={_.includes(range, date.format(DATE_FORMAT))}
                     isSelected={date.isSame(selectedDay, "day")}
                     onClick={() => handleDateClicked(date)}
-                    key={date.format("YYYY-MM-DD")}
+                    key={date.format(DATE_FORMAT)}
                 >
                     <Day>{i}</Day>
                     <EventMarkerWrapper>
@@ -82,7 +83,7 @@ const Calendar = ({ events, rangeChanged }: CalendarProps) => {
                         isSelected={date.isSame(selectedDay, "day")}
                         onClick={() => handleDateClicked(date)}
                         outOfRange={true}
-                        key={isCurrentMonth ? date.format("YYYY-MM-DD") : `next-${date.format("YYYY-MM-DD")}`}
+                        key={isCurrentMonth ? date.format(DATE_FORMAT) : `next-${date.format(DATE_FORMAT)}`}
                     >
                         <Day>{date.format("D")}</Day>
                     </DayCell>
@@ -112,7 +113,7 @@ const Calendar = ({ events, rangeChanged }: CalendarProps) => {
                         isSelected={date.isSame(selectedDay, "day")}
                         onClick={() => handleDateClicked(date)}
                         outOfRange={true}
-                        key={`${date.format("YYYY-MM-DD")}`}
+                        key={`${date.format(DATE_FORMAT)}`}
                     >
                         <Day>{date.format("D")}</Day>
                     </DayCell>
@@ -225,5 +226,5 @@ const Calendar = ({ events, rangeChanged }: CalendarProps) => {
 };
 
 export const createDateRange = (fromDate: string, toDate: string) => getDateRange(moment(fromDate), moment(toDate));
-export const currentDate = getCurrentDate;
+export const CONSTANTS = { NORWEGIAN_SHORT_DAY_NAMES, DATE_FORMAT };
 export default Calendar;
