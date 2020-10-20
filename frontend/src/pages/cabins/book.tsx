d8b89fe94468c7156fc1b22b6cd6b89ec195cb46
Herman Holmøy <herman.holmoy12@gmail.com>
2020-10-20 21:33:01 +0200

    use useBookingRange

--- frontend/src/pages/cabins/book.tsx
+++ frontend/src/pages/cabins/book.tsx
@@ -4,12 +4,13 @@ import { CREATE_BOOKING } from "../../graphql/cabins/mutations";
 import Link from "next/link";
 import Navbar from "@components/navbar/Navbar";
 import Button from "@components/ui/Button";
-import React from "react";
+import React, { useState } from "react";
 import Input from "@components/ui/Input";
 import { SEND_EMAIL } from "@graphql/cabins/mutations";
-import { Heading } from "@components/ui/Typography";
+import { Heading, Paragraph } from "@components/ui/Typography";
 import Summary from "../../components/pages/cabins/Summary";
 import { Card } from "../../components/pages/cabins/Card";
+import useBookingRange from "../../hooks/cabins/useBookingRange";
 
 const BookPage = () => {
     const firstnameRef = React.createRef<HTMLInputElement>();
@@ -17,21 +18,25 @@ const BookPage = () => {
     const emailRef = React.createRef<HTMLInputElement>();
     const phoneRef = React.createRef<HTMLInputElement>();
 
-    const [createBooking] = useMutation(CREATE_BOOKING);
-    const [sendEmail] = useMutation(SEND_EMAIL);
-
     const router = useRouter();
     const data = router.query;
     let fromDate = data.fromDate as string;
     let toDate = data.toDate as string;
 
+    const [createBooking] = useMutation(CREATE_BOOKING);
+    const [sendEmail] = useMutation(SEND_EMAIL);
+    const [errorMessage, setErrorMessage] = useState("");
+    const { isAvailable } = useBookingRange(fromDate, toDate);
+
     const handleSubmit = (e: React.FormEvent<EventTarget>) => {
-        // todo: run checks to see if dates are occupied or not.
         e.preventDefault();
 
         // must parse dates to be "YYYY-MM-DD", not "DD/MM/YYYY"
         fromDate = fromDate.split("/").reverse().join("-");
         toDate = toDate.split("/").reverse().join("-");
+
+        console.log("Klikk");
+        console.log(isAvailable);
 
         // avoid refs being null
         if (firstnameRef.current && surnameRef.current && emailRef.current && phoneRef.current) {
@@ -41,26 +46,32 @@ const BookPage = () => {
             const phone = phoneRef.current.value;
 
             // create booking and send email
-            createBooking({
-                variables: {
-                    contactNum: parseInt(phone),
-                    contactPerson: firstname + " " + surname,
-                    startDay: fromDate,
-                    endDay: toDate,
-                },
-            });
 
-            sendEmail({
-                variables: {
-                    firstname: firstname,
-                    surname: surname,
-                    receiverEmail: email,
-                    bookFrom: fromDate,
-                    bookTo: toDate,
-                },
-            });
+            if (isAvailable) {
+                createBooking({
+                    variables: {
+                        contactNum: parseInt(phone),
+                        contactPerson: firstname + " " + surname,
+                        startDay: fromDate,
+                        endDay: toDate,
+                    },
+                });
 
-            console.log("created booking and sent email");
+                sendEmail({
+                    variables: {
+                        firstname: firstname,
+                        surname: surname,
+                        receiverEmail: email,
+                        bookFrom: fromDate,
+                        bookTo: toDate,
+                    },
+                });
+                setErrorMessage("");
+
+                console.log("created booking and sent email");
+            } else {
+                setErrorMessage("Den valgte tidsperioden er ikke tilgjengelig.");
+            }
         }
     };
 
@@ -68,7 +79,7 @@ const BookPage = () => {
         <div>
             <Navbar></Navbar>
             <Heading>Fullføring av booking</Heading>
-            <Link href="/cabins">Tilbake</Link>{" "}
+            <Link href="/cabins">Tilbake</Link>
             <Summary from={fromDate} to={toDate} cabin={"Bjørnen"} price={1299}></Summary>
             <Card>
                 <Input placeholder="Fornavn" required={true} type="text" ref={firstnameRef}></Input>
@@ -78,6 +89,7 @@ const BookPage = () => {
                 <Button url="#" onClick={(e) => handleSubmit(e)}>
                     Fullfør booking
                 </Button>
+                <Paragraph>{errorMessage}</Paragraph>
             </Card>
         </div>
     );
