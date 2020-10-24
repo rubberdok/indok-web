da21e7e25e03f66b322b8092f74cca0a98fdec4c
Herman Holmøy <herman.holmoy12@gmail.com>
2020-10-25 00:54:48 +0200

    fix mail setup, outsource passwords

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from api.mailconfig import USERNAME, PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -135,11 +136,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = ["backend/static/cabins"]
 
 #CONFIG
-
-#EMAIL_HOST_USER = "booking.indokhyttene@gmail.com"
-#EMAIL_HOST_PASSWORD = "***REMOVED***"
-#EMAIL_HOST_USER = "rubberdok1@gmail.com"
-#EMAIL_HOST_PASSWORD = "***REMOVED***"
+EMAIL_HOST_USER = USERNAME
+EMAIL_HOST_PASSWORD = PASSWORD
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
