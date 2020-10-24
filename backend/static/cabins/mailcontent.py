527fd43e19558599fa2a6a597ee730304eefbfa2
Herman Holmøy <herman.holmoy12@gmail.com>
2020-10-25 00:54:48 +0200

    fix mail setup, outsource passwords

--- backend/static/cabins/mailcontent.py
+++ backend/static/cabins/mailcontent.py
@@ -3,29 +3,29 @@
 
 def get_no_html_mail(ctx):
     return f"""
-        Hei {ctx["firstname"]}!\n
+        Hei {ctx["firstname"]}!
       
-        Vi har nå reservert { ctx["cabin"] } fra { ctx["fromDate"] } til { ctx["toDate"] }.\n
+        Vi har nå reservert { ctx["cabin"] } fra { ctx["fromDate"] } til { ctx["toDate"] }.
 
         Prisen kommer på { ctx["price"] } kr. Betaling skjer via faktura, se
-        vedlegg.\n
+        vedlegg.
 
         Hytten skal forlates i minst like god stand som den ble funnet, og
         instrukser for utvask henger ved ytterdøren, samt som vedlegg i denne
         mailen. Dere står selv ansvarlig for eventuelle skader som måtte
         forekomme. Avbestilling uten gebyr må forekomme minst to uker før
         avreise. Vennligst les nøye gjennom reglementet og sjekklisten før
-        dere kommer til hytta.\n
+        dere kommer til hytta.
 
 
-        Ikke nøl med å ta kontakt hvis dere skulle ha spørsmål.\n
-        God tur!\n
+        Ikke nøl med å ta kontakt hvis dere skulle ha spørsmål.
+        God tur!
 
-        Med vennlig hilsen,\n
-        Ellie Berglund \n
-        Bookingansvarlig Hyttestyret,\n
-        Indøkhyttene NTNU\n
-        booking@indokhyttene.no / tlf. 94258380\n
-        https://www.januslinjeforening.no/indokhyttene/\n
+        Med vennlig hilsen,
+        Ellie Berglund 
+        Bookingansvarlig Hyttestyret,
+        Indøkhyttene NTNU
+        booking@indokhyttene.no / tlf. 94258380
+        https://www.januslinjeforening.no/indokhyttene/
     
     """
