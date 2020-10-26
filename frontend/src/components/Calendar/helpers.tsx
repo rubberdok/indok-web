7f15859c06155c3f40e0fc51a1feb773d70a6550
Sverre Spetalen <sverress@stud.ntnu.no>
2020-10-26 15:54:29 +0100

    refactor

---
+++ frontend/src/components/Calendar/helpers.tsx
@@ -0,0 +1,86 @@
+import moment from "moment";
+import { DATE_FORMAT } from "./constants";
+import { Day, DayCell } from "./styles";
+
+export const getDateRange = (start: moment.Moment, end: moment.Moment, format = DATE_FORMAT): string[] => {
+    const currentDate = start.clone();
+    const output = [];
+    end.add(1, "day");
+    while (currentDate.isBefore(end)) {
+        output.push(currentDate.format(format));
+        currentDate.add(1, "day");
+    }
+    return output;
+};
+
+export const rangeLength = (startDay: string, endDay: string): number => {
+    const start = moment([...startDay.split("-").map(Number)]);
+    const end = moment([...endDay.split("-").map(Number)]);
+    console.log(start, end);
+    return end.diff(start, "days") + 1;
+};
+
+export const previousMonthDays = (
+    month: moment.Moment,
+    selectedDay: moment.Moment,
+    dateClicked: (date: moment.Moment) => void,
+    key = ""
+) => {
+    const previousDays: JSX.Element[] = [];
+    const firstOfMonth = month.clone().startOf("month");
+    const mondayIndex = 0;
+
+    // Check if Month starts with a Monday
+    if (firstOfMonth.weekday() !== mondayIndex) {
+        const previousMonday = firstOfMonth.clone().subtract(1, "months").endOf("month").weekday(mondayIndex);
+        const dayDifference = firstOfMonth.diff(previousMonday, "days") + 1;
+
+        for (let i = 0; i < dayDifference; i++) {
+            const date = moment(firstOfMonth);
+            date.subtract(dayDifference - i, "day");
+            previousDays.push(
+                <DayCell
+                    isSelected={date.isSame(selectedDay, "day")}
+                    onClick={() => dateClicked(date)}
+                    outOfRange={true}
+                    key={`${key}-${date.format(DATE_FORMAT)}`}
+                >
+                    <Day>{date.format("D")}</Day>
+                </DayCell>
+            );
+        }
+    }
+    return previousDays;
+};
+
+export const nextMonthDays = (
+    month: moment.Moment,
+    selectedDay: moment.Moment,
+    dateClicked: (date: moment.Moment) => void
+) => {
+    const nextDays: JSX.Element[] = [];
+    const endOfMonth = month.clone().endOf("month");
+    const sundayIndex = 6;
+
+    // Check if Month ends with a sunday
+    if (endOfMonth.weekday() !== sundayIndex) {
+        const nextSunday = endOfMonth.clone().add(1, "months").startOf("month").weekday(sundayIndex);
+        const dayDifference = nextSunday.diff(endOfMonth, "days") + 1;
+
+        for (let i = 0; i < dayDifference; i++) {
+            const date = moment(endOfMonth);
+            date.add(i + 1, "day");
+            nextDays.push(
+                <DayCell
+                    isSelected={date.isSame(selectedDay, "day")}
+                    onClick={() => dateClicked(date)}
+                    outOfRange={true}
+                    key={`${date.format(DATE_FORMAT)}`}
+                >
+                    <Day>{date.format("D")}</Day>
+                </DayCell>
+            );
+        }
+    }
+    return nextDays;
+};
--- frontend/src/components/Calendar/index.tsx
+++ frontend/src/components/Calendar/index.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import moment from "moment";
 import { Day, DayCell, MonthPickButton, MonthSelector, EventMarkerWrapper, TwoCalendarsContainer } from "./styles";
 import _ from "lodash";
-import { getDateRange, rangeLength } from "./helpers";
+import { getDateRange, rangeLength, nextMonthDays, previousMonthDays } from "./helpers";
 import { NORWEGIAN_SHORT_DAY_NAMES, DATE_FORMAT } from "./constants";
 import CalendarTable from "./CalendarTable";
 
@@ -23,11 +23,10 @@ const Calendar = ({ events, rangeChanged }: CalendarProps) => {
     const [hoverRange, setHoverRange] = useState<string[]>([]);
     const [isRangeFreezed, setIsRangeFreezed] = useState(false);
 
-    const getDaysOfMonth = (isCurrentMonth = true) => {
+    const getDaysOfMonth = (month: moment.Moment) => {
         const daysOfMonth: JSX.Element[] = [];
-        const month = isCurrentMonth ? moment(selectedMonth) : moment(selectedMonth).clone().add(1, "month");
         for (let i = 1; i <= month.daysInMonth(); i++) {
-            const date = isCurrentMonth ? moment(selectedMonth) : moment(selectedMonth).clone().add(1, "month");
+            const date = moment(month);
             date.set("date", i);
             const dateEvents = events.filter((event) => event.date === date.format(DATE_FORMAT));
             daysOfMonth.push(
@@ -48,72 +47,11 @@ const Calendar = ({ events, rangeChanged }: CalendarProps) => {
         return daysOfMonth;
     };
 
-    const previousMonthDays = (isCurrentMonth = true) => {
-        const previousDays: JSX.Element[] = [];
-        const firstOfMonth = isCurrentMonth
-            ? selectedMonth.startOf("month")
-            : selectedMonth.clone().add(1, "month").startOf("month");
-        const mondayIndex = 0;
-
-        // Check if Month starts with a Monday
-        if (firstOfMonth.weekday() !== mondayIndex) {
-            const previousMonday = firstOfMonth.clone().subtract(1, "months").endOf("month").weekday(mondayIndex);
-            const dayDifference = firstOfMonth.diff(previousMonday, "days") + 1;
-
-            for (let i = 0; i < dayDifference; i++) {
-                const date = moment(firstOfMonth);
-                date.subtract(dayDifference - i, "day");
-                previousDays.push(
-                    <DayCell
-                        isSelected={date.isSame(selectedDay, "day")}
-                        onClick={() => handleDateClicked(date)}
-                        outOfRange={true}
-                        key={isCurrentMonth ? date.format(DATE_FORMAT) : `next-${date.format(DATE_FORMAT)}`}
-                    >
-                        <Day>{date.format("D")}</Day>
-                    </DayCell>
-                );
-            }
-        }
-        return previousDays;
-    };
-
-    const nextMonthDays = (isCurrentMonth = true) => {
-        const nextDays: JSX.Element[] = [];
-        const endOfMonth = isCurrentMonth
-            ? selectedMonth.endOf("month")
-            : selectedMonth.clone().add(1, "month").endOf("month");
-        const sundayIndex = 6;
-
-        // Check if Month ends with a sunday
-        if (endOfMonth.weekday() !== sundayIndex) {
-            const nextSunday = endOfMonth.clone().add(1, "months").startOf("month").weekday(sundayIndex);
-            const dayDifference = nextSunday.diff(endOfMonth, "days") + 1;
-
-            for (let i = 0; i < dayDifference; i++) {
-                const date = moment(endOfMonth);
-                date.add(i + 1, "day");
-                nextDays.push(
-                    <DayCell
-                        isSelected={date.isSame(selectedDay, "day")}
-                        onClick={() => handleDateClicked(date)}
-                        outOfRange={true}
-                        key={`${date.format(DATE_FORMAT)}`}
-                    >
-                        <Day>{date.format("D")}</Day>
-                    </DayCell>
-                );
-            }
-        }
-        return nextDays;
-    };
-
     const getRows = (month: moment.Moment) => {
-        const isCurrentMonth = month.isSame(selectedMonth, "month");
         const slots: JSX.Element[] = [
-            ...previousMonthDays(isCurrentMonth),
-            ...getDaysOfMonth(isCurrentMonth),
-            ...(isCurrentMonth ? [] : nextMonthDays(isCurrentMonth)),
+            ...previousMonthDays(month, selectedDay, handleDateClicked),
+            ...getDaysOfMonth(month),
+            ...(month.isSame(selectedMonth, "month") ? [] : nextMonthDays(month, selectedDay, handleDateClicked)),
         ];
         let cells: JSX.Element[];
         return slots.reduce(
@@ -168,7 +106,7 @@ const Calendar = ({ events, rangeChanged }: CalendarProps) => {
                 <MonthPickButton onClick={() => onChangeMonth(1)}>{">"}</MonthPickButton>
             </MonthSelector>
             <TwoCalendarsContainer>
-                <CalendarTable getRows={getRows} month={selectedMonth} />
+                <CalendarTable getRows={getRows} month={selectedMonth.clone()} />
                 <CalendarTable getRows={getRows} month={selectedMonth.clone().add(1, "month")} />
             </TwoCalendarsContainer>
         </>
