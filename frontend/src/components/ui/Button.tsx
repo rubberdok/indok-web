16fec6baf2c851ff297341309fae25e4852e9427
Mathias <skraagen@hotmail.com>
2020-10-26 17:18:54 +0100

    new button

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -3,56 +3,92 @@ import Link from "next/link";
 import styled from "styled-components";
 
 interface ButtonProps {
-    url: string;
+    link?: string;
     children: string | JSX.Element;
-    style?: any;
+    style?: "primary" | "secondary";
+    back?: boolean;
 }
 
+interface LinkProps {
+    condition: boolean;
+    wrapper: { (children: JSX.Element): JSX.Element };
+    children: JSX.Element;
+}
+
+const WithLink: React.FC<LinkProps> = ({ condition, wrapper, children }) => (condition ? wrapper(children) : children);
+
 const Button: React.FC<ButtonProps> = (props) => {
+    const styles = {
+        primary: Primary,
+        secondary: Secondary,
+    };
+
     return (
-        <Link href={props.url}>
-            <StyledButton as={props.style}>
-                <Container>{props.children}</Container>
-                <Icon>
-                    <i dangerouslySetInnerHTML={{ __html: feather.icons["arrow-right"].toSvg() }} />
-                </Icon>
+        <WithLink
+            condition={props.link ? true : false}
+            wrapper={(children: JSX.Element) => <Link href={props.link || "/"}>{children}</Link>}
+        >
+            <StyledButton back={props.back ? true : false} as={props.style ? styles[props.style] : undefined}>
+                {props.back ? (
+                    <>
+                        <Icon>
+                            <i dangerouslySetInnerHTML={{ __html: feather.icons["arrow-left"].toSvg() }} />
+                        </Icon>
+                        <Container>{props.children}</Container>
+                    </>
+                ) : (
+                    <>
+                        <Container>{props.children}</Container>
+                        <Icon>
+                            <i dangerouslySetInnerHTML={{ __html: feather.icons["arrow-right"].toSvg() }} />
+                        </Icon>
+                    </>
+                )}
             </StyledButton>
-        </Link>
+        </WithLink>
     );
 };
 
 const Icon = styled.div`
     background: #111;
     width: 70px;
-    display: table-cell;
+    display: flex;
     text-align: center;
-    vertical-align: middle;
     transition: 0.3s all ease;
     color: #fff;
+    justify-content: center;
+    align-items: center;
 
     & svg {
-        margin-top: 8px;
+        margin-top: 4px;
     }
 `;
 
-const StyledButton = styled.a`
+const StyledButton = styled.button<ButtonProps>`
     background: #282828;
     color: #fff;
     font-family: "Montserrat";
     font-size: 18px;
     border: none;
-    display: table;
+    display: flex;
+    align-items: stretch;
     text-decoration: none !important;
     transition: 0.3s all ease;
+    padding: 0;
 
     &:hover {
         background: #111;
         cursor: pointer;
 
         & ${Icon} {
-            padding-left: 10px;
+            padding: ${(props) => (props.back ? "0 10px 0 0" : "0 0 0 10px")};
             width: 70px;
         }
+    }
+
+    &:focus {
+        border: none;
+        outline: none;
     }
 `;
 
@@ -71,6 +107,19 @@ export const Primary = styled(StyledButton)`
     & ${Icon} {
         background: ${({ theme }) => theme.colors.primaryDark};
     }
-`
+`;
+
+export const Secondary = styled(StyledButton)`
+    background: ${({ theme }) => theme.colors.secondary};
+    color: #fff;
+
+    &:hover {
+        background: ${({ theme }) => theme.colors.secondaryDark};
+    }
+
+    & ${Icon} {
+        background: ${({ theme }) => theme.colors.secondaryDark};
+    }
+`;
 
 export default Button;
