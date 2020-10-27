feab2e4f2af3491712c5103763cd2d6a0e9326c6
Herman Holmøy <herman.holmoy12@gmail.com>
2020-10-27 21:19:15 +0100

    interactive arrow icon

--- frontend/src/components/ui/ArrowIcon.tsx
+++ frontend/src/components/ui/ArrowIcon.tsx
@@ -13,19 +13,31 @@ export const ArrowIconLink = ({ direction, size, href }: ArrowProps): JSX.Elemen
     return (
         <IconWrapper>
             <Link href={href}>
-                {direction == "r" ? (
-                    <ArrowRight size={size}></ArrowRight>
-                ) : direction == "l" ? (
-                    <ArrowLeft size={size}></ArrowLeft>
-                ) : direction == "u" ? (
-                    <ArrowUp size={size}></ArrowUp>
-                ) : (
-                    <ArrowDown size={size}></ArrowDown>
-                )}
+                <Arrow>
+                    {direction == "r" ? (
+                        <ArrowRight size={size}></ArrowRight>
+                    ) : direction == "l" ? (
+                        <ArrowLeft size={size}></ArrowLeft>
+                    ) : direction == "u" ? (
+                        <ArrowUp size={size}></ArrowUp>
+                    ) : (
+                        <ArrowDown size={size}></ArrowDown>
+                    )}
+                </Arrow>
             </Link>
         </IconWrapper>
     );
 };
+
+const Arrow = styled.div`
+    transition: all 0.2s ease-in-out;
+    transform-origin: 150% 50%;
+    &:hover {
+        cursor: pointer;
+        transform: scale(1.1);
+        color: ${({ theme }) => theme.colors.primary};
+    }
+`;
 
 const IconWrapper = styled.div`
     padding: 10px;
