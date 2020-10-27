8b0912a626c799ae1fe6dac495568efad8e5affd
Herman Holmøy <herman.holmoy12@gmail.com>
2020-10-27 21:16:14 +0100

    add simple checkbox component

---
+++ frontend/src/components/ui/Checkbox.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+interface CheckProps {
+    checked: boolean;
+    onClick: () => void;
+    errorMsg: string;
+}
+
+const CheckBox = ({ checked, onClick, errorMsg }: CheckProps): JSX.Element => {
+    console.log(errorMsg);
+    return (
+        <>
+            <CheckboxWrapper status={errorMsg}>
+                <input type="checkbox" onClick={onClick} onChange={onClick} checked={checked}></input>
+                <LabelText>Jeg samtykker til retningslinjene for booking av hytte</LabelText>
+            </CheckboxWrapper>
+        </>
+    );
+};
+
+const CheckboxWrapper = styled.label`
+    padding: 5px;
+    border: ${(props: { status: string }) => (props.status == "" ? "none" : "2px solid red")};
+`;
+
+const LabelText = styled.span`
+    padding: 5px;
+`;
+
+export default CheckBox;
