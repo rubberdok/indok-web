971d9ba2b7859673dba59aaa593fc707dd7cc064
Lars Waage <46653859+larwaa@users.noreply.github.com>
2020-10-29 18:34:59 +0100

    :sparkles: feat: add update survey and question mutations

--- frontend/src/graphql/surveys/mutations.ts
+++ frontend/src/graphql/surveys/mutations.ts
@@ -13,10 +13,36 @@ export const CREATE_SURVEY = gql`
     }
 `;
 
+export const UPDATE_SURVEY = gql`
+    mutation UpdateSurvey($id: ID!, $input: SurveyInput) {
+        createSurvey(id: $id, surveyData: $input) {
+            survey {
+                id
+                descriptiveName
+                description
+            }
+            ok
+        }
+    }
+`;
+
 //temporary implementation until bulk mutation is implemented
 export const CREATE_QUESTION = gql`
     mutation CreateQuestion($input: QuestionInput!) {
         createQuestion(questionData: $input) {
+            question {
+                id
+                question
+                description
+            }
+            ok
+        }
+    }
+`;
+
+export const UPDATE_QUESTION = gql`
+    mutation CreateQuestion($id: ID!, $input: QuestionInput) {
+        createQuestion(id: $id, questionData: $input) {
             question {
                 id
                 question
