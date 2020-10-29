4969063ac28beb65ce077a318c04f5ca8adb2244
Herman Holmøy <herman.holmoy12@gmail.com>
2020-10-29 22:21:14 +0100

    refacter send-mail code into separate file

--- backend/api/graphql/cabins/mutations.py
+++ backend/api/graphql/cabins/mutations.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from email.mime.image import MIMEImage
 from email.mime.text import MIMEText
 from static.cabins.mailcontent import get_no_html_mail
+from .mail import send_mails
 
 
 class CreateBooking(graphene.Mutation):
@@ -101,45 +102,8 @@ class SendEmail(graphene.Mutation):
     ok = graphene.Boolean()
 
     def mutate(self, info, firstname, surname, receiverEmail, bookFrom, bookTo, price):
-        subject = "Bekreftelsesmail for booking av Indøkhytte"
-
-        start_date = datetime.strptime(bookFrom, "%Y-%m-%d").isoformat().replace("-", "").replace(":", "") # Google Calendar wants YYYYMMDDThhmmss
-        end_date = datetime.strptime(bookTo, "%Y-%m-%d").isoformat().replace("-", "").replace(":", "")
-        text = "Hyttetur til Indøkhyttene"
-        location = "Oppdal+Skisenter+-+Stølen"
-        link = f"https://calendar.google.com/calendar/u/0/r/eventedit?text={text}&dates={start_date}/{end_date}&location={location}"
-
-        ctx = {
-            "firstname": firstname,
-            "surname": surname,
-            "cabin": "Bjørnen",
-            "fromDate": bookFrom,
-            "toDate": bookTo,
-            "price": price,
-            "link": link,
-        }
-
-        content = get_template("mailtemplate.html").render(ctx)
-        no_html_content = get_no_html_mail(ctx)
-        image_path = "static/cabins/hyttestyret_logo.png"
-        image_name = Path(image_path).name
-
-        msg = EmailMultiAlternatives(subject, no_html_content, "", [receiverEmail])
-        msg.attach_alternative(content, "text/html")
-        msg.content_subtype = 'html'  
-        msg.mixed_subtype = 'related' 
         
-        with open(image_path, mode='rb') as f:
-            image = MIMEImage(f.read())
-            image.add_header('Content-ID', f"<{image_name}>")
-            msg.attach(image)
-
-        msg.attach_file("static/cabins/Sjekkliste.docx")
-        msg.attach_file("static/cabins/Reglement.docx")
-
-        msg.send()
-
-
+        send_mails(info, firstname, surname, receiverEmail, bookFrom, bookTo, price)
 
         ok = True
         return SendEmail(ok=ok)
