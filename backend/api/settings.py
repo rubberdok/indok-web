5d1ea7d447bd3288c2869916ad5275ac4616c74c
Herman Holmøy <herman.holmoy12@gmail.com>
2020-10-29 17:04:15 +0100

    mail settings update

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from api.mailconfig import USERNAME, PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,8 +135,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = ["backend/static/cabins"]
 
 #CONFIG
-EMAIL_HOST_USER = USERNAME
-EMAIL_HOST_PASSWORD = PASSWORD
+EMAIL_HOST_USER = "hswebkom@gmail.com"
+EMAIL_HOST_PASSWORD = "Progging123"
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
