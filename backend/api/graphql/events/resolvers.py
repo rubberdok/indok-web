3f1edf216e9a841a7d7f2812dd2aedf21c1c3b2f
Morgan Heggland <morganheggland@gmail.com>
2020-10-29 18:55:52 +0100

    add auth decorators

--- backend/api/graphql/events/resolvers.py
+++ backend/api/graphql/events/resolvers.py
@@ -1,10 +1,13 @@
 from apps.events.models import Event
+from apps.users.decorators import login_required
 
 
 class EventResolvers:
+    @login_required
     def resolve_all_events(parent, info):
         return Event.objects.all()
 
+    @login_required
     def resolve_event(parent, info, id):
         try:
             return Event.objects.get(id=id)
