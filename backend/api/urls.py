669174580f5d974f9a80c8a45a80d021d8a8ea64
ankile <lars.ankile@gmail.com>
2021-01-09 17:46:43 +0100

    Add ping and fix dockerfiles

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -18,7 +18,10 @@ from django.urls import path
 from graphene_django.views import GraphQLView
 from django.views.decorators.csrf import csrf_exempt
 
+from apps.ping.views import Ping
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+    path("ping", Ping.as_view()),
 ]
