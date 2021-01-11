a7def128b791c2bd2f318caa48d25d8106128aff
ankile <lars.ankile@gmail.com>
2021-01-11 21:18:54 +0100

    Add missing env var in deploy script

--- deploy.sh
+++ deploy.sh
@@ -33,7 +33,7 @@ deploy_cluster() {
   service="indokweb-backend-service"
   template="ecs_backend_taskdefinition.json"
   task_template=$(cat "ecs/$template")
-  task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_RDS_HOST $AWS_RDS_PASSWORD $PRODUCTION_SECRET_KEY)
+  task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_RDS_HOST $AWS_RDS_PASSWORD $PRODUCTION_SECRET_KEY $DATAPORTEN_SECRET)
   echo "$task_def"
   register_definition
   update_service
