3f5a87b9a47b01ec0783c1766989f7a59ada8c6b
Herman Torsheim Holmøy <herman.holmoy12@gmail.com>
2021-01-11 19:40:05 +0100

    load contract data from book page

--- frontend/src/pages/cabins/contract.tsx
+++ frontend/src/pages/cabins/contract.tsx
@@ -1,24 +1,14 @@
 import Layout from "@components/Layout";
 import Contract from "@components/pages/cabins/Contract";
-
 import styled from "styled-components";
-import { ContractProps } from "../../components/pages/cabins/Contract";
+
 
 const ContractPage = () => {
-    const contractData = {
-        firstname: "Herman",
-        surname: "Holmøy",
-        cabin: "Bjørnen",
-        fromDate: "2020-01-01",
-        toDate: "2020-02-02",
-        price: 3000,
-    };
-
     return (
         <>
             <Layout>
                 <Container>
-                    <Contract data={contractData}></Contract>
+                    <Contract></Contract>
                 </Container>
             </Layout>
         </>
