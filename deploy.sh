035955930f702926b8fc3bd448d0f1b23689db3d
ankile <lars.ankile@gmail.com>
2021-01-11 09:17:16 +0100

    Add update services in deploy script

--- deploy.sh
+++ deploy.sh
@@ -18,21 +18,34 @@ register_definition() {
   fi
 }
 
+update_service() {
+  if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
+    echo "Error updating service."
+    return 1
+  fi
+}
+
 deploy_cluster() {
 
+  cluster="indokweb-cluster"
+
   # backend
+  service="indokweb-backend-service"
   template="ecs_backend_taskdefinition.json"
   task_template=$(cat "ecs/$template")
   task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_RDS_HOST $AWS_RDS_PASSWORD $PRODUCTION_SECRET_KEY)
   echo "$task_def"
   register_definition
+  update_service
 
   # frontend
+  service="indokweb-frontend-service"
   template="ecs_frontend_taskdefinition.json"
   task_template=$(cat "ecs/$template")
   task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
   echo "$task_def"
   register_definition
+  update_service
 
 }
 
