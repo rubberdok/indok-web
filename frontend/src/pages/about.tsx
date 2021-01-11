8ad335f1425b24cda84acd54000d87557a749b0c
ankile <lars.ankile@gmail.com>
2021-01-11 11:08:57 +0100

    Update deploy script to only deploy master

--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 import React from "react";
 
 const AboutPage: NextPage = () => {
-  return <Layout>This is the about page!</Layout>;
+  return <Layout>This is the about page! It is a work in progress right now!</Layout>;
 };
 
 export default AboutPage;
