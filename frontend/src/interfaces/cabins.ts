da9afbd0be8325c0f28874088d23d6bc8a1720f1
Morgan Heggland <morganheggland@gmail.com>
2021-01-15 18:39:42 +0100

    prettier fix

--- frontend/src/interfaces/cabins.ts
+++ frontend/src/interfaces/cabins.ts
@@ -1,37 +1,37 @@
 export interface Booking {
-    id: string;
-    firstname: string;
-    surname: string;
-    phone: number;
-    receiverEmail: string;
-    bookFrom: string;
-    bookTo: string;
-    price: number;
+  id: string;
+  firstname: string;
+  surname: string;
+  phone: number;
+  receiverEmail: string;
+  bookFrom: string;
+  bookTo: string;
+  price: number;
 }
 
 export interface Cabin {
-    name: string;
+  name: string;
 }
 
 export interface BookingFromTo {
-    from: Date;
-    to: Date;
+  from: Date;
+  to: Date;
 }
 
 export interface QueryVariables {
-    year: string;
-    month: string;
-    start?: string;
-    end?: string;
+  year: string;
+  month: string;
+  start?: string;
+  end?: string;
 }
 
 export interface ContractProps {
-    contractData: {
-        firstname: string;
-        surname: string;
-        cabin: string;
-        fromDate: string;
-        toDate: string;
-        price: number;
-    };
+  contractData: {
+    firstname: string;
+    surname: string;
+    cabin: string;
+    fromDate: string;
+    toDate: string;
+    price: number;
+  };
 }
