a7e5217f25daf75375d22f4a86bdd0c34c16b5c1
Herman Torsheim Holmøy <herman.holmoy12@gmail.com>
2021-01-21 18:17:01 +0100

    add confirmation page

---
+++ frontend/src/pages/cabins/confirmation.tsx
@@ -0,0 +1,31 @@
+import Layout from "@components/Layout";
+import { Header } from "@components/pages/cabins/CabinBooker/styles";
+import { Heading, Paragraph } from "@components/ui/Typography";
+import Link from "next/link";
+import React from "react";
+import styled from "styled-components";
+
+const Confirmation = () => {
+  return (
+    <>
+      <Layout>
+        <Container>
+          <Heading>Bekreftelse for booking</Heading>
+          <Paragraph>
+            Takk for din booking! Denne nettsiden er fortsatt under konstruksjon, så det blir ikke faktisk opprettet en
+            booking. Det må du fortsatt gjøre gjennom{" "}
+            <Link href="mailto:booking@indokhyttene.no">booking@indokhyttene.no</Link>
+          </Paragraph>
+        </Container>
+      </Layout>
+    </>
+  );
+};
+
+const Container = styled.div`
+  margin: auto;
+  width: 70%;
+  padding: 50px;
+`;
+
+export default Confirmation;
