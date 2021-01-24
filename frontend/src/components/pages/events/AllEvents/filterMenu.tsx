48921d9ecd37fe0c86b7b8e7fbd618f04e2185c8
mathildesolberg <mathilde.solberg@hotmail.com>
2021-01-24 19:28:33 +0100

    redesign filter component

--- frontend/src/components/pages/events/AllEvents/filterMenu.tsx
+++ frontend/src/components/pages/events/AllEvents/filterMenu.tsx
@@ -1,9 +1,14 @@
 import { useQuery } from "@apollo/client";
+import { Filter } from "react-feather";
 import { GET_CATEGORIES, QUERY_EVENT_FILTERED_ORGANIZATIONS } from "@graphql/events/queries";
 import { Category } from "@interfaces/events";
 import { Organization } from "@interfaces/organizations";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ChevronDown, ChevronRight } from "react-feather";
 import { FilterQuery } from ".";
+import { Paragraph } from "@components/ui/Typography";
+import styled from "styled-components";
+import colors from "src/styles/theme";
 
 interface Props {
   filters: FilterQuery;
@@ -11,61 +16,70 @@ interface Props {
 }
 
 const FilterMenu: React.FC<Props> = ({ filters, onChange }) => {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  const handleClick = () => {};
+  const [currentFilters, setCurrentFilters] = useState(filters);
+  const [openOrganizationDropDown, setOpenOrganizationDropDown] = useState(false);
+  const [openCategoryDropDown, setOpenCategoryDropDown] = useState(false);
+
+  useEffect(() => {
+    setCurrentFilters(filters);
+  }, [filters]);
 
   const OrganizationFilter = () => {
     const { loading, error, data } = useQuery(QUERY_EVENT_FILTERED_ORGANIZATIONS);
-
     // should handle loading status
     if (loading) return <p>Loading...</p>;
 
     if (error) return <p>Error :(</p>;
 
     return (
-      <div>
-        <h5 style={{ margin: "0px" }}>{"Organisasjoner"}</h5>
-        {data.eventFilteredOrganizations.map((org: Organization) => (
-          <div key={org.name}>
-            <div
-              style={{
-                cursor: "pointer",
-                width: "fit-content",
-                color: filters?.organization === org.name ? "#065A5A" : "#707070",
-                fontWeight: filters?.organization === org.name ? 700 : 500,
-              }}
-              aria-hidden="true"
-              onClick={() => {
-                onChange({ ...filters, organization: org.name });
-              }}
-              onKeyDown={handleClick}
-            >
-              <p style={{ marginBottom: "0px", marginTop: "0.05em" }}>{org.name}</p>
-            </div>
+      <div style={{ paddingTop: "0.5em" }}>
+        <InvisibleButton
+          onClick={() => setOpenOrganizationDropDown(!openOrganizationDropDown)}
+          className={openOrganizationDropDown ? "active" : ""}
+        >
+          Organisasjoner
+          {openOrganizationDropDown ? (
+            <ChevronDown style={{ float: "left", width: "18px", paddingBottom: "2%" }} />
+          ) : (
+            <ChevronRight style={{ float: "left", width: "18px", paddingBottom: "2%" }} />
+          )}
+        </InvisibleButton>
 
-            {org.children?.map((subOrg: Organization) => (
-              <div
-                key={subOrg.name}
+        <FadeInAndOutDiv className={openOrganizationDropDown ? "active" : ""}>
+          {data.eventFilteredOrganizations.map((org: Organization) => (
+            <div key={org.name}>
+              <InvisibleButton
                 style={{
-                  marginLeft: "2em",
-                  fontSize: "15px",
-                  cursor: "pointer",
-                  width: "fit-content",
-                  color:
-                    filters?.organization === subOrg.name || filters?.organization === org.name ? "#065A5A" : "#909090",
-                  fontWeight: filters?.organization === subOrg.name || filters?.organization === org.name ? 700 : 500,
+                  marginBottom: "0.4em",
+                  background: currentFilters?.organization === org.name ? "red" : "transparent",
                 }}
-                aria-hidden="true"
-                onClick={() => {
-                  onChange({ ...filters, organization: subOrg.name });
-                }}
-                onKeyDown={handleClick}
+                className={currentFilters?.organization === org.name ? "active" : ""}
+                onClick={() => setCurrentFilters({ ...currentFilters, organization: org.name })}
               >
-                <p style={{ margin: "0px" }}>{subOrg.name}</p>
-              </div>
-            ))}
-          </div>
-        ))}
+                <Paragraph style={{ marginBottom: "0px", marginTop: "0.05em", fontSize: "16px" }}>{org.name}</Paragraph>
+              </InvisibleButton>
+
+              {org.children?.map((subOrg: Organization) => (
+                <InvisibleButton
+                  key={subOrg.name}
+                  style={{ marginBottom: "0.3em", marginLeft: "1em" }}
+                  className={
+                    currentFilters?.organization === subOrg.name || currentFilters?.organization === org.name
+                      ? "active"
+                      : ""
+                  }
+                  onClick={() => setCurrentFilters({ ...currentFilters, organization: subOrg.name })}
+                >
+                  <Paragraph
+                    style={{ marginBottom: "0px", marginTop: "0.05em", fontSize: "14px", width: "fit-content" }}
+                  >
+                    {subOrg.name}
+                  </Paragraph>
+                </InvisibleButton>
+              ))}
+            </div>
+          ))}
+        </FadeInAndOutDiv>
       </div>
     );
   };
@@ -79,50 +93,59 @@ const FilterMenu: React.FC<Props> = ({ filters, onChange }) => {
     if (error) return <p>Error :(</p>;
 
     return (
-      <div>
-        <h5 style={{ marginBottom: "0px" }}>{"Kategorier"}</h5>
-        {data.allCategories.map((category: Category) => (
-          <div
-            key={category.name}
-            style={{
-              cursor: "pointer",
-              width: "fit-content",
-              color: filters?.category === category.name ? "#065A5A" : "#707070",
-              fontWeight: filters?.category === category.name ? 700 : 500,
-            }}
-            aria-hidden="true"
-            onClick={() => {
-              onChange({ ...filters, category: category.name });
-            }}
-            onKeyDown={handleClick}
-          >
-            <p style={{ marginTop: "0px" }}>{category.name}</p>
-          </div>
-        ))}
+      <div style={{ paddingTop: "0.5em" }}>
+        <InvisibleButton
+          onClick={() => setOpenCategoryDropDown(!openCategoryDropDown)}
+          className={openCategoryDropDown ? "active" : ""}
+        >
+          Kategorier
+          {openCategoryDropDown ? (
+            <ChevronDown style={{ float: "left", width: "18px", paddingBottom: "2%" }} />
+          ) : (
+            <ChevronRight style={{ float: "left", width: "18px", paddingBottom: "2%" }} />
+          )}
+        </InvisibleButton>
+
+        <FadeInAndOutDiv className={openCategoryDropDown ? "active" : ""}>
+          {data.allCategories.map((category: Category) => (
+            <InvisibleButton
+              key={category.name}
+              style={{ marginBottom: "0.4em" }}
+              className={currentFilters?.category === category.name ? "active" : ""}
+              onClick={() => setCurrentFilters({ ...currentFilters, category: category.name })}
+            >
+              <Paragraph style={{ marginBottom: "0px", marginTop: "0.05em", fontSize: "16px" }}>
+                {category.name}
+              </Paragraph>
+            </InvisibleButton>
+          ))}
+        </FadeInAndOutDiv>
       </div>
     );
   };
 
   const DateTimeFilter = () => {
     return (
-      <div>
-        <h5 style={{ margin: "0px" }}>{"Tidspunkt"}</h5>
+      <div style={{ paddingTop: "0.5em" }}>
+        <Paragraph style={{ color: "#222", margin: 0, fontSize: "16px" }}>Tidspunkt</Paragraph>
+
         <div>
-          <div>Fra:</div>
+          <Paragraph style={{ marginBottom: "0px", marginTop: "0.05em", fontSize: "14px" }}>Fra:</Paragraph>
           <input
             type="datetime-local"
             placeholder="Start time"
-            value={filters?.startTime}
-            onChange={(e) => onChange({ ...filters, startTime: e.currentTarget.value })}
+            value={currentFilters?.startTime}
+            onChange={(e) => setCurrentFilters({ ...currentFilters, startTime: e.currentTarget.value })}
           />
         </div>
+
         <div>
-          <div>Til:</div>
+          <Paragraph style={{ marginBottom: "0px", marginTop: "0.05em", fontSize: "14px" }}>Til:</Paragraph>
           <input
             type="datetime-local"
             placeholder="Start time"
-            value={filters?.endTime}
-            onChange={(e) => onChange({ ...filters, endTime: e.currentTarget.value })}
+            value={currentFilters?.endTime}
+            onChange={(e) => setCurrentFilters({ ...currentFilters, endTime: e.currentTarget.value })}
           />
         </div>
       </div>
@@ -134,22 +157,151 @@ const FilterMenu: React.FC<Props> = ({ filters, onChange }) => {
       style={{
         float: "left",
         width: "25%",
-        paddingLeft: "1em",
         border: "solid",
-        borderWidth: "0.05em",
-        borderColor: "#6A9997",
+        borderWidth: "1px 2px 2px 1px",
+        borderColor: "#DCDCDC",
         borderRadius: "0.2em",
         backgroundColor: "#FFF",
         paddingBottom: "1em",
+        minWidth: "235px",
       }}
     >
-      <h3 style={{ margin: "0px" }}>Filter</h3>
-      <button onClick={() => onChange({})}>{"Nullstill filtre"}</button>
-      <OrganizationFilter />
-      <CategoryFilter />
-      <DateTimeFilter />
+      <div style={{ paddingLeft: "1em" }}>
+        <div style={{ width: "100%" }}>
+          <Filter style={{ marginTop: "0.1em", marginRight: "0.3em", float: "left", color: "#222" }} />
+          <Paragraph>Filtre</Paragraph>
+        </div>
+        <div style={{ color: "#E3E3E3", backgroundColor: "#E3E3E3", marginRight: "10%", marginTop: "-7%" }}>
+          <hr />
+        </div>
+        <div>
+          <OrganizationFilter />
+          <CategoryFilter />
+          <DateTimeFilter />
+        </div>
+      </div>
+
+      <div
+        style={{
+          marginTop: "1em",
+          marginLeft: "auto",
+          marginRight: "auto",
+          width: "fit-content",
+        }}
+      >
+        <StyledCancelButton
+          style={{ float: "left", marginRight: "1em" }}
+          onClick={() => {
+            onChange({});
+            setCurrentFilters({});
+          }}
+        >
+          Nullstill
+        </StyledCancelButton>
+        <StyledFilterButton style={{ float: "left" }} onClick={() => onChange(currentFilters)}>
+          Filtrer
+        </StyledFilterButton>
+      </div>
     </div>
   );
 };
 
 export default FilterMenu;
+
+const FadeInAndOutDiv = styled.div`
+  padding-left: 1em;
+  opacity: 0;
+  width: 0;
+  height: 0;
+  transition: width 0.5s 0.2s, height 0.5s 0.2s, opacity 0.1s;
+  overflow: hidden;
+  width: 100%;
+
+  &.active {
+    opacity: 1;
+    height: fit-content;
+    transition: width 0.5s, height 1s, opacity 0.5s 0.1s;
+    overflow: visible;
+    width: 100%;
+  }
+`;
+
+const InvisibleButton = styled.button`
+  background: transparent;
+  color: #222;
+  font-family: "Montserrat";
+  font-size: 16px;
+  border: none;
+  display: flex;
+  text-decoration: none !important;
+  padding: 0;
+  border-bottom: 1px solid transparent;
+  transition: border 500ms ease; 
+
+  &.active {
+    cursor: pointer;
+    font-weight: 600;
+  }
+
+  &:hover {
+    cursor: pointer;
+    border-bottom: 1px solid #222;
+  }
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+
+  }
+`;
+
+const StyledFilterButton = styled.button`
+  background: ${colors.colors.primary};
+  color: #fff;
+  font-family: "Montserrat";
+  font-size: 16px;
+  border: none;
+  display: flex;
+  align-items: stretch;
+  text-decoration: none !important;
+  transition: 0.3s all ease;
+  padding: 0.4em;
+  min-width: 80px;
+  padding-left: 9%;
+
+  &:hover {
+    background: ${colors.colors.primaryDark};
+    cursor: pointer;
+  }
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`;
+
+const StyledCancelButton = styled.button`
+  background: ${colors.colors.secondary};
+  color: #fff;
+  font-family: "Montserrat";
+  font-size: 16px;
+  border: none;
+  display: flex;
+  align-items: stretch;
+  text-decoration: none !important;
+  transition: 0.3s all ease;
+  padding: 0.4em;
+  min-width: 80px;
+  padding-left: 5%;
+
+  &:hover {
+    background: ${colors.colors.secondaryDark};
+    cursor: pointer;
+  }
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`;
--- frontend/src/components/pages/events/AllEvents/index.tsx
+++ frontend/src/components/pages/events/AllEvents/index.tsx
@@ -2,10 +2,11 @@ import { useQuery } from "@apollo/client";
 import { GET_EVENTS } from "@graphql/events/queries";
 import { Event } from "@interfaces/events";
 import Link from "next/link";
-import { PlusSquare, Filter } from "react-feather";
+import { PlusSquare } from "react-feather";
 import styled from "styled-components";
 import React, { useState } from "react";
 import { NavItem } from "../../../navbar/NavbarLinks";
+import color from "src/styles/theme";
 import FilterMenu from "./filterMenu";
 
 export interface FilterQuery {
@@ -28,77 +29,73 @@ const AllEvents: React.FC = () => {
 
   const onChange = (newFilters: FilterQuery) => {
     setFilters(newFilters);
-    refetch(newFilters);
+    if (newFilters !== undefined) {
+      refetch(newFilters);
+    }
   };
 
   return (
     <div>
-      <NavItem
-        style={{ paddingBottom: "0px", paddingTop: "0px", margin: "0px", fontSize: "1.1em" }}
-        className={!showTableView ? "active" : ""}
-        primary
-        onClick={() => setShowTableView(!showTableView)}
-      >
-        Liste
-      </NavItem>
-      <NavItem
-        style={{ paddingBottom: "0px", paddingTop: "0px", fontSize: "1.1em" }}
-        className={showTableView ? "active" : ""}
-        primary
-        onClick={() => setShowTableView(!showTableView)}
-      >
-        Kalender
-      </NavItem>
+      <div>
+        <NavItem
+          style={{ paddingBottom: "0px", paddingTop: "0px", margin: "0px", fontSize: "1.1em" }}
+          className={!showTableView ? "active" : ""}
+          primary
+          onClick={() => setShowTableView(!showTableView)}
+        >
+          Liste
+        </NavItem>
+        <NavItem
+          style={{ paddingBottom: "0px", paddingTop: "0px", fontSize: "1.1em" }}
+          className={showTableView ? "active" : ""}
+          primary
+          onClick={() => setShowTableView(!showTableView)}
+        >
+          Kalender
+        </NavItem>
 
-      <div style={{ float: "right" }}>
-        <Link href={`/events/create-event`}>
-          <StyledIconButton>
-            <PlusSquare />
-            <p style={{ margin: 0 }}>Opprett</p>
-          </StyledIconButton>
-        </Link>
-      </div>
-
-      <div style={{ float: "right", marginRight: "15px" }}>
-        <StyledIconButton>
-          <Filter />
-          <p style={{ margin: 0 }}>Filter</p>
-        </StyledIconButton>
+        <div style={{ float: "right" }}>
+          <Link href={`/events/create-event`}>
+            <StyledIconButton>
+              <PlusSquare />
+              <p style={{ margin: 0 }}>Opprett</p>
+            </StyledIconButton>
+          </Link>
+        </div>
       </div>
 
       <div style={{ width: "100%", paddingTop: "1em" }}>
-        {showTableView ? (
-          <p>{"Kommer snart! :)"}</p>
-        ) : (
-          <>
-            {data.allEvents.length === 0 ? (
-              <h4>{"Ingen arrangementer passer til valgte filtere"}</h4>
-            ) : (
-              data.allEvents.map((event: Event) => (
-                <Link href={`/events/${event.id}`} key={event.id}>
-                  <a href={`/events/${event.id}`} style={{ color: "#000" }}>
-                    <div
-                      style={{
-                        border: "solid",
-                        borderWidth: "0.05em 0.05em 0.05em 1.2em",
-                        borderColor: event.organization.color ?? "#6A9997",
-                        borderRadius: "0.2em",
-                        padding: "0.5em",
-                        marginBottom: "0.5em",
-                        backgroundColor: "#FFF",
-                      }}
-                    >
-                      <p style={{ marginBottom: "0.2em" }}>{event.title}</p>
-                      <p style={{ marginTop: 0 }}>Starttid: {event.startTime.slice(0, 19).replace("T", " ")}</p>
-                    </div>
-                  </a>
-                </Link>
-              ))
-            )}
-          </>
-        )}
+        <div>
+          <FilterMenu filters={filters} onChange={onChange} />
+        </div>
+
+        <div style={{ width: "70%", float: "right" }}>
+          {showTableView ? (
+            <p>{"Kommer snart! :)"}</p>
+          ) : (
+            <>
+              {data.allEvents.length === 0 ? (
+                <h4>{"Ingen arrangementer passer til valgte filtere"}</h4>
+              ) : (
+                data.allEvents.map((event: Event) => (
+                  <Link href={`/events/${event.id}`} key={event.id}>
+                    <EventContainerLink href={`/events/${event.id}`} style={{ color: "#000" }}>
+                      <EventContainer
+                        style={{
+                          borderColor: event.organization.color ?? color.colors.primaryLight,
+                        }}
+                      >
+                        <p style={{ marginBottom: "0.2em" }}>{event.title}</p>
+                        <p style={{ marginTop: 0 }}>Starttid: {event.startTime.slice(0, 19).replace("T", " ")}</p>
+                      </EventContainer>
+                    </EventContainerLink>
+                  </Link>
+                ))
+              )}
+            </>
+          )}
+        </div>
       </div>
-      <FilterMenu filters={filters} onChange={onChange} />
     </div>
   );
 };
@@ -124,5 +121,28 @@ const StyledIconButton = styled.button`
   &:focus {
     border: none;
     outline: none;
+  }
+`;
+
+const EventContainer = styled.div`
+  border: solid;
+  border-width: 0.05em 0.05em 0.05em 1.2em;
+  border-color: ${color.colors.primaryLight};
+  border-radius: 0.2em;
+  padding: 0.5em;
+  margin-bottom: 0.5em;
+  background-color: #fff;
+
+  &:hover {
+    cursor: pointer;
+    background-color: #f4f4f4;
+  }
+`;
+
+const EventContainerLink = styled.a`
+  text-decoration: none;
+
+  &:hover {
+    text-decoration: none;
   }
 `;
