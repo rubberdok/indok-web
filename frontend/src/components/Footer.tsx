f62b2b8bfbb9f7589d4e42add57aae46b3066fa6
Skraagen <skraagen@hotmail.com>
2021-01-26 17:11:21 +0100

    added hall of fame and copyright to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,22 @@
-import { Box, Link, makeStyles, Typography } from "@material-ui/core";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  Link,
+  makeStyles,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from "@material-ui/core";
 import Container from "@material-ui/core/Container";
+import CloseIcon from "@material-ui/icons/Close";
 import React from "react";
 
 const useStyles = makeStyles((theme) => ({
@@ -20,81 +37,95 @@ const useStyles = makeStyles((theme) => ({
     color: "#fff",
     background: "#000",
   },
+  table: {
+    minWidth: 800,
+  },
+  closeButton: {
+    position: "absolute",
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
+  },
+  text: {
+    marginLeft: 40,
+  },
 }));
 
 const Footer: React.FC = () => {
   const classes = useStyles();
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-      <Box pt={10} pb={5}>
-        {/* <Container>
-          <Box position="relative" py={10}>
-            <img className={classes.nth} src="./nth.png" />
-            <Grid container>
-              <Grid item xs={1}>
-                <List dense disablePadding>
-                  <ListItem>
-                    <ListItemText>
-                      <Typography variant="caption"><Link href="#">Home</Link></Typography>
-                    </ListItemText>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemText>
-                      <Typography variant="caption"><Link href="#">Home</Link></Typography>
-                    </ListItemText>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemText>
-                      <Typography variant="caption"><Link href="#">Home</Link></Typography>
-                    </ListItemText>
-                  </ListItem>
-                </List>
-              </Grid>
-              <Grid item xs={1}>
-                <List dense disablePadding>
-                  <ListItem>
-                    <ListItemText>
-                      <Typography variant="caption"><Link href="#">Home</Link></Typography>
-                    </ListItemText>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemText>
-                      <Typography variant="caption"><Link href="#">Home</Link></Typography>
-                    </ListItemText>
-                  </ListItem>
-                  <ListItem>
-                    <ListItemText>
-                      <Typography variant="caption"><Link href="#">Home</Link></Typography>
-                    </ListItemText>
-                  </ListItem>
-                </List>
-              </Grid>
-              <Grid item xs>
-
-              </Grid>
-              <Grid item xs={2}>
-                <Box>
-                  <img className={classes.logo} src={"/logo.svg"} alt="INDØK Hovedstyre" />
-                  <Typography variant="caption">Hjemmesiden for studentfrivilligheten ved Industriell Økonomi og Teknologiledelse, NTNU</Typography>
-                </Box>
-              </Grid>
-            </Grid>
-          </Box>
-
-        </Container> */}
-      </Box>
-      <Box className={classes.credits}>
+      <Box mt={10} className={classes.credits}>
         <Container>
-          <Typography variant="caption">
+          <Button variant="contained" onClick={handleClickOpen}>
+            Hall of Fame
+          </Button>
+          <Typography className={classes.text} variant="caption">
             Under konstruksjon av{" "}
             <Link color="inherit" target="_blank" href="https://github.com/hovedstyret/indok-web">
               RubberDøk
             </Link>
-            , Hovedstyret på Indøk sin webkomité.
+            , Hovedstyret på Indøk sin webkomité. Kopirett © 2021 Foreningen for studentene ved Indøk. Alle rettigheter
+            reservert.
           </Typography>
         </Container>
       </Box>
+      <Dialog
+        maxWidth="xl"
+        fullWidth={false}
+        onClose={handleClose}
+        aria-labelledby="customized-dialog-title"
+        open={open}
+      >
+        <DialogTitle disableTypography>
+          <Typography gutterBottom variant="h4">
+            Hall of Fame 2020/2021
+          </Typography>
+          <Typography variant="body1">Laget med blod, svette, tårer og kjærlighet av</Typography>
+          <IconButton className={classes.closeButton} aria-label="close" onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </DialogTitle>
+        <DialogContent>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} aria-label="simple table">
+              <TableBody>
+                <TableRow>
+                  <TableCell>Morgan Heggland &apos;22</TableCell>
+                  <TableCell>Ingrid Aaseng &apos;22</TableCell>
+                  <TableCell>Herman Holmøy &apos;25</TableCell>
+                  <TableCell>Fredrik Ahlborg &apos;22</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Patrik Kjærran &apos;22</TableCell>
+                  <TableCell>Anna Sofie Lunde &apos;22</TableCell>
+                  <TableCell>Hermann Mørkrid &apos;23</TableCell>
+                  <TableCell>Mathias Raa &apos;25</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Mathilde Marie Solberg &apos;23</TableCell>
+                  <TableCell>Sverre Spetalen &apos;21</TableCell>
+                  <TableCell>Lars Waage &apos;23</TableCell>
+                  <TableCell>Lars Lien Ankile &apos;22</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Andreas Johannesen &apos;22</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
