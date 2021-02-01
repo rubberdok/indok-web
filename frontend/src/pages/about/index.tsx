60311fb4f95aad854ce7fb1154851ece3b109ccf
Fredrik Ahlborg <fredrik.sha@gmail.com>
2021-02-01 19:46:32 +0100

    update about content

--- frontend/src/pages/about/index.tsx
+++ frontend/src/pages/about/index.tsx
@@ -7,14 +7,51 @@ const AboutPage: NextPage = () => {
   return (
     <Layout>
       <Container>
-        <Typography variant="h1">Om foreningen</Typography>
-        <Typography variant="h2">Fredrik lærer seg progging atm :D</Typography>
-        <Typography variant="h3">Les mer her</Typography>
+        <Typography variant="h1">Om Foreningen</Typography>
+        <br />
+        <Typography variant="h3">Hvem er vi?</Typography>
         <Typography variant="body1">
-          Velkommen til den offisielle nettsiden for Foreningen for studentene ved Indøk! Siden er laget av og for
-          studentene selv. Her kan man lære mer om Indøk, vår historie, studentfrivilligheten og alle sosiale
-          begivenheter.
+          Foreningen for Studentene ved Industriell Økonomi og Teknologiledelse er den øverste instansen
+          (moderorganisasjonen) for all studentfrivillighet på masterstudiet Indøk ved NTNU. Foreningen drives av over
+          200 ivrige sjeler og over 20 ulike organisasjoner, hvor alt fra veldedighet og ølbrygging til fadderuker og
+          case-trening står på agendaen. Foreningen ledes av Hovedstyret, som forvalter midlene og fungerer som et
+          koordinerende forum mellom de ulike studentforeningene. Hovedstyret består av et valgt lederpar, lederne for
+          hver av de større studentforeningene på Indøk og instituttillitsvalgt på IØT. Generalforsamlingen er
+          Foreningens øverste myndighet, hvor alle studentene på Indøk har stemmerett.
         </Typography>
+        <br />
+        <Typography variant="h3">Hovedstyret blir født</Typography>
+        <Typography variant="body1">
+          Foreningen for Studentene ved Industriell Økonomi og Teknologiledelse, NTNU er formaliseringen av all
+          foreningsaktivitet på Indøk under ett og samme tak. På den måten kan vi si at Foreningen har eksistert så
+          lenge studentfrivilligheten på Indøk har det. Allerede på det første Indøk-kullet i 86´ ble
+          næringslivskontakten Bindeleddet startet. I år 2000 hadde man den første festen på Janus-kjellerne. I løpet av
+          de neste 20 årene skulle flere titalls tilbud bli startet. <br />
+          <br />I tidligere år hadde linjeforeningene på Indøk ett «Fordelingsmøte» i semesteret der studenter og
+          linjeforeninger på studiet kunne søke om støtte til ulike prosjekter eller arrangementer. Fordelingsmøtet
+          hadde som mandat å behandle søknader og fremme en innstilling om bruk av tilgjengelige midler overfor Allmøtet
+          på IØT. Da denne ordningen ikke viste seg å være tilstrekkelig for å sikre langsiktighet, og da
+          linjeforeningene så behov for et koordinerende forum, ble det i 2008 opprettet et Hovedstyre for studentene på
+          Indøk med tilhørende vedtekter.
+          <br />
+          <br /> For å formalisere dette organet i høyere grad, og for å tilfredsstille krav til vedtekter fra
+          Brønnøysundregisteret, opprettet studentene på Indøk Foreningen for studentene ved Industriell Økonomi og
+          Teknologiledelse, NTNU, i februar 2010. Dette er en forening for alle studentene på sivilingeniørstudiet
+          Industriell Økonomi og Teknologiledelse på NTNU, og denne foreningen styres av det som kalles Hovedstyret. I
+          dette styret sitter følgende personer i ett år:
+          <ul>
+            <li>Et lederpar (valgt på Generalforsamlingen i april)</li>
+            <li> President for Janus</li>
+            <li> Leder i Bindeleddet NTNU</li>
+            <li> Leder i ESTIEM</li>
+            <li> Leder i Hyttestyret</li>
+            <li> Leder av Janus IF</li>
+            <li> Leder av Indøk Kultur</li>
+            <li> Én av ITV’ene</li>
+          </ul>
+        </Typography>
+        <br />
+        <Typography variant="h3">Hovedstyret blir født</Typography>
       </Container>
     </Layout>
   );
