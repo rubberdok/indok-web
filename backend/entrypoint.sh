64381998687c0761d19ab436baf9a28a401aa251
ankile <lars.ankile@gmail.com>
2021-02-01 18:13:08 +0100

    Add env variable for settings module to streamline backend

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python manage.py migrate --settings api.settings
-python manage.py runserver 0.0.0.0:8000 --settings api.settings
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
 exec "$@"
