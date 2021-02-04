74dc5b8cb0c056951e5c5f707bb4e7093bca5c27
Sverre Spetalen <sverress@stud.ntnu.no>
2021-02-04 20:01:11 +0100

    removing momentjs from project

--- frontend/src/components/Calendar/helpers.tsx
+++ frontend/src/components/Calendar/helpers.tsx
@@ -1,58 +1,58 @@
-import moment from "moment";
-import { DATE_FORMAT } from "./constants";
 import { Day, DayCell } from "./styles";
+import { DATE_FORMAT } from "./constants";
 import dayjs from "dayjs";
-const hello = dayjs();
-console.log(hello.format());
+import duration from "dayjs/plugin/duration";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import weekday from "dayjs/plugin/weekday";
+import "dayjs/locale/nb";
 
-export const getDateRange = (start: string, end: string, format = DATE_FORMAT): string[] => {
-  const currentDate = moment(start);
-  const endDate = moment(end);
+dayjs.locale("nb");
+dayjs.extend(duration);
+dayjs.extend(isSameOrBefore);
+dayjs.extend(weekday);
+
+export const getDateRange = (start: string, end: string): string[] => {
+  let startDate = dayjs(start);
+  const endDate = dayjs(end);
   const output = [];
-  endDate.add(1, "day");
-  while (currentDate.isBefore(endDate)) {
-    output.push(currentDate.format(format));
-    currentDate.add(1, "day");
+  while (startDate.isSameOrBefore(endDate, "date")) {
+    output.push(startDate.format(DATE_FORMAT));
+    startDate = startDate.add(1, "day");
   }
   return output;
 };
 export const rangeLength = (startDay: string, endDay: string): number => {
-  const start = moment(startDay, "YYYY-MM-DD");
-  const end = moment(endDay, "YYYY-MM-DD");
-  return end.diff(start, "days") + 1;
+  return dayjs(endDay).diff(dayjs(startDay), "days") + 1;
 };
 
 export const previousMonthDays = (
-  month: moment.Moment,
-  selectedDay: moment.Moment,
-  dateClicked: (date: moment.Moment) => void,
-  visable = true,
-  key = ""
+  month: dayjs.Dayjs,
+  selectedDay: dayjs.Dayjs,
+  dateClicked: (date: dayjs.Dayjs) => void
 ): JSX.Element[] => {
+  const visable = false;
   const previousDays: JSX.Element[] = [];
-  const firstOfMonth = month.clone().startOf("month");
+  const firstOfMonth = month.startOf("month");
   const mondayIndex = 0;
-
   // Check if Month starts with a Monday
   if (firstOfMonth.weekday() !== mondayIndex) {
-    const previousMonday = firstOfMonth.clone().subtract(1, "months").endOf("month").weekday(mondayIndex);
+    const previousMonday = firstOfMonth.subtract(1, "months").endOf("month").weekday(mondayIndex);
     const dayDifference = firstOfMonth.diff(previousMonday, "days") + 1;
 
     for (let i = 0; i < dayDifference; i++) {
-      const date = moment(firstOfMonth);
-      date.subtract(dayDifference - i, "day");
+      const date = firstOfMonth.subtract(dayDifference - i, "day");
       previousDays.push(
         visable ? (
           <DayCell
             isSelected={date.isSame(selectedDay, "day")}
             onClick={() => dateClicked(date)}
             isDisabled={true}
-            key={`${key}-${date.format(DATE_FORMAT)}`}
+            key={`prev-${date.format(DATE_FORMAT)}`}
           >
             <Day>{date.format("D")}</Day>
           </DayCell>
         ) : (
-          <DayCell isHidden key={`${key}-${date.format(DATE_FORMAT)}`}>
+          <DayCell isHidden key={`prev-${date.format(DATE_FORMAT)}`}>
             <Day></Day>
           </DayCell>
         )
@@ -63,22 +63,21 @@ export const previousMonthDays = (
 };
 
 export const nextMonthDays = (
-  month: moment.Moment,
-  selectedDay: moment.Moment,
-  dateClicked: (date: moment.Moment) => void
+  month: dayjs.Dayjs,
+  selectedDay: dayjs.Dayjs,
+  dateClicked: (date: dayjs.Dayjs) => void
 ): JSX.Element[] => {
   const nextDays: JSX.Element[] = [];
-  const endOfMonth = month.clone().endOf("month");
+  const endOfMonth = month.endOf("month");
   const sundayIndex = 6;
 
   // Check if Month ends with a sunday
   if (endOfMonth.weekday() !== sundayIndex) {
-    const nextSunday = endOfMonth.clone().add(1, "months").startOf("month").weekday(sundayIndex);
+    const nextSunday = endOfMonth.add(1, "months").startOf("month").weekday(sundayIndex);
     const dayDifference = nextSunday.diff(endOfMonth, "days") + 1;
 
     for (let i = 0; i < dayDifference; i++) {
-      const date = moment(endOfMonth);
-      date.add(i + 1, "day");
+      const date = dayjs(endOfMonth).add(i + 1, "day");
       nextDays.push(
         <DayCell
           isSelected={date.isSame(selectedDay, "day")}
