792b4d92fad754bd805120478f6ae0836ab395c3
Skraagen <skraagen@hotmail.com>
2021-02-05 12:40:34 +0100

    Updated hero section

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,8 +1,49 @@
-import { Box, Button, Container, Grid, Paper, Typography } from "@material-ui/core";
+import { Box, Button, Container, Grid, IconButton, Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import MeetingRoomIcon from "@material-ui/icons/MeetingRoom";
 import NavigateNextIcon from "@material-ui/icons/NavigateNext";
-import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+
+const organizations = [
+  {
+    id: 1,
+    title: "Bindeleddet",
+    link: "https://bindeleddet.no/",
+    img: "img/bindeleddet.jpg",
+  },
+  {
+    id: 2,
+    title: "ESTIEM",
+    link: "https://bindeleddet.no/",
+    img: "img/estiem.jpg",
+  },
+  {
+    id: 3,
+    title: "Indøk Kultur",
+    link: "https://bindeleddet.no/",
+    img: "img/indok-kultur.jpg",
+  },
+  {
+    id: 4,
+    title: "Hyttestyret",
+    link: "https://bindeleddet.no/",
+    img: "img/hytte.jpg",
+  },
+  {
+    id: 5,
+    title: "Janus",
+    link: "https://bindeleddet.no/",
+    img: "img/gang.jpg",
+  },
+  {
+    id: 6,
+    title: "Janus IF",
+    link: "https://bindeleddet.no/",
+    img: "img/janus-if.jpg",
+  },
+];
 
 const useStyles = makeStyles(() => ({
   hero: {
@@ -19,44 +60,176 @@ const useStyles = makeStyles(() => ({
     marginTop: -70,
     padding: "32px 48px",
   },
+  relative: {
+    position: "relative",
+    height: "100%",
+  },
+  container: {
+    maxWidth: "90vw",
+    height: "70vh",
+    paddingTop: "21vh",
+  },
+  height: {
+    height: "inherit",
+  },
+  orgLink: {
+    color: "black",
+    position: "relative",
+    marginBottom: 16,
+    marginRight: -15,
+    transition: "0.3s all linear",
+
+    ["&:hover"]: {
+      textDecoration: "none",
+      marginRight: 0,
+    },
+
+    // ["&::before"]: {
+    //   content: "''",
+    //   transition: "1s all ease",
+    // },
+
+    // ["&:hover::before"]: {
+    //   width: "calc(100% + 32px)",
+    //   height: 30,
+    //   content: "''",
+    //   position: "absolute",
+    //   background: "rgb(11 60 60 / 58%)",
+    //   bottom: -2,
+    //   zIndex: -1,
+    //   right: 0,
+    // },
+  },
+  orgBg: {
+    transition: "0.4s all linear",
+    backgroundPosition: "center!important",
+    backgroundSize: "cover!important",
+
+    ["&::before"]: {
+      background: "linear-gradient(to right, rgb(255 255 255 / 40%), rgb(255 255 255 / 88%))",
+      content: "''",
+      display: "block",
+      height: "100%",
+      position: "absolute",
+      width: "100%",
+    },
+  },
+  orgIcon: {
+    marginTop: 9,
+    color: "rgb(0 0 0 / 55%)",
+  },
+  hide: {
+    opacity: "0!important",
+    backgroundPosition: "calc(50% + 20px) center!important",
+  },
+  nth: {
+    transition: "0.7s all ease",
+    background: "url('nth.png')",
+    backgroundSize: "cover",
+    backgroundPosition: "left bottom!important",
+    backgroundRepeat: "no-repeat",
+    opacity: 0.03,
+    zIndex: -2,
+    height: "85vh",
+    marginLeft: "-20vh",
+    display: "none",
+  },
 }));
 
 const Hero: React.FC = () => {
   const classes = useStyles();
+  const [isShown, setIsShown] = useState("");
 
   return (
-    <Box className={classes.hero} height="calc(100vh - 130px)">
-      <Box
-        className={classes.heroImage}
-        style={{
-          backgroundImage: `linear-gradient(to left, rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.95)),
-        url('img/hero.jpg')`,
-        }}
-      ></Box>
-      <Container>
-        <Box display="flex" top="0" alignItems="center" position="absolute" height="90vh" zIndex="4">
-          <Box width={650}>
-            <Typography variant="overline">Foreningen for studentene ved</Typography>
-            <Typography variant="h1">Industriell Økonomi og Teknologiledelse</Typography>
-          </Box>
-        </Box>
-        <Grid justify="center" container>
-          <Grid item xs={10}>
-            <Paper className={classes.heroCard}>
-              <Box display="flex" alignItems="center">
-                <Box flexGrow={1}>
-                  <Typography variant="h5">Les de siste nyhetene fra Hovedstyret</Typography>
-                </Box>
-                <Link href="/about">
-                  <Button variant="contained" endIcon={<NavigateNextIcon />}>
-                    Klikk her
-                  </Button>
-                </Link>
-              </Box>
-            </Paper>
+    <Box id="back-to-top-anchor" height="100vh" position="relative">
+      <Grid container style={{ height: "100%", position: "absolute", zIndex: -1 }}>
+        <Grid item xs={6} className={classes.relative}>
+          <Box position="absolute" width="100%" height="100%" style={{ background: "white" }} zIndex="-1"></Box>
+        </Grid>
+        <Grid item xs={6} className={classes.relative}>
+          {organizations.map((item) => (
+            <Box
+              key={item.id}
+              className={[classes.orgBg, isShown == item.img ? "" : classes.hide].join(" ")}
+              position="absolute"
+              width="100%"
+              height="100%"
+              style={{ backgroundImage: "url(" + item.img + ")" }}
+              zIndex="-1"
+            ></Box>
+          ))}
+          <Box
+            className={[classes.nth, isShown == "" ? "" : classes.hide].join(" ")}
+            position="absolute"
+            width="100%"
+            height="100%"
+            zIndex="-1"
+          ></Box>
+        </Grid>
+      </Grid>
+      <Container className={classes.container}>
+        <Grid container className={classes.height}>
+          <Grid item xs={5}>
+            <Box height="100%" width="100%" display="flex" flexDirection="column" alignItems="flex-start">
+              <Typography variant="overline">Foreningen for studentene ved</Typography>
+              <Typography variant="h3">
+                Industriell Økonomi <br />
+                og Teknologiledelse
+              </Typography>
+              <br />
+              <Button size="large" startIcon={<NavigateNextIcon />}>
+                Les om foreningen
+              </Button>
+              <Button size="large" startIcon={<NavigateNextIcon />}>
+                Arrangementer
+              </Button>
+              <Button size="large" startIcon={<NavigateNextIcon />}>
+                Book hytte
+              </Button>
+              <br />
+              <br />
+              <br />
+              <Typography variant="overline" gutterBottom>
+                Nyttige lenker
+              </Typography>
+              <IconButton>
+                <FacebookIcon />
+              </IconButton>
+            </Box>
+          </Grid>
+          <Grid item xs={7}>
+            <Box height="100%" width="100%" display="flex" flexDirection="column" alignItems="flex-end">
+              <Typography variant="overline">Foreningene under hovedstyret</Typography>
+              {organizations.map((item) => (
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  onMouseEnter={() => setIsShown(item.img)}
+                  onMouseLeave={() => setIsShown("")}
+                  href={item.link}
+                  className={classes.orgLink}
+                  key={item.id}
+                >
+                  <Box display="flex" alignItems="center">
+                    <Typography variant="h2">{item.title}</Typography>
+                    <NavigateNextIcon fontSize="large" className={classes.orgIcon} />
+                  </Box>
+                </a>
+              ))}
+            </Box>
           </Grid>
         </Grid>
       </Container>
+      <Box position="absolute" right="0" top="0" m="24px 24px 0 0">
+        <Button color="primary" size="medium" variant="contained" endIcon={<MeetingRoomIcon />}>
+          Logg inn
+        </Button>
+      </Box>
+      <Box position="absolute" left="0" top="0" m="24px 0 0 24px">
+        <Button color="primary" size="medium" variant="contained" startIcon={<MailOutlineIcon />}>
+          Kontakt
+        </Button>
+      </Box>
     </Box>
   );
 };
