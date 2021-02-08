cab2f0e724f60e08c4e031fff374462be53441de
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-02-08 19:33:01 +0100

    :sparkles: add absolute slug field

--- backend/api/graphql/organizations/types.py
+++ backend/api/graphql/organizations/types.py
@@ -1,12 +1,24 @@
+import graphene
 from graphene_django import DjangoObjectType
 
 from apps.organizations.models import Organization, Membership, Role
 
 
 class OrganizationType(DjangoObjectType):
+    absolute_slug = graphene.String()
+
     class Meta:
         model = Organization
         fields = ["id", "name", "slug", "color", "description", "parent", "children"]
+    
+    @staticmethod
+    def resolve_absolute_slug(organization: Organization, info):
+        slug_list = [organization.slug]
+        while (organization := organization.parent) and organization.parent != organization:
+            print(slug_list)
+            slug_list.insert(0, organization.slug)
+        return "/".join(slug_list)
+
 
 
 class MembershipType(DjangoObjectType):
