32799d6194275c5a0d301aeb9fca87ecc29e6f17
Anna <annasofie.lunde@gmail.com>
2021-02-22 19:52:26 +0100

    Update year filter for non-year filterable document types

--- backend/api/graphql/archive/resolvers.py
+++ backend/api/graphql/archive/resolvers.py
@@ -25,9 +25,15 @@ class ArchiveDocumentResolvers:
 
     @login_required
     def resolve_archive_by_types(self, info, type_doc, year=None):
+        nonFilter = ["Foreningens lover", "Utveksling", "Annet", "Støtte fra HS"]
         documents = ArchiveDocument.objects.all()
+        filter_docs_year = []
         if type_doc:
             documents = documents.filter(type_doc__in=type_doc)
         if year is not None:
-            documents = documents.filter(year=year)
+            for doc in documents:
+                if doc.type_doc not in nonFilter and doc.year != year:
+                    filter_docs_year.append(doc.id)
+        if filter_docs_year:
+            documents = documents.exclude(id__in=filter_docs_year)
         return documents
--- frontend/src/components/pages/archive/yearSelector.tsx
+++ frontend/src/components/pages/archive/yearSelector.tsx
@@ -82,8 +82,8 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
 interface YearSelectorProps {
-  yearFilter: string;
-  updateYearFilters: (value: string) => void;
+  yearFilter: number | null;
+  updateYearFilters: (value: number | null) => void;
 }
 
 const YearSelector: React.FC<YearSelectorProps> = ({ yearFilter, updateYearFilters }) => {
@@ -105,7 +105,7 @@ const YearSelector: React.FC<YearSelectorProps> = ({ yearFilter, updateYearFilte
               icon: classes.icon,
             }}
             value={yearFilter}
-            onChange={(event) => updateYearFilters(event.target.value)}
+            onChange={(event) => updateYearFilters(parseInt(event.target.value))}
             name="Årstall"
             inputProps={{
               id: "gpuChildQuantity",
@@ -116,6 +116,15 @@ const YearSelector: React.FC<YearSelectorProps> = ({ yearFilter, updateYearFilte
             <option value="2020">2020</option>
             <option value="2019">2019</option>
             <option value="2018">2018</option>
+            <option value="2017">2017</option>
+            <option value="2016">2016</option>
+            <option value="2015">2015</option>
+            <option value="2014">2014</option>
+            <option value="2013">2013</option>
+            <option value="2012">2012</option>
+            <option value="2011">2011</option>
+            <option value="2010">2010</option>
+            <option value="2009">2009</option>
           </NativeSelect>
         </FormControl>
       </MuiThemeProvider>
