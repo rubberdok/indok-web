2477b5a6b2bb534c77a491cbd1d3c2826118f20e
Sverre Spetalen <sverress@stud.ntnu.no>
2021-03-01 20:28:04 +0100

    disable unavailable dates in date picker

--- frontend/src/components/pages/cabins/CabinDatePicker/index.tsx
+++ frontend/src/components/pages/cabins/CabinDatePicker/index.tsx
@@ -3,7 +3,8 @@ import useDisabledDates from "@hooks/cabins/useDisabledDates";
 import { Cabin } from "@interfaces/cabins";
 import { Grid, Typography, Divider, makeStyles } from "@material-ui/core";
 import { NextPage } from "next";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import dayjs from "dayjs";
 
 const useStyles = makeStyles({
   header: {
@@ -15,47 +16,64 @@ interface Props {
   chosenCabins: Cabin[];
 }
 
+interface DatePick {
+  checkInDate?: string;
+  checkOutDate?: string;
+}
+
 const CabinDatePicker: NextPage<Props> = ({ chosenCabins }) => {
   const classes = useStyles();
-  const [checkInDate, setCheckInDate] = useState<string>();
-  const [checkOutDate, setCheckOutDate] = useState<string>();
-  const { disabledDates: checkInDisabledDates, setDisabledDates: setCheckInDisabledDates } = useDisabledDates(
-    chosenCabins
-  );
-  const { disabledDates: checkOutDisabledDates, setDisabledDates: setCheckOutDisabledDates } = useDisabledDates(
-    chosenCabins
-  );
-  const getHeader = (header: string, isFrom: boolean) => (
+  const [datePick, setDatePick] = useState<DatePick>({});
+  const { disabledDates } = useDisabledDates(chosenCabins);
+  const [disableAfter, setDisableAfter] = useState<string>();
+  const getHeader = (header: string, prefix: string) => (
     <Grid item>
       <Typography className={classes.header} variant="h5">
         {header}
       </Typography>
       <Typography className={classes.header} variant="body1">
-        {getSubheader(isFrom)}
+        {getSubheader(prefix)}
       </Typography>
       <Divider />
     </Grid>
   );
 
-  const getSubheader = (isFrom: boolean) => {
+  useEffect(() => {
+    if (datePick.checkInDate) {
+      // Find next booking and diable all dates after that booking starts
+      const startDate = dayjs(datePick.checkInDate);
+      setDisableAfter(disabledDates.find((date: string) => startDate.isBefore(dayjs(date))));
+    }
+  }, [datePick]);
+
+  const getSubheader = (prefix: string) => {
     if (chosenCabins.length === 1) {
-      return ` ${isFrom ? "fra" : "til"} ${chosenCabins[0].name}`;
+      return ` ${prefix} ${chosenCabins[0].name}`;
     } else {
-      return ` ${isFrom ? "fra" : "til"} ${chosenCabins[0].name} og ${chosenCabins[1].name}`;
+      return ` ${prefix} ${chosenCabins[0].name} og ${chosenCabins[1].name}`;
     }
   };
   return (
     <Grid container spacing={5}>
       <Grid item xs container direction="column" spacing={3}>
-        {getHeader("Innsjekk", false)}
+        {getHeader("Innsjekk", "til")}
         <Grid item xs>
-          <Calendar disabledDates={checkInDisabledDates} handleDateClicked={(date) => setCheckInDate(date)} />
+          <Calendar
+            disabledDates={disabledDates}
+            handleDateClicked={(date) => setDatePick((prev: DatePick) => ({ ...prev, checkInDate: date }))}
+            disableAfter={datePick.checkOutDate}
+          />
         </Grid>
       </Grid>
       <Grid item xs container direction="column" spacing={3}>
-        {getHeader("Utsjekk", true)}
+        {getHeader("Utsjekk", "fra")}
         <Grid item xs>
-          <Calendar disabledDates={checkOutDisabledDates} handleDateClicked={(date) => setCheckOutDate(date)} />
+          <Calendar
+            disabledDates={disabledDates}
+            handleDateClicked={(date) => setDatePick((prev: DatePick) => ({ ...prev, checkOutDate: date }))}
+            disableBefore={datePick.checkInDate}
+            disableAfter={disableAfter}
+          />
         </Grid>
       </Grid>
     </Grid>
