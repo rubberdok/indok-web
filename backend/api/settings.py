470b28fcaf63b5d42f1267bb6765769f5409724b
ankile <lars.ankile@gmail.com>
2021-03-11 17:00:37 +0100

    Add whitenoise to handle static files

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -72,6 +72,7 @@ MIDDLEWARE = [
     "django_alive.middleware.healthcheck_bypass_host_check",
     "api.auth.middleware.IndokWebJWTMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -158,7 +159,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = ["backend/static/cabins"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # CONFIG
 EMAIL_HOST_USER = env("BOOKING_EMAIL")
