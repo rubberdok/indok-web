bd010705b9bec9426285b5185e5c2acd7eb4e80b
hermannm <hermann.morkrid@gmail.com>
2021-03-11 19:44:32 +0100

    style surveyAnswers

--- frontend/src/components/pages/surveys/surveyAdmin/surveyAnswers.tsx
+++ frontend/src/components/pages/surveys/surveyAdmin/surveyAnswers.tsx
@@ -2,6 +2,7 @@ import { User } from "@interfaces/users";
 import { SURVEY_ANSWERS } from "@graphql/surveys/queries";
 import { useQuery } from "@apollo/client";
 import { Question, Answer } from "@interfaces/surveys";
+import { Typography } from "@material-ui/core";
 
 type QuestionWithAnswer = Question & {
   answer: Answer;
@@ -11,20 +12,32 @@ const SurveyAnswers: React.FC<{
   surveyId: number;
   user: User;
 }> = ({ surveyId, user }) => {
-  const { loading, error, data } = useQuery<{ survey: { questions: QuestionWithAnswer[] } }>(SURVEY_ANSWERS, {
-    variables: { surveyId: surveyId, userId: parseInt(user.id) },
-  });
+  const { loading, error, data } = useQuery<{ survey: { descriptiveName: string; questions: QuestionWithAnswer[] } }>(
+    SURVEY_ANSWERS,
+    {
+      variables: { surveyId: surveyId, userId: parseInt(user.id) },
+    }
+  );
   if (error) return <p>Error...</p>;
   if (loading) return <p>Loading...</p>;
   return (
     <>
-      {data &&
-        data.survey.questions.map((question) => (
-          <>
-            <p>{question.question}</p>
-            <p>{question.answer.answer}</p>
-          </>
-        ))}
+      {data && (
+        <>
+          <Typography variant="h3">{data.survey.descriptiveName}</Typography>
+          <Typography>
+            <b>Søker:</b> {user.firstName} {user.lastName}
+          </Typography>
+          {data.survey.questions.map((question) => (
+            <>
+              <Typography>
+                <b>{question.question}</b>
+              </Typography>
+              <Typography>{question.answer.answer}</Typography>
+            </>
+          ))}
+        </>
+      )}
     </>
   );
 };
