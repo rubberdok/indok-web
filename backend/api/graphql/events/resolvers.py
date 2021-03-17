b74dd858f9131626c025fcd2b14aeb58c1636505
Morgan Heggland <morganheggland@gmail.com>
2021-03-17 12:02:23 +0100

    run black and update default fields

--- backend/api/graphql/events/resolvers.py
+++ backend/api/graphql/events/resolvers.py
@@ -2,8 +2,8 @@ from datetime import date
 
 from apps.events.models import Category, Event
 from apps.organizations.models import Organization
-from apps.users.models import User
 from django.db.models import Q
+from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from datetime import date
 import pandas as pd
@@ -14,16 +14,24 @@ from collections import namedtuple
 import base64
 
 
-DEFAULT_REPORT_FIELDS = ['event_id', 'event_title', 'user_id', 'user_username', 'user_first_name', 'user_last_name', 'user_email', 'user_year']
+DEFAULT_REPORT_FIELDS = [
+    "event_title",
+    "user_last_name",
+    "user_first_name",
+    "user_phone_number",
+    "user_email",
+    "user_year",
+    "user_allergies",
+]
 
-FiletypeSpec = namedtuple('Filetype', ['content_type', 'extension'])
+FiletypeSpec = namedtuple("Filetype", ["content_type", "extension"])
 filetype_specs = {
-    "xlsx": FiletypeSpec(content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                         extension="xlsx"),
-    "csv": FiletypeSpec(content_type="text/csv",
-                         extension="csv"),
-    "html": FiletypeSpec(content_type="text/html",
-                         extension="html"),
+    "xlsx": FiletypeSpec(
+        content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        extension="xlsx",
+    ),
+    "csv": FiletypeSpec(content_type="text/csv", extension="csv"),
+    "html": FiletypeSpec(content_type="text/html", extension="html"),
 }
 
 
@@ -99,21 +107,22 @@ class EventResolvers:
         except Category.DoesNotExist:
             return None
 
-    def resolve_attendee_report(parent, info, event_id, fields=None, filetype='xlsx'):
+    def resolve_attendee_report(self, info, event_id, fields=None, filetype="xlsx"):
         df = create_attendee_report([event_id], fields)
         file_basename = f"attendee_report__eventid_{event_id}"
         return wrap_attendee_report_as_json(df, file_basename, filetype)
 
-    def resolve_attendee_reports(parent, info, event_ids, fields=None, filetype='xlsx'):
+    def resolve_attendee_reports(self, info, event_ids, fields=None, filetype="xlsx"):
         df = create_attendee_report(event_ids, fields)
-        file_basename = f"attendee_report__eventid_{'|'.join(str(id_) for id_ in event_ids)}"
+        file_basename = (
+            f"attendee_report__eventid_{'|'.join(str(id_) for id_ in event_ids)}"
+        )
         return wrap_attendee_report_as_json(df, file_basename, filetype)
 
-    def resolve_attendee_report_org(parent, info, org_id, fields=None, filetype='xlsx'):
-        event_ids = Organization.objects \
-                                .get(id=org_id) \
-                                .event_set \
-                                .values_list("id", flat=True)
+    def resolve_attendee_report_org(self, info, org_id, fields=None, filetype="xlsx"):
+        event_ids = Organization.objects.get(id=org_id).event_set.values_list(
+            "id", flat=True
+        )
         df = create_attendee_report(event_ids, fields)
         file_basename = f"attendee_report__orgid_{org_id}"
         return wrap_attendee_report_as_json(df, file_basename, filetype)
@@ -121,35 +130,45 @@ class EventResolvers:
 
 def create_attendee_report(event_ids, fields):
     fields = fields if fields is not None else DEFAULT_REPORT_FIELDS
-    user_ids = Event.objects.filter(id__in=event_ids) \
-                            .values_list("signed_up_users__id", flat=True)
+    user_ids = Event.objects.filter(id__in=event_ids).values_list(
+        "signed_up_users__id", flat=True
+    )
 
     # Fetch data
-    df_events = pd.DataFrame(Event.objects.filter(id__in=event_ids).values()) \
-                            .set_index('id') \
-                            .add_prefix('event_')
-    df_users = pd.DataFrame(User.objects.filter(id__in=user_ids).values()) \
-                            .set_index('id') \
-                            .add_prefix('user_')
+    df_events = (
+        pd.DataFrame(Event.objects.filter(id__in=event_ids).values())
+        .set_index("id")
+        .add_prefix("event_")
+    )
+    df_users = (
+        pd.DataFrame(get_user_model().objects.filter(id__in=user_ids).values())
+        .set_index("id")
+        .add_prefix("user_")
+    )
     df_events_users = pd.DataFrame(Event.signed_up_users.through.objects.all().values())
-    df_joined = df_events_users.join(df_events, on='event_id', how='inner') \
-                               .join(df_users, on='user_id', how='inner') \
-                               .sort_values(["event_id", "user_id"])
+    df_joined = (
+        df_events_users.join(df_events, on="event_id", how="inner")
+        .join(df_users, on="user_id", how="inner")
+        .sort_values(["event_id", "user_id"])
+    )
 
     # Return empty dataframe, lookups on an empty frame will raise an exception
     if df_joined.empty:
         return pd.DataFrame()
-    return df_joined.loc[:, DEFAULT_REPORT_FIELDS] \
-                    .drop('password', errors='ignore', axis=1) \
-                    .loc[:, fields]
-    
+    return (
+        df_joined.loc[:, DEFAULT_REPORT_FIELDS]
+        .drop("password", errors="ignore", axis=1)
+        .loc[:, fields]
+    )
 
 
 def wrap_attendee_report_as_json(df, file_basename, filetype):
     # Handle different content types
     if filetype == "xlsx":
         buffer = io.BytesIO()
-        with pd.ExcelWriter(buffer, engine='xlsxwriter', options={'remove_timezone': True}) as writer:
+        with pd.ExcelWriter(
+            buffer, engine="xlsxwriter", options={"remove_timezone": True}
+        ) as writer:
             df.to_excel(writer, index=False)
         data = base64.b64encode(buffer.getvalue()).decode("utf-8")
     elif filetype == "csv":
@@ -158,11 +177,11 @@ def wrap_attendee_report_as_json(df, file_basename, filetype):
         data = df.to_html(index=False)
     else:
         raise ValueError(f"Filetype not supported: '{filetype}'")
-    
+
     spec = filetype_specs[filetype]
     response = {
         "data": data,
         "filename": f"{file_basename}.{spec.extension}",
-        "contentType": spec.content_type
+        "contentType": spec.content_type,
     }
     return json.dumps(response)
