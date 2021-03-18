26dc3a477de74c11ab8476a52898d98f87c653ac
ankile <lars.ankile@gmail.com>
2021-03-18 15:09:57 +0100

    Update links in backend and frontend

--- backend/api/settings_prod.py
+++ backend/api/settings_prod.py
@@ -9,7 +9,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ["api.gamma.indokntnu.no", "api.indokntnu.no"]
 
-CORS_ORIGIN_WHITELIST = ["https://gamma.indokntnu.no"]
+CORS_ORIGIN_WHITELIST = ["https://gamma.indokntnu.no", "https://indokntnu.no"]
 
 DATABASES = {
     "default": {
