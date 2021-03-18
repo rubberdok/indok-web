9623a38ebba073e1fd850834944dac6f2b757c20
hermannm <hermann.morkrid@gmail.com>
2021-03-18 21:15:22 +0100

    add styling to listing admin page

--- frontend/src/pages/orgs/[orgId]/listings/[listingId].tsx
+++ frontend/src/pages/orgs/[orgId]/listings/[listingId].tsx
@@ -5,15 +5,24 @@ import { Listing } from "@interfaces/listings";
 import { LISTING_WITH_RESPONDERS } from "@graphql/listings/queries";
 import Link from "next/link";
 import Layout from "@components/Layout";
-import { Grid, Box, Tabs, Tab } from "@material-ui/core";
+import { makeStyles, Grid, Box, Tabs, Tab, Container, Card, CardContent } from "@material-ui/core";
 import { useState } from "react";
 import { User } from "@interfaces/users";
 import SurveyAnswers from "@components/pages/surveys/surveyAdmin/surveyAnswers";
 import OrganizationListing from "@components/pages/listings/organization/organizationListing";
 
+const useStyles = makeStyles((theme) => ({
+  container: {
+    padding: theme.spacing(4),
+    paddingBottom: theme.spacing(2),
+  },
+}));
+
 // the page for organization admins to administer a listing, edit its survey, and review applicants
 const ListingAdminPage: NextPage = () => {
   const { orgId, listingId } = useRouter().query;
+
+  const classes = useStyles();
 
   // fetches the listing along with all users who have applied to it
   const { loading, error, data } = useQuery<{ listing: Listing }>(LISTING_WITH_RESPONDERS, {
@@ -29,35 +38,45 @@ const ListingAdminPage: NextPage = () => {
   if (loading) return <p>Loading...</p>;
   return (
     <Layout>
-      <Link href={`/orgs/${orgId}/listings`}>Tilbake</Link>
-      {data && (
-        <Grid container direction="row">
-          <Tabs
-            orientation="vertical"
-            variant="scrollable"
-            value={selectedApplicant}
-            onChange={(_, applicant) => {
-              selectApplicant(applicant);
-            }}
-          >
-            <Tab value={null} label="Se verv og søknad" />
-            {data.listing.survey?.responders.map((responder, index) => (
-              <Tab key={index} value={responder} label={`${responder.firstName} ${responder.lastName}`} />
-            ))}
-          </Tabs>
-          <Box marginLeft={3}>
-            {selectedApplicant ? (
-              <>
-                {data.listing.survey && (
-                  <SurveyAnswers user={selectedApplicant} surveyId={parseInt(data.listing.survey.id)} />
+      <Container className={classes.container}>
+        <Card>
+          <CardContent>
+            <Link href={`/orgs/${orgId}/listings`}>Tilbake</Link>
+          </CardContent>
+        </Card>
+        {data && (
+          <Grid container direction="row">
+            <Card>
+              <Tabs
+                orientation="vertical"
+                variant="scrollable"
+                value={selectedApplicant}
+                onChange={(_, applicant) => {
+                  selectApplicant(applicant);
+                }}
+              >
+                <Tab value={null} label="Se verv og søknad" />
+                {data.listing.survey?.responders.map((responder, index) => (
+                  <Tab key={index} value={responder} label={`${responder.firstName} ${responder.lastName}`} />
+                ))}
+              </Tabs>
+            </Card>
+            <Card>
+              <CardContent>
+                {selectedApplicant ? (
+                  <>
+                    {data.listing.survey && (
+                      <SurveyAnswers user={selectedApplicant} surveyId={parseInt(data.listing.survey.id)} />
+                    )}
+                  </>
+                ) : (
+                  <OrganizationListing listing={data.listing} />
                 )}
-              </>
-            ) : (
-              <OrganizationListing listing={data.listing} />
-            )}
-          </Box>
-        </Grid>
-      )}
+              </CardContent>
+            </Card>
+          </Grid>
+        )}
+      </Container>
     </Layout>
   );
 };
