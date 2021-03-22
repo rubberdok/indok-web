82d1c16ae31c23535a48989e5973dad5b7b531ca
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-03-22 19:09:38 +0100

    Return tz-aware datetime

--- backend/api/graphql/utils/time/resolvers.py
+++ backend/api/graphql/utils/time/resolvers.py
@@ -1,5 +1,5 @@
-from datetime import datetime
+from django.utils import timezone
 
 class TimeResolvers:
   def resolve_server_time(self, info):
-    return datetime.now()
+    return timezone.now()
