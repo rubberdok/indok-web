d066f9ca134b8ed4e7c6daadc80e5642a4ef753e
Morgan Heggland <morganheggland@gmail.com>
2021-03-25 20:33:50 +0100

    make guardian anonymous user work with graphql-jwt

--- backend/api/auth/anonymous_user_graphene_middleware.py
+++ backend/api/auth/anonymous_user_graphene_middleware.py
@@ -2,19 +2,6 @@ from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth import get_user_model
 
 
-class IndokWebJWTMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        response = self.get_response(request)
-        jwt_token = getattr(request, "set_jwt_cookie", None)
-        if jwt_token:
-            response.set_cookie("JWT", jwt_token, max_age=24 * 60 * 60, httponly=True)
-
-        return response
-
-
 class AnonymousUserMiddleware(object):
     def resolve(self, next, root, info, **args):
         if isinstance(info.context.user, AnonymousUser):
