b1bf38f625a3f6bb8c2104ad16c679efeb23f4af
Sverre Spetalen <sverress@stud.ntnu.no>
2021-03-27 11:38:04 +0100

    started on all resolvers tests

--- backend/apps/cabins/tests.py
+++ backend/apps/cabins/tests.py
@@ -1,3 +1,38 @@
-from django.test import TestCase
+import json
 
-# Create your tests here.
+from graphene_django.utils.testing import GraphQLTestCase
+
+
+class CabinsResolversTestCase(GraphQLTestCase):
+    """
+    Testing all resolvers for cabins
+    """
+    def test_resolve_all_bookings(self):
+        response = self.query(
+            '''
+            query {
+                allBookings {
+                    id
+                    firstName
+                    surName
+                    phone
+                    receiverEmail
+                    checkIn
+                    checkOut
+                    price
+                    cabins {
+                        id
+                        name
+                    }
+                    timestamp
+                }
+            }
+            ''',
+            op_name='allBookings'
+        )
+
+        content = json.loads(response.content)
+
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
+
