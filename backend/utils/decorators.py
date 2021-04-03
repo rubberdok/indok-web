819b40326ac26c9090410fc623e298122f540b24
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-04-03 23:43:49 +0200

    :sparkles: feat: add object getter, optional return None if no perm

--- backend/utils/decorators.py
+++ backend/utils/decorators.py
@@ -10,8 +10,8 @@ from graphql_jwt.decorators import context
 def permission_required(
     perms: Union[list[str], str],
     lookup_variables: tuple[Union[Model, ModelBase, str], ...] = None,
-    obj_arg_pos: int = None,
     fn: Callable = None,
+    return_none: bool = False,
     **kwargs,
 ):
     """Decorator to check for row level permissions
@@ -22,14 +22,10 @@ def permission_required(
         A permission or list of permissions in the format app_label.action_modelname.
     lookup_variables : tuple[Union[Model, ModelBase, str], ...], optional
         A tuple with: (1) the model which to look up, (2) a series of attribute name, parameter name pairs to use for the lookup, by default None
-    obj_arg_post : int, optional
-        The argument position of the object to use for checking object permissions. For example:
-        
-        @permission_required("app.some_permission", obj_arg_pos=0)
-        def resolve_field(obj, info):
-            ...
-
-        Here, we will check if user.has_perm("app.some_permission", obj) where user is the currently logged in user.
+    fn : Callable, optional
+        A callable used to retrieve the object for which to check if the user has object permissions.
+    return_none : bool, optional, default = False
+        If the user does not have the required permissions, return None instead of raising an error.
     accept_global_perms : bool
         Whether global permissions are permitted, i.e. does not require object-level permission, by default False
 
@@ -56,8 +52,9 @@ def permission_required(
         @context(resolver)
         def wrapper(context, *args, **kwargs):
             obj = None
-            if fn:
+            if fn and isinstance(fn, Callable):
                 obj = fn(context, *args, **kwargs)
+
             elif lookup_variables:
                 model, lookups = lookup_variables[0], lookup_variables[1:]
 
@@ -90,10 +87,7 @@ def permission_required(
                     lookup_dict[lookup] = kwargs[resolver_arg]
 
                 obj = model.objects.get(**lookup_dict)
-            elif obj_arg_pos is not None:
-                if isinstance(args[obj_arg_pos], Model):
-                    obj = args[obj_arg_pos]
-
+            
             if has_permissions(
                 user=context.user,
                 perms=perms,
@@ -101,6 +95,8 @@ def permission_required(
                 accept_global_perms=accept_global_perms,
             ):
                 return resolver(*args, **kwargs)
+            elif return_none:
+                return None
 
             raise PermissionError("You do not have the permissions required.")
 
@@ -133,3 +129,43 @@ def has_permissions(
     return (accept_global_perms and all(user.has_perm(perm) for perm in perms)) or all(
         user.has_perm(perm, obj) for perm in perms
     )
+
+def permission_required_or_return_none(
+    perms: Union[list[str], str],
+    lookup_variables: tuple[Union[Model, ModelBase, str], ...] = None,
+    fn: Callable = None,
+    **kwargs,
+):
+    """Wrapper for permission required with return_none = True
+    Checks if the user has the required permissions for a given object, returns None if not.
+
+    Parameters
+    ----------
+    perms : Union[list[str], str]
+        [description]
+    lookup_variables : tuple[Union[Model, ModelBase, str], ...], optional
+        [description], by default None
+    fn : Callable, optional
+        [description], by default None
+
+    Returns
+    -------
+    None
+        If the user does not have the given permission, wrapped function otherwise.
+    """
+    return permission_required(perms=perms, lookup_variables=lookup_variables, fn=fn, return_none=True, **kwargs)
+
+def get_resolver_parent(context, *args, **kwargs):
+    """Default objectgetter for field resolvers
+
+    Parameters
+    ----------
+    context
+        The GraphQL context
+
+    Returns
+    -------
+    Model
+        The root resolver object, i.e. the object type to which we wish to resolve the field.
+    """
+    return args[0]
