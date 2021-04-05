88a1f959c6c7733ff6459d173096dad6c4dd86db
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-04-05 23:10:37 +0200

    :sparkles: feat: add paragraph spacing to markdown

--- frontend/src/components/pages/listings/markdown/renderer.tsx
+++ frontend/src/components/pages/listings/markdown/renderer.tsx
@@ -4,7 +4,7 @@ import { Variant } from "@material-ui/core/styles/createTypography";
 
 const renderers: {[nodeType: string]: ElementType} = {
   heading: (props) => (<Typography variant={`h${props.level}` as Variant}>{props.children}</Typography>),
-  paragraph: (props) => (<Typography variant="body2" component="span">{props.children}</Typography>),
+  paragraph: (props) => (<Typography variant="body2" component="p" paragraph>{props.children}</Typography>),
 };
 
 export default renderers;
