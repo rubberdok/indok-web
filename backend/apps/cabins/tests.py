80cbd38cc40a09cb7a9b2afb86848b1b1ea725f9
Sverre Spetalen <sverress@stud.ntnu.no>
2021-04-05 20:28:59 +0200

    simple create booking test

--- backend/apps/cabins/tests.py
+++ backend/apps/cabins/tests.py
@@ -1,5 +1,6 @@
 import json
 
+from apps.cabins.models import Booking
 from utils.testing.ExtendedGraphQLTestCase import ExtendedGraphQLTestCase
 from utils.testing.cabins_factories import BookingFactory
 import datetime
@@ -9,15 +10,17 @@ class CabinsBaseTestCase(ExtendedGraphQLTestCase):
     def setUp(self) -> None:
         super().setUp()
         # Create three bookings
-        now = datetime.datetime.now()
-        self.firstBooking = BookingFactory(check_in=now, check_out=now + datetime.timedelta(days=4))
+        self.now = datetime.datetime.now()
+        self.firstBooking = BookingFactory(
+            check_in=self.now, check_out=self.now + datetime.timedelta(days=4)
+        )
         self.secondBooking = BookingFactory(
-            check_in=now + datetime.timedelta(days=6),
-            check_out=now + datetime.timedelta(days=12)
+            check_in=self.now + datetime.timedelta(days=6),
+            check_out=self.now + datetime.timedelta(days=12),
         )
         self.thirdBooking = BookingFactory(
-            check_in=now + datetime.timedelta(days=24),
-            check_out=now + datetime.timedelta(days=30)
+            check_in=self.now + datetime.timedelta(days=24),
+            check_out=self.now + datetime.timedelta(days=30),
         )
 
 
@@ -25,9 +28,10 @@ class CabinsResolversTestCase(CabinsBaseTestCase):
     """
     Testing all resolvers for cabins
     """
+
     def test_resolve_all_bookings(self):
         response = self.query(
-            '''
+            """
             query {
                 allBookings {
                     id
@@ -45,19 +49,19 @@ class CabinsResolversTestCase(CabinsBaseTestCase):
                     timestamp
                 }
             }
-            ''',
+            """,
         )
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
         # Fetching content of response
         content = json.loads(response.content)
 
         # There are three bookings in the database
         self.assertEqual(len(content["data"]["allBookings"]), 3)
-        # This validates the status code and if you get errors
-        self.assertResponseNoErrors(response)
 
     def test_resolve_cabins(self):
         response = self.query(
-            '''
+            """
             query AllCabins {
                 cabins {
                   id
@@ -67,12 +71,54 @@ class CabinsResolversTestCase(CabinsBaseTestCase):
                   externalPrice
                 }
               }
-            ''',
+            """,
         )
+        # This validates the status code and if you get errors
+        self.assertResponseNoErrors(response)
         # Fetching content of response
         content = json.loads(response.content)
 
         # There are two cabins in the database
         self.assertEqual(len(content["data"]["cabins"]), 2)
+
+
+class CabinsMutationsTestCase(CabinsBaseTestCase):
+    """
+    Testing all mutations for cabins
+    """
+
+    def test_create_booking(self):
+        new_fake_booking = BookingFactory.build(
+            check_in=self.now + datetime.timedelta(days=50),
+            check_out=self.now + datetime.timedelta(days=52),
+        )
+        query = f"""
+            mutation CreateBooking {{
+                createBooking(
+                  firstname: \"{new_fake_booking.firstname}\",
+                  surname: \"{new_fake_booking.surname}\",
+                  phone: {new_fake_booking.phone},
+                  receiverEmail: \"{new_fake_booking.receiver_email}\",
+                  checkIn: \"{new_fake_booking.check_in.strftime("%Y-%m-%d")}\",
+                  checkOut: \"{new_fake_booking.check_out.strftime("%Y-%m-%d")}\",
+                  price: {new_fake_booking.price},
+                  cabins: [1],
+                ) {{
+                  ok
+                }}
+                }}
+            """
+        response = self.query(query)
+
         # This validates the status code and if you get errors
         self.assertResponseNoErrors(response)
+
+        # Check that booking is created
+        self.assertEqual(
+            1,
+            len(
+                Booking.objects.filter(
+                    firstname=new_fake_booking.firstname, phone=new_fake_booking.phone
+                )
+            ),
+        )
