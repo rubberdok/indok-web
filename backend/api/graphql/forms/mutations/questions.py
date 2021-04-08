b5dfc0c01e46ebce426968788154f595766edd68
hermannm <hermann.morkrid@gmail.com>
2021-04-08 18:40:40 +0200

    add mandatory argument to question mutation inputs backend

--- backend/api/graphql/forms/mutations/questions.py
+++ backend/api/graphql/forms/mutations/questions.py
@@ -21,11 +21,13 @@ class BaseQuestionInput(graphene.InputObjectType):
     question_type = graphene.Field(QuestionTypeEnum)
     question = graphene.String()
     description = graphene.String()
+    mandatory = graphene.Boolean()
 
 
 class CreateQuestionInput(BaseQuestionInput):
     form_id = graphene.ID(required=True)
     question = graphene.String(required=True)
+    mandatory = graphene.Boolean(required=True)
 
 
 class CreateQuestion(graphene.Mutation):
