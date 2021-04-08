a9e96073b716e3ac44813972c05897fbd0cebf09
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-04-08 22:11:10 +0200

    :lipstick: style: refactor and add informative loading

---
+++ frontend/src/components/pages/listings/index/Listings.tsx
@@ -0,0 +1,60 @@
+import ListingItem from "@components/pages/listings/index/ListingItem";
+import { LISTINGS } from "@graphql/listings/queries";
+import { Listing } from "@interfaces/listings";
+import { useQuery } from "@apollo/client";
+import { Grid, Typography, makeStyles, CircularProgress, Button } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(4),
+    marginBottom: theme.spacing(4),
+  },
+}));
+
+interface ListingsProps {
+  handleClick: () => void,
+}
+
+const Listings: React.FC<ListingsProps> = ({ handleClick }) => {
+  const { loading, error, data } = useQuery<{ listings: Listing[] }>(LISTINGS);
+  const classes = useStyles();
+
+
+  return (
+    <Grid container direction="row" spacing={2} className={classes.root} justify="center" alignItems="stretch">
+      {loading &&
+        <Grid item>
+          <CircularProgress />
+        </Grid>
+      }
+      {error &&
+        <Grid container item direction="column" alignItems="center">
+          <Grid item>
+            <Typography variant="body1" align="center">
+              Noe gikk galt da vi forsøkte å laste inn vervene.
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography variant="caption" align="center">
+              Skjer dette ofte?
+              Ta kontakt på med <a href="mailto:web@indokhs.no">Indøks Webkomité</a>
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" onClick={handleClick}>Prøv på nytt</Button>        
+          </Grid>
+        </Grid>
+        
+      } 
+      {data && (
+        data.listings.map((listing) => (
+          <Grid container item key={listing.id} md={5} sm={7} xs={10}>
+            <ListingItem listing={listing} />
+          </Grid>
+        ))
+      )}
+    </Grid>
+  )
+}
+
+export default Listings;
--- frontend/src/pages/listings/index.tsx
+++ frontend/src/pages/listings/index.tsx
@@ -1,39 +1,19 @@
-import { useQuery } from "@apollo/client";
-import Layout from "@components/Layout";
-import ListingItem from "@components/pages/listings/ListingItem";
-import { LISTINGS } from "@graphql/listings/queries";
-import { Listing } from "@interfaces/listings";
-import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
-import { NextPage } from "next";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    marginTop: theme.spacing(4),
-    marginBottom: theme.spacing(4),
-  },
-  hero: {
-    color: "white",
-    height: "30vh",
-    width: "100%",
-    backgroundColor: "black",
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    backgroundImage: `linear-gradient(to left, rgba(0, 0, 0, 0.0), rgba(0, 0, 0, 0.8)), url('img/bindeleddet.jpg')`,
-    marginBottom: 20,
-  },
-}));
+import Layout from "@components/Layout";
+import Listings from "@components/pages/listings/index/Listings";
+
+import { Container, Typography } from "@material-ui/core";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+
+
 
 // page to show all listings open to users
 const ListingsPage: NextPage = () => {
-  // fetches all listings
-  const { loading, error, data } = useQuery<{ listings: Listing[] }>(LISTINGS);
-
-  const classes = useStyles();
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error</p>;
-
+  const router = useRouter();
+  const reload = () => {
+    return router.reload();
+  }
   // renders a ListingItem for each listing
   return (
     <Layout>
@@ -41,16 +21,7 @@ const ListingsPage: NextPage = () => {
         <Typography variant="h1" component="h1" align="center">
           Verv
         </Typography>
-        <Grid container direction="row" spacing={2} className={classes.root} justify="center" alignItems="stretch">
-          {data &&
-            data.listings.map((listing) => (
-              <>
-                <Grid container item key={listing.id} md={5} sm={7} xs={10}>
-                  <ListingItem listing={listing} />
-                </Grid>
-              </>
-            ))}
-        </Grid>
+        <Listings handleClick={reload} />
       </Container>
     </Layout>
   );
