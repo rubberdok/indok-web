f42fb10e844ad6dfe05a9e0e0d82cf67fab76d7d
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-04-13 00:14:09 +0200

    :recycle: refactor: split listing detail for markdown editor

---
+++ frontend/src/components/pages/listings/detail/TitleCard.tsx
@@ -0,0 +1,39 @@
+import { Grid, Hidden, Card, CardContent } from "@material-ui/core";
+
+/**
+ * component for title and organization info on the listing detail page
+ * props: the listing to render
+ */
+interface TitleCardProps {
+  title: React.ReactNode
+  subtitle?: React.ReactNode
+  action?: React.ReactNode
+}
+
+const TitleCard: React.FC<TitleCardProps> = ({ title, subtitle, action }) => {
+  return (
+      <Card>
+        <CardContent>
+          <Grid container direction="column" justify="space-between" alignItems="center">
+            <Grid item>
+              {title}
+            </Grid>
+            {subtitle &&
+              <Grid item>
+                {subtitle}
+              </Grid>
+            }
+            {action &&  
+              <Hidden xsDown>
+                <Grid item>
+                  {action}
+                </Grid>
+              </Hidden>
+            }
+          </Grid>
+        </CardContent>
+      </Card>
+  );
+};
+
+export default TitleCard;
