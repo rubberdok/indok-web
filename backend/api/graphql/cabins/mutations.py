4126d7a3dec8795579eb2c5cd60b892f42edd093
Sverre Spetalen <sverress@stud.ntnu.no>
2021-04-12 18:38:09 +0200

    Update backend/api/graphql/cabins/mutations.py
    Co-authored-by: Lars Waage <46653859+larwaa@users.noreply.github.com>

--- backend/api/graphql/cabins/mutations.py
+++ backend/api/graphql/cabins/mutations.py
@@ -16,14 +16,11 @@ def checkin_validation(check_in, check_out, cabin_ids):
         raise GraphQLError("invalid input: Checkin is after checkout")
     # https://stackoverflow.com/questions/325933/determine-whether-two-date-ranges-overlap
     if (
-        len(
-            BookingModel.objects.filter(
-                check_in__lte=check_out,
-                check_out__gt=check_in,
-                cabins__id__in=cabin_ids,
-            )
-        )
-        > 0
+        BookingModel.objects.filter(
+            check_in__lte=check_out,
+            check_out__gt=check_in,
+            cabins__id__in=cabin_ids,
+        ).exists()
     ):
         raise GraphQLError("Input dates overlaps existing booking")
 
