ca220d7cb5a81398a02f848c61118b3aedfe7488
mathildesolberg <mathilde.solberg@hotmail.com>
2021-04-15 18:56:52 +0200

    updated the information in the Påmelding and Venteliste lists

--- backend/api/auth/dataporten_auth.py
+++ backend/api/auth/dataporten_auth.py
@@ -194,6 +194,8 @@ class DataportenAuth:
             print(f"\nUser {username} exists, updating in the database")
             user.id_token = id_token
             user.last_login = timezone.now()
+            if not user.feide_email:
+                user.feide_email = email
             user.save()
             enrolled = True
 
