d71afc5e13432c9f51d00c76abffc904936fbb27
Morgan Heggland <morganheggland@gmail.com>
2021-04-15 20:19:12 +0200

    initial test setup and user factory

---
+++ backend/utils/testing/ExtendedGraphQLTestCase.py
@@ -0,0 +1,12 @@
+from graphene_django.utils.testing import GraphQLTestCase
+from graphql_jwt.shortcuts import get_token
+
+
+class ExtendedGraphQLTestCase(GraphQLTestCase):
+    def setUp(self) -> None:
+        # TODO: Add fields and methods for user authentication
+        self.GRAPHQL_URL = "/graphql"
+
+    def auth_query(self, query, user, **kwargs):
+        token = get_token(user)
+        self.query(self, query, headers={"cookie": {"JWT": token}}, **kwargs)
