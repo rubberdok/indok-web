6e984cfa83519210a3b87fdf0d020d0a65bcfed5
Sverre Spetalen <sverress@stud.ntnu.no>
2021-04-15 20:10:16 +0200

    sort by check-in date

--- backend/api/graphql/cabins/resolvers.py
+++ backend/api/graphql/cabins/resolvers.py
@@ -3,7 +3,7 @@ from apps.cabins.models import Booking as BookingModel, Cabin
 
 class CabinResolvers:
     def resolve_all_bookings(self, root, **kwargs):
-        query = BookingModel.objects.all()
+        query = BookingModel.objects.all().order_by("check_in")
         if kwargs.get("before"):
             query = query.filter(check_in__lt=kwargs.get("before"))
         if kwargs.get("after"):
