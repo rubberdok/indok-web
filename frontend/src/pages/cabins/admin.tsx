4dc3a85afca15fe4e55d936134fec3e5c70a9955
Sverre Spetalen <sverress@stud.ntnu.no>
2021-04-15 20:04:27 +0200

    show all bookings in admin table

--- frontend/src/pages/cabins/admin.tsx
+++ frontend/src/pages/cabins/admin.tsx
@@ -2,7 +2,21 @@ import { useQuery } from "@apollo/client";
 import Layout from "@components/Layout";
 import { QUERY_ALL_BOOKINGS } from "@graphql/cabins/queries";
 import { Booking } from "@interfaces/cabins";
-import { Typography, Grid, Box, List, ListItem } from "@material-ui/core";
+import {
+  Typography,
+  Grid,
+  Box,
+  List,
+  ListItem,
+  TableContainer,
+  Paper,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+} from "@material-ui/core";
+import { toStringChosenCabins } from "@utils/cabins";
 import dayjs from "dayjs";
 import { NextPage } from "next";
 const BookingAdminPage: NextPage = () => {
@@ -20,11 +34,36 @@ const BookingAdminPage: NextPage = () => {
           </Box>
         </Grid>
         <Grid item>
-          <List>
-            {allBookingsQuery.data?.allBookings.map((booking: Booking) => (
-              <ListItem key={booking.id}>{booking.firstname}</ListItem>
-            ))}
-          </List>
+          <Box m={5}>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Bookings</TableCell>
+                    <TableCell align="right">Epost</TableCell>
+                    <TableCell align="right">Telefonnummer</TableCell>
+                    <TableCell align="right">Innsjekk</TableCell>
+                    <TableCell align="right">Utsjekk</TableCell>
+                    <TableCell align="right">Hytte</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {allBookingsQuery.data?.allBookings.map((booking: Booking) => (
+                    <TableRow key={booking.id}>
+                      <TableCell component="th" scope="row">
+                        {booking.firstname + " " + booking.surname}
+                      </TableCell>
+                      <TableCell align="right">{booking.receiverEmail}</TableCell>
+                      <TableCell align="right">{booking.phone}</TableCell>
+                      <TableCell align="right">{booking.checkIn}</TableCell>
+                      <TableCell align="right">{booking.checkOut}</TableCell>
+                      <TableCell align="right">{toStringChosenCabins(booking.cabins)}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Box>
         </Grid>
       </Grid>
     </Layout>
