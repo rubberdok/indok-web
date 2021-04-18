6faed5cf5a0130f02795a6d67608e0a7bec96b6b
hermannm <hermann.morkrid@gmail.com>
2021-04-19 00:35:59 +0200

    move Layout/Container to pages instead of components
    move NewListing into organization component folder

--- frontend/src/pages/orgs/[orgId]/listings/new.tsx
+++ frontend/src/pages/orgs/[orgId]/listings/new.tsx
@@ -1,16 +1,22 @@
 import { NextPage } from "next";
-import NewListing from "@components/pages/listings/NewListing";
+import NewListing from "@components/pages/listings/organization/NewListing";
 import { useRouter } from "next/router";
+import Layout from "@components/Layout";
+import { Container } from "@material-ui/core";
 
 /**
  * @description Page for creating new listings, navigates to the newly created listing upon completion
  */
 const NewListingPage: NextPage = () => {
-  const router = useRouter()
-  const { orgId } = router.query
+  const router = useRouter();
+  const { orgId } = router.query;
 
   return (
-    <NewListing defaultOrganizationId={orgId as string}/>
+    <Layout>
+      <Container>
+        <NewListing defaultOrganizationId={orgId as string} />
+      </Container>
+    </Layout>
   );
 };
 export default NewListingPage;
