a077c11d1bfe161bdb884afa52f210b156fd36a6
hermannm <hermann.morkrid@gmail.com>
2021-04-19 04:27:18 +0200

    move [listingId] page to [listingId]/index

--- frontend/src/pages/orgs/[orgId]/listings/new.tsx
+++ frontend/src/pages/orgs/[orgId]/listings/new.tsx
@@ -1,8 +1,6 @@
 import { NextPage } from "next";
 import NewListing from "@components/pages/listings/organization/NewListing";
 import { useRouter } from "next/router";
-import Layout from "@components/Layout";
-import { Container, makeStyles } from "@material-ui/core";
 
 /**
  * @description Page for creating new listings, navigates to the newly created listing upon completion
