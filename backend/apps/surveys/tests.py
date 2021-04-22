8c12e30a118827d370dcf2ff0dbe9a98d553de00
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-04-22 17:52:24 +0200

    :construction: wip: tests

--- backend/apps/surveys/tests.py
+++ backend/apps/surveys/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
+import json
+from datetime import datetime
 
-# Create your tests here.
+from graphene.utils.str_converters import to_snake_case
+
+from ...utils.testing.ExtendedGraphQLTestCase import ExtendedGraphQLTestCase
+from ...utils.testing.factories.organizations import OrganizationFactory, MembershipFactory
+from ...utils.testing.factories.users import UserFactory
+
+class ListingsBaseTestCase(ExtendedGraphQLTestCase):
+  def setUp(self) -> None:
+    super().setUp()
+
+    self.user = UserFactory()
+    self.organization = OrganizationFactory()
+    self.organization_user = UserFactory()
+    self.membership = MembershipFactory(user=self.organization_user, organization=self.organization)
+
+
+class ListingsResolversTestCase(ListingsBaseTestCase):
+  query = """
+    query {
+      listings {
+        id
+      }
+    }
+  """
