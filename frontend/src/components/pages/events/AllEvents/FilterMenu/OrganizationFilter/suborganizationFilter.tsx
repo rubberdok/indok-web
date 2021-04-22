9e5daef817f3f5be1e0869884648af1c7c6c9519
Morgan Heggland <morganheggland@gmail.com>
2021-04-22 16:21:30 +0200

    fix ts error

--- frontend/src/components/pages/events/AllEvents/FilterMenu/OrganizationFilter/suborganizationFilter.tsx
+++ frontend/src/components/pages/events/AllEvents/FilterMenu/OrganizationFilter/suborganizationFilter.tsx
@@ -2,7 +2,7 @@ import { Organization } from "@interfaces/organizations";
 import { Collapse, List, ListItem, ListItemText } from "@material-ui/core";
 import { ExpandLess, ExpandMore } from "@material-ui/icons";
 import React, { useState } from "react";
-import { FilterQuery } from "../../..";
+import { FilterQuery } from "../..";
 
 interface Props {
   filters: FilterQuery;
