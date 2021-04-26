0a587e36c82a2a6221f052ad722f16dfb558b2a8
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-04-26 21:40:57 +0200

    :sparkles: feat: add default permissions post migrate

--- backend/apps/users/signals.py
+++ backend/apps/users/signals.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_migrate, post_save
 from guardian.shortcuts import assign_perm
 
 
@@ -8,3 +8,9 @@ from guardian.shortcuts import assign_perm
 def assign_default_permissions(sender, instance, created, **kwargs):
     if created:
         assign_perm("users.view_sensitive_info", instance, instance)
+
+
+@receiver(post_migrate)
+def assign_bulk_default_permissions(**kwargs):
+    for user in get_user_model().objects.all():
+        assign_perm("users.view_sensitive_info", user, user)
