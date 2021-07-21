7a06f1115409cdfa31f0d8f50cec910abe481ce9
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-07-21 22:01:07 +0200

    chore (deps): remove restframework from installed apps

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
     # External apps
     "corsheaders",
     "graphene_django",
-    "rest_framework",
     "phonenumber_field",
     "guardian",
 ]
@@ -181,11 +180,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 # CONFIG
-EMAIL_BACKEND = 'django_ses.SESBackend'
+EMAIL_BACKEND = "django_ses.SESBackend"
 
-AWS_SES_REGION_NAME = 'eu-north-1'
-AWS_SES_REGION_ENDPOINT = 'email.eu-north-1.amazonaws.com'
-AWS_ACCESS_KEY_ID = 'AKIA3KG6AVJ476JEMRTF'
+AWS_SES_REGION_NAME = "eu-north-1"
+AWS_SES_REGION_ENDPOINT = "email.eu-north-1.amazonaws.com"
+AWS_ACCESS_KEY_ID = "AKIA3KG6AVJ476JEMRTF"
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 
 GOOGLE_DRIVE_API_KEY = env("GOOGLE_DRIVE_API_KEY")
