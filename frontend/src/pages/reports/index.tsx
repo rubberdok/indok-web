2132ccbda46817f87179fb77219ab8b291cb63c1
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-08-12 19:59:44 +0200

    feat (reports): add content

--- frontend/src/pages/reports/index.tsx
+++ frontend/src/pages/reports/index.tsx
@@ -1,55 +1,69 @@
 import Layout from "@components/Layout";
 import {
+  Avatar,
+  Button,
   Card,
   CardContent,
+  CircularProgress,
+  Grid,
   List,
   ListItem,
   ListItemAvatar,
+  ListItemIcon,
   ListItemText,
-  Avatar,
-  Grid,
   Typography,
-  CircularProgress,
-  CardHeader,
-  makeStyles,
 } from "@material-ui/core";
-import { deepPurple, deepOrange, indigo } from "@material-ui/core/colors";
+import { amber, deepOrange, deepPurple, green, indigo, red } from "@material-ui/core/colors";
+import { ArrowRight } from "@material-ui/icons";
 import { NextPage } from "next";
-
-const useStyles = makeStyles((theme) => ({
-  form: {
-    [theme.breakpoints.down("sm")]: {
-      flexDirection: "column-reverse",
-    },
-    [theme.breakpoints.up("md")]: {
-      flexDirection: "row",
-    },
-  },
-}));
+import { useRef } from "react";
 
 const ReportsPage: NextPage = () => {
-  const classes = useStyles();
+  const formRef = useRef<null | HTMLDivElement>(null);
+  const scrollToForm = () => formRef.current?.scrollIntoView({ behavior: "smooth" });
+
   const responsibles = [
     {
       id: 1,
-      name: "Maria Ruiz",
-      year: "4",
-      initials: "MR",
-      color: deepPurple[500],
+      name: "Amund Andreassen",
+      position: "Leder",
+      initials: "AA",
+      color: red[800],
     },
     {
       id: 2,
-      name: "Lars Waage",
-      year: "4",
-      initials: "LW",
+      name: "Christine Lindberg",
+      position: "Medlem",
+      initials: "CL",
       color: deepOrange[500],
     },
     {
       id: 3,
-      name: "Hermann Mørkrid",
-      year: "4",
-      initials: "HM",
+      name: "Laila Voll",
+      position: "Medlem",
+      initials: "LV",
+      color: amber[500],
+    },
+    {
+      id: 4,
+      name: "Benjamin Pettersen",
+      position: "Medlem",
+      initials: "BP",
+      color: green[500],
+    },
+    {
+      id: 5,
+      name: "Jesper Engvik Skovdahl",
+      position: "Medlem",
+      initials: "JS",
       color: indigo[500],
+    },
+    {
+      id: 6,
+      name: "Maria Ruiz Ulltveit-Moe (permisjon)",
+      position: "Medlem",
+      initials: "MM",
+      color: deepPurple[500],
     },
   ];
 
@@ -59,63 +73,293 @@ const ReportsPage: NextPage = () => {
         container
         direction="row"
         justify="center"
-        alignItems="center"
+        alignItems="stretch"
         spacing={2}
-        style={{ marginTop: 16, marginBottom: 16 }}
+        style={{ marginTop: 32, marginBottom: 16 }}
       >
-        <Grid item container direction="column" justify="center" alignItems="center" spacing={4} md={8}>
+        <Grid item container direction="column" justify="center" alignItems="stretch" spacing={4} md={8}>
           <Grid item>
-            <Typography variant="h1">Varslinger</Typography>
-          </Grid>
-          <Grid item md>
             <Card>
-              <CardHeader title="Informasjon" />
-              <CardContent>
-                <Typography variant="body1">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                  aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-                  officia deserunt mollit anim id est laborum.
+              <CardContent style={{ padding: 32 }}>
+                <Typography variant="h3" component="h2" gutterBottom>
+                  Varsling
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Indøk utgjør et stort og viktig sosialt fellesskap for studentene ved studieretningen.
+                  Baksideløsningen er rettet både mot foreningene tilknyttet studiet og studenter ved studiet generelt.
+                  Indøk skal være et trygt fellesskap for alle deltakere på aktiviteter og arrangementer, og for alle
+                  som er tillitsvalgte eller bidrar frivillig til en forening tilknyttet Indøk. Det overordnede målet er
+                  at grenseoverskridende atferd aldri finner sted i organisasjonen eller i tilknytning til
+                  studieretningens arrangement eller aktiviteter.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Det er viktig for oss at du sier ifra dersom du eller andre opplever noe som ikke er greit, slik at vi
+                  får muligheten til å gjøre noe med det som har skjedd og hindre at det skjer igjen.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Om du er i tvil, si ifra!
+                </Typography>
+                <Grid container direction="column" alignItems="center" spacing={2}>
+                  <Grid item>
+                    <Button onClick={scrollToForm} variant="contained" color="primary" size="large">
+                      Varsel? Trykk her
+                    </Button>
+                  </Grid>
+
+                  <Grid item>
+                    <Typography variant="body2" gutterBottom>
+                      Flere måter å varsle på står beskrevet lenger ned.
+                    </Typography>
+                  </Grid>
+                </Grid>
+
+                <Typography variant="h4" component="h3">
+                  Hva er et varsel?
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Et varsel er å melde ifra om bekymringsverdige forhold. Det kan være å si ifra om noe som ikke er
+                  greit, noe som føles ubehagelig eller skremmende, eller handlinger som er ulovlige.{" "}
+                </Typography>
+
+                <Typography variant="h4" component="h3">
+                  Hvem kan varsle?
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Et varsel er å melde ifra om bekymringsverdige forhold. Det kan være å si ifra om noe som ikke er
+                  greit, noe som føles ubehagelig eller skremmende, eller handlinger som er ulovlige.
+                </Typography>
+
+                <Typography variant="h4" component="h3">
+                  Hva kan jeg varsle om?
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Du kan varsle om noe du selv har opplevd, noe som har skjedd med andre, noe du har blitt fortalt eller
+                  noe du har sett som du mener ikke er greit eller som er ulovlig. Du har mulighet til å varsle både om
+                  ting du ser på som veldig alvorlig og om ting som ikke nødvendigvis er så farlig, men du tenker er
+                  greit at baksidekontakt vet om og potensielt kan ta tak i.
+                </Typography>
+
+                <Typography variant="subtitle1">Eksempler på kritikkverdige forhold:</Typography>
+                <List>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>Brudd på Retningslinjer varslingssystem Indøk</ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>Lovbrudd</ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>Mobbing, seksuell trakassering og overgrep</ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>Diskriminering og rasisme</ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>Uansvarlige strukturerer eller valg fra en forening</ListItemText>
+                  </ListItem>
+                </List>
+
+                <Typography variant="subtitle1">Eksempler på forhold baksideløsningen ikke håndterer:</Typography>
+                <List>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>Økonomisk mislighold og korrupsjon - Kontakt revisjonskomiteen</ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Kritikk av faglig innhold eller studiets oppbygning - Kontakt kulltillitsvalgt
+                    </ListItemText>
+                  </ListItem>
+                </List>
+
+                <Typography variant="h4" component="h3">
+                  Hvem kan varsle?
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Du har alltid mulighet til å varsle. Dersom du er bekymret eller i tvil bør du si ifra slik at
+                  situasjonen kan oppklares. Det er lav terskel for å varsle og alle henvendelser blir tatt på alvor.
+                </Typography>
+
+                <Typography variant="h4" component="h3">
+                  Hvordan varsler jeg?
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Dersom varselet er tilknyttet en forening eller et arrangement i regi av en forening skal du ta
+                  kontakt med din baksidekontakt. Dersom saken er av høy alvorlighetsgrad eller du av ulike grunner ikke
+                  ønsker å varsle til sin baksidekontakt, kan varselet sendes direkte til baksideutvalget eller en
+                  enkeltperson i baksideutvalget. I et varsel må du ha med ditt eget navn og hva du vil varsle eller
+                  sende en bekymring om.
+                </Typography>
+
+                <List>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <Grid container direction="row" spacing={1}>
+                        <Grid item>
+                          <strong>Skjema på nettsiden:</strong>
+                        </Grid>
+                        <Grid item>
+                          <Button size="small" variant="contained" color="primary" onClick={scrollToForm}>
+                            Fylle ut varslingsskjema
+                          </Button>
+                        </Grid>
+                      </Grid>
+                    </ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <strong>E-post:</strong> Sende inn en e-post til baksideansvarlig i en forening eller til et
+                      medlem i baksideutvalget. Dersom baksidekontakt eller medlemmer av baksideutvalget av ulike
+                      grunner ønskes ekskludert fra saksgangen, skal dette spesifiseres i e-posten.
+                    </ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <strong>Annet:</strong> Ringe, sende melding eller be om et personlig møte med baksidekontakt
+                      eller baksideutvalget
+                    </ListItemText>
+                  </ListItem>
+                </List>
+
+                <Typography variant="body1" gutterBottom>
+                  Meld fra på den måten du er mest komfortabel med. Dersom du synes det er vanskelig å si ifra på
+                  egenhånd kan du ta med deg en kontaktperson gjennom prosessen.
+                </Typography>
+
+                <Typography variant="h4" component="h3">
+                  Kontaktinfo Baksideutvalget
+                </Typography>
+                <Grid container direction="row">
+                  <Grid item>
+                    <List dense>
+                      {responsibles.slice(0, 3).map((responsible) => (
+                        <ListItem key={responsible.id}>
+                          <ListItemAvatar>
+                            <Avatar style={{ backgroundColor: responsible.color }}>
+                              <Typography variant="body2">{responsible.initials}</Typography>
+                            </Avatar>
+                          </ListItemAvatar>
+                          <ListItemText primary={responsible.name} secondary={responsible.position} />
+                        </ListItem>
+                      ))}
+                    </List>
+                  </Grid>
+                  <Grid item>
+                    <List dense>
+                      {responsibles.slice(3).map((responsible) => (
+                        <ListItem key={responsible.id}>
+                          <ListItemAvatar>
+                            <Avatar style={{ backgroundColor: responsible.color }}>
+                              <Typography variant="body2">{responsible.initials}</Typography>
+                            </Avatar>
+                          </ListItemAvatar>
+                          <ListItemText primary={responsible.name} secondary={responsible.position} />
+                        </ListItem>
+                      ))}
+                    </List>
+                  </Grid>
+                </Grid>
+
+                <Typography variant="subtitle1">Den som varsler oppfordres til å informere om:</Typography>
+                <List>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Hva det varsles om, hvor og når det skjedde og hvem som er involvert. Beskriv det som har skjedd
+                      så nøye du kan.
+                    </ListItemText>
+                  </ListItem>
+                  <ListItem>
+                    <ListItemIcon>
+                      <ArrowRight />
+                    </ListItemIcon>
+                    <ListItemText>Andre som eventuelt er informert i saken.</ListItemText>
+                  </ListItem>
+                </List>
+
+                <Typography variant="body1" gutterBottom>
+                  Dersom du har forslag til tiltak og hensyn som bør ivaretas kan du gjerne informere om det. Dersom
+                  baksidekontakt eller medlemmer av baksideutvalget av ulike grunner ønskes ekskludert fra saksgangen,
+                  skal varsler også spesifisere dette. Det er ikke mulig å ekskludere mer enn halvparten av
+                  baksideutvalget. I slike tilfeller vil Hovedstyret involveres for å finne en løsning.
+                </Typography>
+
+                <Typography variant="h4" component="h3">
+                  Hva skjer når jeg har varslet?
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Varsel og varslere vil alltid bli tatt på alvor.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Ved innsending av en sak viderebringes saken til baksideutvalget. Baksideutvalget er ansvarlig for
+                  håndteringen av saken. Du vil også få tildelt en kontaktperson fra baksideutvalget.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Baksideutvalget sin første oppgave vil være å få klarhet i hva som har skjedd. Som regel blir du
+                  kontaktet kort tid etter du har sendt inn varselet for en samtale. Baksideutvalget vil dermed samle
+                  ytterligere informasjon om hendelsen og vurdere om sanksjoner er relevant.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Mens saken håndteres vil din kontaktperson til enhver tid holde deg oppdatert på hva som skjer. Du som
+                  varsler skal ivaretas så godt som mulig gjennom hele prosessen.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Varslingssaker vil alltid bli behandlet fortrolig og av så få som mulig. Parter som er spesifisert
+                  ekskludert vil derfor ikke være involvert i saksgangen. Enhver saksbehandler vil signere en
+                  taushetserklæring.
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Baksideløsningens håndtering av varsler handler om tillitsspørsmål og ikke skyld. Politi og domstol
+                  tar stilling til skyldspørsmål. Saksbehandler kan imidlertid bistå i å ta slike saker videre.
                 </Typography>
               </CardContent>
             </Card>
           </Grid>
-          <Grid className={classes.form} container item justify="space-between" alignItems="stretch" spacing={4}>
-            <Grid item md>
-              <Card>
-                <CardContent>
-                  <iframe
-                    title="Reports"
-                    src="https://docs.google.com/forms/d/e/1FAIpQLSf9yxNEZoXYYDIPSsDTLUhgnFKr-pbFWPJNkPaTDN86oI7Sjg/viewform?embedded=true"
-                    width="100%"
-                    height="943px"
-                    frameBorder="0"
-                  >
-                    <CircularProgress />
-                  </iframe>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item>
-              <Card>
-                <CardHeader title="Ansvarlige" />
-                <CardContent>
-                  <List dense>
-                    {responsibles.map((responsible) => (
-                      <ListItem key={responsible.id}>
-                        <ListItemAvatar>
-                          <Avatar style={{ backgroundColor: responsible.color }}>
-                            <Typography variant="body2">{responsible.initials}</Typography>
-                          </Avatar>
-                        </ListItemAvatar>
-                        <ListItemText primary={responsible.name} secondary={`${responsible.year}. klasse`} />
-                      </ListItem>
-                    ))}
-                  </List>
-                </CardContent>
-              </Card>
-            </Grid>
+
+          <Grid style={{ width: "100%" }} item ref={formRef}>
+            <Card style={{ width: "100%" }}>
+              <CardContent style={{ width: "100%" }}>
+                <iframe
+                  title="Reports"
+                  src="https://docs.google.com/forms/d/e/1FAIpQLScP9hqHr2lzlbh1Ie7xXaLAjRkQx9MdRf5Hl1EDLb1kpulmjg/viewform?embedded=true"
+                  width="100%"
+                  height="1842px"
+                  frameBorder="0"
+                >
+                  <CircularProgress />
+                </iframe>
+              </CardContent>
+            </Card>
           </Grid>
         </Grid>
       </Grid>
