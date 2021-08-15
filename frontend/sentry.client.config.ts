895f116bf9263252fc1a049d883626aa75dfb91d
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-08-15 21:14:11 +0200

    feat: add environment tag to production

--- frontend/sentry.client.config.ts
+++ frontend/sentry.client.config.ts
@@ -5,6 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const ENVIRONMENT = process.env.NODE_ENV === "production" ? process.env.NODE_ENV : undefined;
 
 Sentry.init({
   dsn: SENTRY_DSN || "https://4a40409b5c6f4a759eff513c338a18be@o514678.ingest.sentry.io/5618306",
@@ -14,4 +15,5 @@ Sentry.init({
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
+  environment: ENVIRONMENT,
 });
