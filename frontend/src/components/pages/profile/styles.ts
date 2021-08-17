a9023fffd2b906090696457eb4da2f211270591e
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-08-18 01:50:48 +0200

    feat: add style file

---
+++ frontend/src/components/pages/profile/styles.ts
@@ -0,0 +1,16 @@
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  large: {
+    width: theme.spacing(16),
+    height: theme.spacing(16),
+  },
+  cards: {
+    flexDirection: "row",
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
+}));
+
+export default useStyles;
