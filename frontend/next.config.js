3970841788406c5cce9ae49bcdea76f0b80a6be4
hermannm <hermann.morkrid@gmail.com>
2021-08-17 17:31:06 +0200

    change report URLs to '/report' and '/varsle'

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,20 +2,8 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: "/report",
-        destination: "/reports",
-      },
-      {
-        source: "/varsler",
-        destination: "/reports",
-      },
-      {
         source: "/varsle",
-        destination: "/reports",
-      },
-      {
-        source: "/varslinger",
-        destination: "/reports",
+        destination: "/report",
       },
     ];
   },
