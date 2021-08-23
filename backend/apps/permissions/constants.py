91a43a78f98608f24ba94a43588756bacf19b8a1
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-08-23 16:02:26 +0200

    tests: add tests and fix permission issues

--- backend/apps/permissions/constants.py
+++ backend/apps/permissions/constants.py
@@ -1,20 +1,21 @@
 from typing import Final
 
+DefaultPermissionsType = Final[list[tuple[str, str]]]
 
 ORGANIZATION: Final = "Organization member"
 INDOK: Final = "Indøk"
 
-DEFAULT_ORGANIZATION_PERMISSIONS: Final = [
+DEFAULT_ORGANIZATION_PERMISSIONS: DefaultPermissionsType = [
     ("events", "add_event"),
     ("events", "change_event"),
     ("events", "delete_event"),
-    ("listings" "add_listing"),
+    ("listings", "add_listing"),
     ("listings", "change_listing"),
     ("listings", "delete_listing"),
     ("organizations", "add_membership"),
 ]
 
-DEFAULT_INDOK_PERMISSIONS: Final = [
+DEFAULT_INDOK_PERMISSIONS: DefaultPermissionsType = [
     ("listings", "view_listing"),
     ("events", "view_event"),
     ("organizations", "view_organization"),
@@ -22,6 +23,6 @@ DEFAULT_INDOK_PERMISSIONS: Final = [
     ("forms", "change_answer"),
     ("forms", "view_answer"),
     ("forms", "add_response"),
-    ("events", "add_sign_up"),
-    ("events", "view_sign_up"),
+    ("events", "add_signup"),
+    ("events", "view_signup"),
 ]
