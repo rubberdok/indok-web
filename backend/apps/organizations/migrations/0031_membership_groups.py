65cf719a69e41da302a7bc1174246b5e2b8fe0ff
hermannm <hermann.morkrid@gmail.com>
2021-08-24 12:34:05 +0200

    add necessary user migration dependency
    remove redundant save

--- backend/apps/organizations/migrations/0031_membership_groups.py
+++ backend/apps/organizations/migrations/0031_membership_groups.py
@@ -4,63 +4,49 @@ from apps.permissions.constants import ORGANIZATION
 def add_org_members_to_permission_groups(apps, schema_editor):
   MembershipModel = apps.get_model('organizations', 'Membership')
   GroupModel = apps.get_model('auth', 'Group')
+  UserModel = apps.get_model('users', 'User')
 
   organization_member_group = GroupModel.objects.get(name=ORGANIZATION)
 
   for membership in MembershipModel.objects.all():
-    user_changed = False
+    user = UserModel.objects.get(id=membership.user.id)
 
-    user_groups = membership.user.groups
-
-    if organization_member_group not in user_groups.all():
-      user_groups.add(organization_member_group)
-      user_changed = True
+    if organization_member_group not in user.groups.all():
+      user.groups.add(organization_member_group)
       print(f"User {membership.user.username} added to Organization member group")
 
     organization_hr_group = membership.organization.hr_group.group
 
-    if membership.group.group == organization_hr_group and organization_hr_group not in user_groups.all():
-      user_groups.add(organization_hr_group)
-      user_changed = True
+    if membership.group.group == organization_hr_group and organization_hr_group not in user.groups.all():
+      user.groups.add(organization_hr_group)
       print(f"User {membership.user.username} added to {membership.organization.name}'s HR group")
-
-    if user_changed:
-      membership.user.save()
-      print(f"{membership.user.username} updated")
     
 
 def remove_org_members_from_permission_groups(apps, schema_editor):
-  print("reversed, remove groups manually in Django admin")
-  """ MembershipModel = apps.get_model('organizations', 'Membership')
+  MembershipModel = apps.get_model('organizations', 'Membership')
   GroupModel = apps.get_model('auth', 'Group')
+  UserModel = apps.get_model('users', 'User')
 
   organization_member_group = GroupModel.objects.get(name=ORGANIZATION)
 
   for membership in MembershipModel.objects.all():
-    user_changed = False
+    user = UserModel.objects.get(id=membership.user.id)
 
-    user_groups = membership.user.groups
-
-    if organization_member_group in user_groups.all():
-      user_groups.remove(organization_member_group)
-      user_changed = True
+    if organization_member_group in user.groups.all():
+      user.groups.remove(organization_member_group)
       print(f"User {membership.user.username} removed from Organization member group")
 
     organization_hr_group = membership.organization.hr_group.group
 
-    if membership.group.group == organization_hr_group and organization_hr_group in user_groups.all():
-      user_groups.remove(organization_hr_group)
-      user_changed = True
+    if membership.group.group == organization_hr_group and organization_hr_group in user.groups.all():
+      user.groups.remove(organization_hr_group)
       print(f"User {membership.user.username} removed from {membership.organization.name}'s HR group")
-
-    if user_changed:
-      membership.user.save()
-      print(f"{membership.user.username} updated") """
 
 class Migration(migrations.Migration):
 
   dependencies = [
-    ('organizations', '0030_auto_20210426_2129')
+    ('organizations', '0030_auto_20210426_2129'),
+    ('users', '0007_auto_20210312_1746')
   ]
 
   operations = [
