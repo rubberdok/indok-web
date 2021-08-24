34c7294cd7db7c49f8ffcd027839a2b6438434fe
hermannm <hermann.morkrid@gmail.com>
2021-08-24 16:53:54 +0200

    fix indentation and remove prints

--- backend/apps/organizations/migrations/0031_membership_groups.py
+++ backend/apps/organizations/migrations/0031_membership_groups.py
@@ -2,53 +2,49 @@ from django.db import migrations, models
 from apps.permissions.constants import ORGANIZATION
 
 def add_org_members_to_permission_groups(apps, schema_editor):
-  MembershipModel = apps.get_model('organizations', 'Membership')
-  GroupModel = apps.get_model('auth', 'Group')
-  UserModel = apps.get_model('users', 'User')
+    MembershipModel = apps.get_model('organizations', 'Membership')
+    GroupModel = apps.get_model('auth', 'Group')
+    UserModel = apps.get_model('users', 'User')
 
-  organization_member_group = GroupModel.objects.get(name=ORGANIZATION)
+    organization_member_group = GroupModel.objects.get(name=ORGANIZATION)
 
-  for membership in MembershipModel.objects.all():
-    user = UserModel.objects.get(id=membership.user.id)
+    for membership in MembershipModel.objects.all():
+        user = UserModel.objects.get(id=membership.user.id)
 
-    if organization_member_group not in user.groups.all():
-      user.groups.add(organization_member_group)
-      print(f"User {membership.user.username} added to Organization member group")
+        if organization_member_group not in user.groups.all():
+            user.groups.add(organization_member_group)
 
-    organization_hr_group = membership.organization.hr_group.group
+        organization_hr_group = membership.organization.hr_group.group
 
-    if membership.group.group == organization_hr_group and organization_hr_group not in user.groups.all():
-      user.groups.add(organization_hr_group)
-      print(f"User {membership.user.username} added to {membership.organization.name}'s HR group")
+        if membership.group.group == organization_hr_group and organization_hr_group not in user.groups.all():
+            user.groups.add(organization_hr_group)
     
 
 def remove_org_members_from_permission_groups(apps, schema_editor):
-  MembershipModel = apps.get_model('organizations', 'Membership')
-  GroupModel = apps.get_model('auth', 'Group')
-  UserModel = apps.get_model('users', 'User')
+    MembershipModel = apps.get_model('organizations', 'Membership')
+    GroupModel = apps.get_model('auth', 'Group')
+    UserModel = apps.get_model('users', 'User')
 
-  organization_member_group = GroupModel.objects.get(name=ORGANIZATION)
+    organization_member_group = GroupModel.objects.get(name=ORGANIZATION)
 
-  for membership in MembershipModel.objects.all():
-    user = UserModel.objects.get(id=membership.user.id)
+    for membership in MembershipModel.objects.all():
+        user = UserModel.objects.get(id=membership.user.id)
 
-    if organization_member_group in user.groups.all():
-      user.groups.remove(organization_member_group)
-      print(f"User {membership.user.username} removed from Organization member group")
+        if organization_member_group in user.groups.all():
+            user.groups.remove(organization_member_group)
 
-    organization_hr_group = membership.organization.hr_group.group
+        organization_hr_group = membership.organization.hr_group.group
 
-    if membership.group.group == organization_hr_group and organization_hr_group in user.groups.all():
-      user.groups.remove(organization_hr_group)
-      print(f"User {membership.user.username} removed from {membership.organization.name}'s HR group")
+        if membership.group.group == organization_hr_group and organization_hr_group in user.groups.all():
+            user.groups.remove(organization_hr_group)
 
 class Migration(migrations.Migration):
 
-  dependencies = [
-    ('organizations', '0030_auto_20210426_2129'),
-    ('users', '0007_auto_20210312_1746')
-  ]
+    dependencies = [
+        ('organizations', '0030_auto_20210426_2129'),
+        ('users', '0007_auto_20210312_1746')
+    ]
 
-  operations = [
-    migrations.RunPython(add_org_members_to_permission_groups, remove_org_members_from_permission_groups)
-  ]
+    operations = [
+        migrations.RunPython(add_org_members_to_permission_groups, remove_org_members_from_permission_groups)
+    ]
