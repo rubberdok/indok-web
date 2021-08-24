33d664f3c4d6ac1e8fed763a5b55f52cb50629c7
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-08-24 18:05:23 +0200

    build: tag images with source version

--- deploy.sh
+++ deploy.sh
@@ -25,11 +25,6 @@ update_service() {
   fi
 }
 
-push_images() {
-  docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/indokweb-backend:$CODEBUILD_RESOLVED_SOURCE_VERSION
-  docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/indokweb-frontend:$CODEBUILD_RESOLVED_SOURCE_VERSION
-}
-
 deploy_cluster() {
 
   cluster="indokweb-cluster"
@@ -38,7 +33,7 @@ deploy_cluster() {
   service="indokweb-backend-fg-service"
   template="ecs_backend_taskdefinition.json"
   task_template=$(cat "ecs/$template")
-  task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CODEBUILD_RESOLVED_SOURCE_VERSION $AWS_RDS_HOST $AWS_RDS_PASSWORD $PRODUCTION_SECRET_KEY $DATAPORTEN_SECRET $GOOGLE_DRIVE_API_KEY $DJANGO_SETTINGS_MODULE $AWS_SECRET_ACCESS_KEY $CODEBUILD_RESOLVED_SOURCE_VERSION)
+  task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CODEBUILD_SOURCE_VERSION $AWS_RDS_HOST $AWS_RDS_PASSWORD $PRODUCTION_SECRET_KEY $DATAPORTEN_SECRET $GOOGLE_DRIVE_API_KEY $DJANGO_SETTINGS_MODULE $AWS_SECRET_ACCESS_KEY $CODEBUILD_SOURCE_VERSION)
   echo "$task_def"
   register_definition
   update_service
@@ -47,19 +42,18 @@ deploy_cluster() {
   service="indokweb-frontend-fg-service"
   template="ecs_frontend_taskdefinition.json"
   task_template=$(cat "ecs/$template")
-  task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CODEBUILD_RESOLVED_SOURCE_VERSION)
+  task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $CODEBUILD_SOURCE_VERSION)
   echo "$task_def"
   register_definition
   update_service
 
 }
 
-echo $CODEBUILD_RESOLVED_SOURCE_VERSION
+echo $CODEBUILD_SOURCE_VERSION
 echo $CODEBUILD_WEBHOOK_BASE_REF
 echo $CODEBUILD_WEBHOOK_HEAD_REF
 echo $CODEBUILD_WEBHOOK_TRIGGER
 echo $CODEBUILD_WEBHOOK_EVENT
-push_images
 
 if  [ "$CODEBUILD_WEBHOOK_TRIGGER" == "branch/master" ] && \
     ([ "$CODEBUILD_WEBHOOK_HEAD_REF" == "refs/heads/master" ] || ["$CODEBUILD_WEBHOOK_WEBHOOK_EVENT" == "PULL_REQUEST_MERGED"])
