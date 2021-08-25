8938f2442c0b782d560908d5651cad30370ad510
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-08-25 13:29:38 +0200

    fix: merge migrations

--- backend/apps/organizations/migrations/0022_auto_20210721_1914.py
+++ backend/apps/organizations/migrations/0022_auto_20210721_1914.py
@@ -6,23 +6,22 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('organizations', '0021_auto_20210415_1706'),
+        ("organizations", "0021_auto_20210415_1706"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='membership',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="membership",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
         migrations.AlterField(
-            model_name='organization',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
-        ),
-        migrations.AlterField(
-            model_name='role',
-            name='id',
-            field=models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
+            model_name="organization",
+            name="id",
+            field=models.BigAutoField(
+                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+            ),
         ),
     ]
