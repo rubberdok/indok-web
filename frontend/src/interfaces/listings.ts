58287b1becef9f53e55d2566e3d62d67cece80f5
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-08-27 09:44:14 +0200

    fix: remove unecessary duplication

--- frontend/src/interfaces/listings.ts
+++ frontend/src/interfaces/listings.ts
@@ -18,19 +18,7 @@ export interface Listing {
 }
 
 export interface ListingWithForm extends Listing {
-  id: string;
-  title: string;
-  slug: string;
-  description: string;
-  startDatetime: string;
-  deadline: string;
-  endDatetime: string;
-  url?: string;
-  hero?: string;
-  logo?: string;
-  organization: Organization;
   form?: Form;
-  chips: string[];
 }
 
 export interface ListingInput extends Omit<Listing, "startDatetime" | "endDatetime" | "chips" | "slug"> {
