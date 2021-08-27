782326a499af0b6bd956c44269afc15e4135c48a
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-08-27 09:39:58 +0200

    fix: set the correct types

--- frontend/src/interfaces/listings.ts
+++ frontend/src/interfaces/listings.ts
@@ -13,6 +13,22 @@ export interface Listing {
   hero?: string;
   logo?: string;
   organization: Organization;
+  form?: Pick<Form, "id">;
+  chips: string[];
+}
+
+export interface ListingWithForm extends Listing {
+  id: string;
+  title: string;
+  slug: string;
+  description: string;
+  startDatetime: string;
+  deadline: string;
+  endDatetime: string;
+  url?: string;
+  hero?: string;
+  logo?: string;
+  organization: Organization;
   form?: Form;
   chips: string[];
 }
