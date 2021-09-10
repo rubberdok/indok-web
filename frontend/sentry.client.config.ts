87753a9077da9e0b06dcea4e24665f561102caf2
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-09-10 11:16:35 +0200

    fix: make releases staging

--- frontend/sentry.client.config.ts
+++ frontend/sentry.client.config.ts
@@ -5,16 +5,14 @@
 import * as Sentry from "@sentry/nextjs";
 
 const ENVIRONMENT = process.env.NODE_ENV;
-const SENTRY_RELEASE = process.env.SENTRY_RELEASE;
 
-if (ENVIRONMENT === "production" && SENTRY_RELEASE) {
+if (ENVIRONMENT === "production") {
   const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
   Sentry.init({
     dsn: SENTRY_DSN || "https://4a40409b5c6f4a759eff513c338a18be@o514678.ingest.sentry.io/5618306",
     // Adjust this value in production, or use tracesSampler for greater control
     tracesSampleRate: 1.0,
-    release: SENTRY_RELEASE,
     // ...
     // Note: if you want to override the automatic release value, do not set a
     // `release` value here - use the environment variable `SENTRY_RELEASE`, so
