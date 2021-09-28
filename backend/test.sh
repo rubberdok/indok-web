7ca19080f190f9113d195d050092a9975f2f6c32
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-09-28 11:17:26 +0200

    fix: add envs

--- backend/test.sh
+++ backend/test.sh
@@ -15,7 +15,14 @@ fi
 echo "found image"
 
 echo "trying to run"
-docker run -e $DATAPORTEN_ID -e $DATAPORTEN_SECRET -e $DATAPORTEN_REDIRECT_URI -e $AWS_ACCESS_KEY_ID -e $AWS_SECRET_ACCESS_KEY -e $GOOGLE_DRIVE_API_KEY $dockerImage python manage.py test
+docker run \
+    -e DATAPORTEN_ID=$DATAPORTEN_ID \
+    -e DATAPORTEN_SECRET=$DATAPORTEN_SECRET \
+    -e DATAPORTEN_REDIRECT_URI=$DATAPORTEN_REDIRECT_URI \
+    -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+    -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+    -e GOOGLE_DRIVE_API_KEY=$GOOGLE_DRIVE_API_KEY \
+    $dockerImage python manage.py test
 echo "ran"
 
 echo "Success"
