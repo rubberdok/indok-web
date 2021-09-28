aeaae8704535af0ce4f86fb3b1a97e3903cf4463
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-09-28 11:13:54 +0200

    fix: add missing interpolation

--- backend/test.sh
+++ backend/test.sh
@@ -15,7 +15,7 @@ fi
 echo "found image"
 
 echo "trying to run"
-docker run -e DATAPORTEN_ID -e DATAPORTEN_SECRET -e DATAPORTEN_REDIRECT_URI -e AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY -e GOOGLE_DRIVE_API_KEY $dockerImage python manage.py test
+docker run -e $DATAPORTEN_ID -e $DATAPORTEN_SECRET -e $DATAPORTEN_REDIRECT_URI -e $AWS_ACCESS_KEY_ID -e $AWS_SECRET_ACCESS_KEY -e $GOOGLE_DRIVE_API_KEY $dockerImage python manage.py test
 echo "ran"
 
 echo "Success"
