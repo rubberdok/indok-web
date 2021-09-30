c23bbc7ad01850e771480233359595595764959e
Sebastian Cheng <sebast1908@gmail.com>
2021-09-30 19:55:04 +0200

    Make blog post model

--- backend/apps/blogs/models.py
+++ backend/apps/blogs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class BlogPost(models.Model):
+
+    title = models.CharField(max_length=150)
+    text = models.TextField()
+    publishDate = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.title}"
