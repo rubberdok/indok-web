27419f80110e255c6fe58760cf34ce361a0943de
Sebastian Cheng <sebast1908@gmail.com>
2021-09-30 20:00:41 +0200

    Make resolvers for all blog posts and by id

---
+++ backend/apps/blogs/resolvers.py
@@ -0,0 +1,11 @@
+from .models import BlogPost as BlogPostModel
+
+class BlogPostResolvers:
+    def resolve_all_blog_posts(self, info):
+        return BlogPostModel.objects.all()
+
+    def resolve_blog_post_by_id(self, info, blog_post_id):
+        try:
+            return BlogPostModel.objects.get(pk=blog_post_id)
+        except BlogPostModel.DoesNotExist:
+            return None
