1085bf99cea5367a4edb5f47949c727e96a1df6a
hermannm <hermann.morkrid@gmail.com>
2021-09-30 19:43:22 +0200

    refactor rubberdokMembers into constants.ts, separate Props type in HallOfFame

--- frontend/src/components/Footer/HallOfFame/index.tsx
+++ frontend/src/components/Footer/HallOfFame/index.tsx
@@ -1,42 +1,6 @@
 import { Dialog, DialogTitle, Typography, IconButton, DialogContent, makeStyles, Grid, Box } from "@material-ui/core";
 import { Close } from "@material-ui/icons";
-
-const rubberdokMembers: {
-  [year: string]: {
-    name: string;
-    class: number;
-    position?: string;
-  }[];
-} = {
-  "2021/2022": [
-    { name: "Lars Waage", class: 4, position: "Prosjektleder" },
-    { name: "Hermann Mørkrid", class: 4, position: "Prosjektleder" },
-    { name: "Morgan Heggland", class: 5 },
-    { name: "Jorunn Leithe", class: 4 },
-    { name: "Jørgen Rosager", class: 4 },
-    { name: "Mathilde Marie Solberg", class: 4 },
-    { name: "Erik Thinn Tvedt", class: 3 },
-    { name: "Sebastian Cheng", class: 3 },
-    { name: "Herman Holmøy", class: 2 },
-    { name: "Ragnhild Bodsberg", class: 2 },
-    { name: "Johan Haga Mohn", class: 1 },
-  ],
-  "2020/2021": [
-    { name: "Morgan Heggland", class: 4, position: "Prosjektleder" },
-    { name: "Lars Lien Ankile", class: 4, position: "Lederpar HS" },
-    { name: "Andreas Johannesen", class: 4, position: "Lederpar HS" },
-    { name: "Sverre Spetalen", class: 5 },
-    { name: "Anna Sofie Lunde", class: 4 },
-    { name: "Fredrik Ahlborg", class: 4 },
-    { name: "Ingrid Aaseng", class: 4 },
-    { name: "Patrik Kjærran", class: 4 },
-    { name: "Hermann Mørkrid", class: 3 },
-    { name: "Lars Waage", class: 3 },
-    { name: "Mathilde Marie Solberg", class: 3 },
-    { name: "Herman Holmøy", class: 1 },
-    { name: "Mathias Raa", class: 1 },
-  ],
-};
+import { rubberdokMembers } from "@components/Footer/HallOfFame/constants";
 
 const useStyles = makeStyles((theme) => ({
   closeButton: {
@@ -50,10 +14,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const HallOfFame: React.VFC<{
+type Props = {
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-}> = ({ open, setOpen }) => {
+};
+
+const HallOfFame: React.VFC<Props> = ({ open, setOpen }) => {
   const classes = useStyles();
 
   return (
