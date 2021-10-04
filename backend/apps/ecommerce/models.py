412f8da9de0f08b3f744b47cbd011754b56b00fa
Jørgen Rosager <joergen.rosager@gmail.com>
2021-10-04 19:58:49 +0200

    functioning migration w. product/event/signup/order

--- backend/apps/ecommerce/models.py
+++ backend/apps/ecommerce/models.py
@@ -5,7 +5,6 @@ from django.db import models
 from django.db.models.fields import DateTimeField
 
 from apps.organizations.models import Organization
-from apps.events.models import Event, SignUp
 
 
 class Product(models.Model):
@@ -13,7 +12,6 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=11, decimal_places=2)
     description = models.TextField()
     organization = models.ForeignKey(Organization, on_delete=models.CASCADE, related_name="products")
-    event = models.OneToOneField(Event, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -42,7 +40,6 @@ class Order(models.Model):
     date = DateTimeField(auto_now_add=True)
     auth_token = models.CharField(max_length=32, default=get_auth_token)  # For authenticating Vipps callback
     payment_attempt = models.IntegerField(default=1)
-    sign_up = models.OneToOneField(SignUp, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return f"Order(product={self.product}, user={self.user})"
