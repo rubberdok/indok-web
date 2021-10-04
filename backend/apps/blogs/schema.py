7813b8ba73aaed01bf7e1470f438050966b41bff
Sebastian Cheng <sebast1908@gmail.com>
2021-10-04 18:37:46 +0200

    Add delete and update mutation

--- backend/apps/blogs/schema.py
+++ backend/apps/blogs/schema.py
@@ -1,12 +1,15 @@
 from apps.blogs.mutations import CreateBlogPost
 import graphene
 
-from .mutations import CreateBlogPost
+from .mutations import CreateBlogPost, DeleteBlogPost, UpdateBlogPost
 from .types import BlogPostType
 from .resolvers import BlogPostResolvers
 
 class BlogPostMutations(graphene.ObjectType):
-    create_blogPost = CreateBlogPost.Field()
+    create_blog_post = CreateBlogPost.Field()
+    delete_blog_post = DeleteBlogPost.Field()
+    update_blog_post = UpdateBlogPost.Field()
+    
 
 class BlogPostQueries(graphene.ObjectType, BlogPostResolvers):
     all_blog_posts = graphene.List(BlogPostType)
