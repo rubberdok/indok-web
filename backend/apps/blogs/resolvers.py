dee623385c7eaaf724f6a950b18eebce794a206c
Sebastian Cheng <sebast1908@gmail.com>
2021-10-07 17:45:40 +0200

    Fix flake8 linting

--- backend/apps/blogs/resolvers.py
+++ backend/apps/blogs/resolvers.py
@@ -5,6 +5,7 @@ from apps.blogs import models
 class BlogPostResolvers:
     def resolve_all_blog_posts(self, info):
         return models.BlogPost.objects.all()
+
     def resolve_blog_post_by_id(self, info, blog_post_id: int) -> Optional[models.BlogPost]:
         try:
             return models.BlogPost.objects.get(pk=blog_post_id)
