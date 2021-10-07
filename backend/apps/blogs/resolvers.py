9cddf812d06b983e8404f0cfa95d6460932e426e
Sebastian Cheng <sebast1908@gmail.com>
2021-10-07 20:04:43 +0200

    Change detail resolve method-name

--- backend/apps/blogs/resolvers.py
+++ backend/apps/blogs/resolvers.py
@@ -6,7 +6,7 @@ class BlogPostResolvers:
     def resolve_all_blog_posts(self, info):
         return models.BlogPost.objects.all()
 
-    def resolve_blog_post_by_id(self, info, blog_post_id: int) -> Optional[models.BlogPost]:
+    def resolve_blog_post(self, info, blog_post_id: int) -> Optional[models.BlogPost]:
         try:
             return models.BlogPost.objects.get(pk=blog_post_id)
         except models.BlogPost.DoesNotExist:
