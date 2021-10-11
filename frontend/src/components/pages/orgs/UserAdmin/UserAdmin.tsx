03b9db28a392d0031cc915c910881caa9e62915f
erikttv <erikttv@stud.ntnu.no>
2021-10-11 17:57:49 +0200

    fetch users inside org and display in list

--- frontend/src/components/pages/orgs/UserAdmin/UserAdmin.tsx
+++ frontend/src/components/pages/orgs/UserAdmin/UserAdmin.tsx
@@ -1,12 +1,44 @@
+import React from "react";
 import { useQuery, gql } from "@apollo/client";
 import { useRouter } from "next/router";
 import { User } from "@interfaces/users";
-import { Grid } from "@material-ui/core";
+import {
+  Grid,
+  Box,
+  Typography,
+  Card,
+  CardHeader,
+  Divider,
+  CardContent,
+  TableContainer,
+  Table,
+  TableRow,
+  TableHead,
+  TableCell,
+  TableBody,
+  makeStyles,
+  Button,
+} from "@material-ui/core";
+import { Organization } from "@interfaces/organizations";
+import { Delete } from "@material-ui/icons";
 
-const EditUsersInOrganization: React.FC = () => {
+const useStyles = makeStyles(() => ({
+  hover: {
+    "&:hover": {
+      cursor: "default",
+    },
+  },
+}));
+
+type Props = {
+  organization: Organization;
+};
+
+const EditUsersInOrganization: React.FC<Props> = ({ organization }) => {
   const router = useRouter();
   const { orgId } = router.query;
   const orgNumberId = parseInt(orgId as string);
+  const classes = useStyles();
 
   const { error, loading, data } = useQuery<{
     organization: { users: User[] };
@@ -16,6 +48,9 @@ const EditUsersInOrganization: React.FC = () => {
         organization(id: $id) {
           users {
             username
+            firstName
+            lastName
+            email
           }
         }
       }
@@ -25,16 +60,69 @@ const EditUsersInOrganization: React.FC = () => {
 
   if (error) return <p>Error</p>;
   if (loading) return <p>Loading...</p>;
+  /* Fetching user data and then mapping it to components
+  {data &&
+    data.organization.users?.map((user) => (
+      <Grid item key={user.username}>
+        {user.username}
+      </Grid>
+    ))}
+  */
 
   return (
-    <Grid container>
-      {data &&
-        data.organization.users?.map((user) => (
-          <Grid item key={user.username}>
-            {user.username}
+    <Box m={10}>
+      {data?.organization?.users ? (
+        <Grid container spacing={10}>
+          <Grid item>
+            <Typography variant="h1" align="center">
+              {organization.name}
+            </Typography>
           </Grid>
-        ))}
-    </Grid>
+          <Grid item container>
+            <Grid item xs>
+              <Card variant="outlined">
+                <CardHeader title="Rediger medlemmer" />
+                <Divider variant="middle" />
+                {data.organization.users ? (
+                  <CardContent>
+                    <TableContainer>
+                      <Table>
+                        <TableHead>
+                          <TableRow>
+                            <TableCell>Navn</TableCell>
+                            <TableCell>Email</TableCell>
+                            <TableCell />
+                            <TableCell />
+                          </TableRow>
+                        </TableHead>
+                        <TableBody>
+                          {data?.organization?.users.map((user: User, index) => (
+                            <TableRow className={classes.hover} key={index} hover>
+                              <TableCell>{user.firstName + " " + user.lastName}</TableCell>
+                              <TableCell>{user.email}</TableCell>
+                              <TableCell size="small" align="right">
+                                <Button variant="contained" color="primary">
+                                  Rediger rolle
+                                </Button>
+                              </TableCell>
+                              <TableCell size="small" align="right">
+                                <Button variant="contained" color="primary" startIcon={<Delete />}>
+                                  Fjern fra gruppe
+                                </Button>
+                              </TableCell>
+                            </TableRow>
+                          ))}
+                        </TableBody>
+                      </Table>
+                    </TableContainer>
+                  </CardContent>
+                ) : null}
+              </Card>
+            </Grid>
+          </Grid>
+        </Grid>
+      ) : null}
+    </Box>
   );
 };
 
--- frontend/src/pages/orgs/[orgId]/admin.tsx
+++ frontend/src/pages/orgs/[orgId]/admin.tsx
@@ -1,15 +1,22 @@
 import Layout from "@components/Layout";
 import { NextPage } from "next";
+import { useQuery } from "@apollo/client";
+import { Organization } from "@interfaces/organizations";
+import { GET_ORGANIZATION } from "@graphql/orgs/queries";
+import { useRouter } from "next/router";
 import EditUsersInOrganization from "@components/pages/orgs/UserAdmin/UserAdmin";
 
+const AdminPage: NextPage = () => {
+  const { orgId } = useRouter().query;
 
-const AdminPage: NextPage = () =>{
+  const { loading, error, data } = useQuery<{ organization: Organization }>(GET_ORGANIZATION, {
+    variables: { orgId: orgId as string },
+  });
 
-    return(
-        <Layout>
-          <EditUsersInOrganization/>
-        </Layout>
-    );
-}
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error</p>;
+
+  return <Layout>{data && <EditUsersInOrganization organization={data?.organization} />}</Layout>;
+};
 
 export default AdminPage;
