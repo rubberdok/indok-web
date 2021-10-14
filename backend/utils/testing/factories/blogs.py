6465618632ec162df73c37d2254eb47030bb0b36
Sebastian Cheng <sebast1908@gmail.com>
2021-10-14 18:19:20 +0200

    Make factory for blog

--- backend/utils/testing/factories/blogs.py
+++ backend/utils/testing/factories/blogs.py
@@ -1,12 +1,21 @@
 from factory.django import DjangoModelFactory
 import factory, factory.django
 from faker import Faker
-from apps.blogs.models import BlogPost
+from apps.blogs.models import Blog, BlogPost
 from utils.testing.factories.users import UserFactory
+from utils.testing.factories.organizations import OrganizationFactory
 
 
 fake: Faker = Faker(["no-NO"])
 
+
+class BlogFactory(DjangoModelFactory):
+    class Meta:
+        model = Blog
+
+    name = fake.company()
+    description = fake.paragraph(nb_sentences=10)
+    organization = factory.SubFactory(OrganizationFactory)
 
 class BlogPostFactory(DjangoModelFactory):
     class Meta:
