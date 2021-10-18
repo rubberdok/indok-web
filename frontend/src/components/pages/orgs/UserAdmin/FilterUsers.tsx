805801024719c6fdaf641a7f64953600d28287ad
erikttv <erikttv@stud.ntnu.no>
2021-10-18 18:24:36 +0200

    add dynamic filterbuttons

--- frontend/src/components/pages/orgs/UserAdmin/FilterUsers.tsx
+++ frontend/src/components/pages/orgs/UserAdmin/FilterUsers.tsx
@@ -1,23 +1,29 @@
 import { Organization } from "@interfaces/organizations";
 import React, { useEffect, useState } from "react";
 import FilterButtons from "@components/pages/archive/FilterButtons";
-import SearchBar from "material-ui-search-bar";
+import SearchBar from "@components/pages/archive/SearchBar";
 import { useRouter } from "next/router";
 import { Grid } from "@material-ui/core";
 import { gql, useQuery } from "@apollo/client";
-import { User } from "@interfaces/users";
 
 type Props = {
   organization: Organization;
 };
 
+//TODO: filter when clicking on a group or writing in searchbar
 const FilterUsers: React.FC<Props> = ({ organization }) => {
   const router = useRouter();
   const { orgId } = router.query;
   const orgNumberId = parseInt(orgId as string);
 
+  const [searchFilter, setSearchFilter] = useState("");
+
+  const [viewFeatured, setViewFeatured] = useState(true);
+
+  const [typeFilters, setTypeFilters] = useState<{ [Key: string]: { active: boolean; title: string } }>({});
+
   const { error, loading, data } = useQuery<{
-    organization: { permissionGroups: { permissionGroups: { name: string; uuid: string } } };
+    organization: { permissionGroups: { name: string; uuid: string }[] };
   }>(
     gql`
       query organization($id: ID) {
@@ -32,22 +38,21 @@ const FilterUsers: React.FC<Props> = ({ organization }) => {
     { variables: { id: orgNumberId } }
   );
 
-  //TODO: make dynamic
-  const [typeFilters, setTypeFilters] = useState<{ [key: string]: { active: boolean; title: string } }>({
-    Budget: { active: false, title: "Budsjett og Regnskap" },
-    Summary: { active: false, title: "Generalforsamling" },
-    Yearbook: { active: false, title: "Årbøker" },
-    Guidelines: { active: false, title: "Støtte fra HS" },
-    Regulation: { active: false, title: "Foreningens lover" },
-    Statues: { active: false, title: "Utveksling" },
-    Others: { active: false, title: "Annet" },
-  });
-
-  const [viewFeatured, setViewFeatured] = useState(true);
+  // Fetching correct buttons dynamically
+  useEffect(() => {
+    const buttons: { [key: string]: { active: boolean; title: string } } = {};
+    data?.organization.permissionGroups.forEach((group) => {
+      buttons[group.uuid] = {
+        title: group.name,
+        active: false,
+      };
+    });
+    setTypeFilters(buttons);
+  }, [data?.organization]);
 
   if (error) return <p>Error</p>;
   if (loading) return <p>Loading...</p>;
-
+  //      <p>{data?.organization.permissionGroups[1].name}</p>
   return (
     <>
       <Grid item xs={12} md={6}>
@@ -65,7 +70,14 @@ const FilterUsers: React.FC<Props> = ({ organization }) => {
         />
       </Grid>
       <Grid item xs={8} md={6}>
-        <SearchBar></SearchBar>
+        <SearchBar
+          searchFilter={searchFilter}
+          handleSearchFilterChanged={(newValue: string) => {
+            [setSearchFilter(newValue), setViewFeatured(false)];
+          }}
+          handleSearchFilterCanceled={() => setSearchFilter("")}
+          placeholder="Søk på medlemmer"
+        />
       </Grid>
     </>
   );
