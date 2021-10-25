5936aeb83840fc0940e4129b4f5b468926d810b8
ranhildbodsberg <91271420+ragnhildbodsberg@users.noreply.github.com>
2021-10-25 20:42:17 +0200

    Add fields to types

--- backend/apps/blogs/types.py
+++ backend/apps/blogs/types.py
@@ -6,8 +6,22 @@ from .models import BlogPost as BlogPostModel, Blog as BlogModel
 class BlogType(DjangoObjectType):
     class Meta:
         model = BlogModel
+        fields = [
+            "id",
+            "description",
+            "organization",
+            "name",
+        ]
 
 
 class BlogPostType(DjangoObjectType):
     class Meta:
         model = BlogPostModel
+        fields = [
+            "id",
+            "title",
+            "text",
+            "publish_date",
+            "author",
+            "blog",
+        ]
