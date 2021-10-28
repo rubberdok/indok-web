26c26991221bac541081a455ea79ef92b73c2a6d
ranhildbodsberg <91271420+ragnhildbodsberg@users.noreply.github.com>
2021-10-28 19:50:06 +0200

    Make organization page

---
+++ frontend/src/pages/[org]/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Layout from "@components/Layout";
+
+const OrgPage: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Org</h1>;
+    </Layout>
+  );
+};
+
+export default OrgPage;
