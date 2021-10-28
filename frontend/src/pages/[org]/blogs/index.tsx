3c96352adc87385e3bff35ea4e87032839f50fc3
ranhildbodsberg <91271420+ragnhildbodsberg@users.noreply.github.com>
2021-10-28 19:50:31 +0200

    Make main page for blog

---
+++ frontend/src/pages/[org]/blogs/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "@components/Layout";
+import { NextPage } from "next";
+
+const MainBlogPage: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Main page</h1>
+    </Layout>
+  );
+};
+
+export default MainBlogPage;
