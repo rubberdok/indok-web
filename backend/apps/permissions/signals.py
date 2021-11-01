67529ecd7d1a9da84ddd41ee38403895193d8386
hermannm <hermann.morkrid@gmail.com>
2021-11-01 19:15:07 +0100

    refactor group name post_migrate signal to use post_save

--- backend/apps/permissions/signals.py
+++ backend/apps/permissions/signals.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.contrib.auth.models import Group, Permission
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
-from django.db.models.signals import post_migrate, pre_save
+from django.db.models.signals import post_migrate, post_save, pre_save
 from django.dispatch import receiver
 
 from apps.permissions.constants import DEFAULT_GROUPS, DEFAULT_ORGANIZATION_PERMISSIONS, ORGANIZATION
@@ -23,6 +23,17 @@ def create_named_group(sender, instance: ResponsibleGroup, **kwargs):
         prefix: str = instance.organization.name
         group = Group.objects.create(name=f"{prefix}:{instance.name}:{uuid4().hex}")
         instance.group = group
+
+
+@receiver(post_save, sender=ResponsibleGroup)
+def synchronize_group_and_responsible_group_names(instance: ResponsibleGroup, **kwargs):
+    """
+    Synchronizes the names of organizations' responsible groups and their attached Groups
+    """
+    group_name_split: list[str] = instance.group.name.split(":", 2)
+    if len(group_name_split) == 3:
+        if group_name_split[1] != instance.name:
+            instance.group.name = f"{group_name_split[0]}:{instance.name}:{group_name_split[2]}"
 
 
 @receiver(post_migrate)
@@ -59,20 +70,3 @@ def assign_standard_organization_permissions(apps, **kwargs):
         .exclude(organizations=None)
         .values_list("id", flat=True)
     )
-
-
-@receiver(post_migrate)
-def synchronize_group_and_responsible_group_names(apps, **kwargs):
-    """
-    Synchronizes the names of organizations' responsible groups and their attached Groups
-    """
-    Organization = apps.get_model("organizations", "Organization")
-
-    for organization in Organization.objects.all():
-        for responsible_group in organization.permission_groups.all():
-            group_name_split: list[str] = responsible_group.group.name.split(":", 2)
-            if len(group_name_split) == 3:
-                if group_name_split[1] != responsible_group.name:
-                    responsible_group.group.name = (
-                        f"{group_name_split[0]}:{responsible_group.name}:{group_name_split[2]}"  # noqa
-                    )
