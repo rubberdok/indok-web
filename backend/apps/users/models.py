140f4893a7e9036ad7e8531dbf5fb6c9035c37e4
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-11-01 15:57:56 +0100

    fix: set id_token to 8k instead of allergies

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -14,8 +14,8 @@ from apps.events.models import Event, SignUp
 class User(AbstractUser):
     feide_userid = models.CharField(max_length=255, db_index=True)
     feide_email = models.EmailField(blank=True, default="")
-    id_token = models.CharField(max_length=1000, blank=True, default="")
-    allergies = models.CharField(max_length=8000, blank=True, default="")
+    id_token = models.CharField(max_length=8000, blank=True, default="")
+    allergies = models.CharField(max_length=1000, blank=True, default="")
     phone_number = PhoneNumberField(blank=True)
     first_login = models.BooleanField(default=True)
     graduation_year = models.IntegerField(null=True, blank=True)
