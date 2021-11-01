1776fe1929a2d14fa7b9fb02da5c9fd40d7934b0
Sebastian <sebast1908gmail.com>
2021-11-01 20:55:05 +0100

    Add blog main page

--- frontend/src/pages/blogs/[id]/index.tsx
+++ frontend/src/pages/blogs/[id]/index.tsx
@@ -1,10 +1,36 @@
 import Layout from "@components/Layout";
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useQuery } from "@apollo/client";
+import { BlogPost } from "../../../types/blogs";
+import { QUERY_ALL_BLOG_POSTS_THUMBNAIL } from "@graphql/blogs/queries";
+import { BlogPostThumbnail } from "@components/pages/blogs/BlogPostThumbnail";
+import { Grid, Container, Box } from "@material-ui/core";
 
 const MainBlogPage: NextPage = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, error } = useQuery<{ blogPost: BlogPost[] }>(QUERY_ALL_BLOG_POSTS_THUMBNAIL, {
+    variables: { blogId: id },
+  });
+
+  const BlogPostThumbnails: React.VFC = () => {
+    return (
+      <Grid container>
+        <BlogPostThumbnail title={"innlegg"} />
+        <BlogPostThumbnail title={"innlegg"} />
+        <BlogPostThumbnail title={"innlegg"} />
+      </Grid>
+    );
+  };
+
   return (
     <Layout>
-      <h1>Main page</h1>
+      <Container>
+        <Box>
+          <BlogPostThumbnails />
+        </Box>
+      </Container>
     </Layout>
   );
 };
