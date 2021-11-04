2768359c65cdfb49f9d1f73b663382f112a84d2e
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-11-04 11:06:09 +0100

    fix: skip test database, run loadfactories directly

--- backend/entrypoint.test.sh
+++ backend/entrypoint.test.sh
@@ -2,6 +2,7 @@
 set -e
 
 python manage.py migrate
-python manage.py integrationserver --no-input
+python manage.py loadfactories
+python manage.py collectstatic --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
 exec "$@"
