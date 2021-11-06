55242f90dce8ef75ec9e593ea34bb4c5b6b34a0b
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-11-06 01:37:25 +0100

    refactor: validate frontend environment variables during build

---
+++ frontend/src/utils/config.ts
@@ -0,0 +1,15 @@
+const getEnvironmentVariable = (environmentVariable: string): string => {
+  const unvalidatedEnvironmentVariable = process.env[environmentVariable];
+  if (!unvalidatedEnvironmentVariable) {
+    throw new Error(`Couldn't find environment variable: ${environmentVariable}`);
+  }
+  return unvalidatedEnvironmentVariable;
+};
+
+export const config = {
+  dataportenId: getEnvironmentVariable("NEXT_PUBLIC_DATAPORTEN_ID"),
+  dataportenRedirectUri: getEnvironmentVariable("NEXT_PUBLIC_DTAPORTEN_REDIRECT_URI"),
+  dataportenState: getEnvironmentVariable("NEXT_PUBLIC_DATAPORTEN_STATE"),
+  graphqlEndpoint: getEnvironmentVariable("NEXT_PUBLIC_GRAPHQL_BACKEND_URI"),
+  sentryDsn: getEnvironmentVariable("NEXT_PUBLIC_SENTRY_DSN"),
+};
