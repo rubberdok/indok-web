bfe35257ba97f64654448cd258875063ce414e73
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-11-09 10:30:25 +0100

    refactor: update db service name in entrypoint, remove cypress debug

--- backend/entrypoints/local.sh
+++ backend/entrypoints/local.sh
@@ -2,5 +2,5 @@
 set -e
 
 python manage.py migrate
-./wait-for-it.sh db:5432 -- python manage.py runserver 0.0.0.0:8000
+./wait-for-it.sh postgres:5432 -- python manage.py runserver 0.0.0.0:8000
 exec "$@"
