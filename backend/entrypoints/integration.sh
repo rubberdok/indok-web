681845b023fbafe6f3ae9a28b8b3d40f52dd212c
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-11-09 10:20:38 +0100

    ci(integration): move wait-for-it to backend root folder

--- backend/entrypoints/integration.sh
+++ backend/entrypoints/integration.sh
@@ -4,5 +4,5 @@ set -e
 python manage.py migrate
 python manage.py loadfactories
 python manage.py collectstatic --noinput
-./wait-for-it.sh postgres:5432 gunicorn config.wsgi:application --bind 0.0.0.0:8000
+./wait-for-it.sh postgres:5432 -- gunicorn config.wsgi:application --bind 0.0.0.0:8000
 exec "$@"
