ef14ef4da5ed188aefe389fc577c3d6133ab6ee9
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-11-09 11:46:31 +0100

    refactor: improve error messages for envs

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,15 +1,21 @@
-const validateEnvironmentVariable = (environmentVariable: string | undefined): string => {
+const validateEnvironmentVariable = (environmentVariable: string | undefined, name: string): string => {
   if (!environmentVariable) {
-    throw new Error(`Couldn't find environment variable: ${environmentVariable}`);
+    throw new Error(`Couldn't find environment variable: ${name}`);
   }
   return environmentVariable;
 };
 
 export const config = {
-  dataportenId: validateEnvironmentVariable(process.env.NEXT_PUBLIC_DATAPORTEN_ID),
-  dataportenRedirectUri: validateEnvironmentVariable(process.env.NEXT_PUBLIC_REDIRECT_URI),
-  dataportenState: validateEnvironmentVariable(process.env.NEXT_PUBLIC_DATAPORTEN_STATE),
-  graphqlEndpoint: validateEnvironmentVariable(process.env.NEXT_PUBLIC_GRAPHQL_BACKEND_URI),
-  sentryDsn: validateEnvironmentVariable(process.env.NEXT_PUBLIC_SENTRY_DSN),
-  frontendUri: validateEnvironmentVariable(process.env.NEXT_PUBLIC_FRONTEND_URI),
+  dataportenId: validateEnvironmentVariable(process.env.NEXT_PUBLIC_DATAPORTEN_ID, "NEXT_PUBLIC_DATAPORTEN_ID"),
+  dataportenRedirectUri: validateEnvironmentVariable(process.env.NEXT_PUBLIC_REDIRECT_URI, "NEXT_PUBLIC_REDIRECT_URI"),
+  dataportenState: validateEnvironmentVariable(
+    process.env.NEXT_PUBLIC_DATAPORTEN_STATE,
+    "NEXT_PUBLIC_DATAPORTEN_STATE"
+  ),
+  graphqlEndpoint: validateEnvironmentVariable(
+    process.env.NEXT_PUBLIC_GRAPHQL_BACKEND_URI,
+    "NEXT_PUBLIC_GRAPHQL_BACKEND_URI"
+  ),
+  sentryDsn: validateEnvironmentVariable(process.env.NEXT_PUBLIC_SENTRY_DSN, "NEXT_PUBLIC_SENTRY_DSN"),
+  frontendUri: validateEnvironmentVariable(process.env.NEXT_PUBLIC_FRONTEND_URI, "NEXT_PUBLIC_FRONTEND_URI"),
 };
