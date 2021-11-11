b692e3573509a59956e48b96c5e8f3da07d0b176
hermannm <hermann.morkrid@gmail.com>
2021-11-11 17:32:56 +0100

    fix type hinting and add comments

--- backend/utils/permissions.py
+++ backend/utils/permissions.py
@@ -7,17 +7,18 @@ if TYPE_CHECKING:
     from django.db.models import Model
 
 
-def assign_object_permissions(app_name: str, model_name: str, instance: Model, organization: Organization) -> None:
+def assign_object_permissions(app_name: str, model_name: str, instance: "Model", organization: "Organization") -> None:
     """
     Takes in a newly created instance of a model, and the organization tied to it.
     Assigns permissions on the instance to the organization's appropriate permission groups.
     """
     for permission_group in organization.permission_groups.all():
+        # Using try-except due to Python's "Easier to Ask for Forgiveness than Permission" principle
         try:
             permissions = DEFAULT_ORG_PERMISSION_GROUPS[permission_group.group_type][app_name][model_name]
 
             for permission in permissions:
                 assign_perm(f"{app_name}.{permission}", permission_group.group, instance)
 
-        except (KeyError):
+        except KeyError:
             continue
