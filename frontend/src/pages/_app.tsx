3a2357657966ac6dd62be4dcb5e290181d1216b7
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-11-21 16:05:03 +0100

    feat: capture, send exceptions to sentry frontend

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -8,7 +8,9 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import React, { useEffect } from "react";
 
-const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+type AppPropsWithError = AppProps & { err: Error };
+
+const App = ({ Component, pageProps, err }: AppPropsWithError): JSX.Element => {
   const link = createHttpLink({
     uri: config.GRAPHQL_ENDPOINT,
     credentials: "include",
@@ -35,10 +37,7 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
       </Head>
       <ThemeProvider theme={responsiveTheme}>
         <CssBaseline />
-        <Component {...pageProps} />
-        {/* <div id="mobile-warning">
-          Denne siden fungerer ikke optimalt på mobil enda. Prøv nettsiden på en større skjerm.
-        </div> */}
+        <Component {...pageProps} err={err} />
       </ThemeProvider>
     </ApolloProvider>
   );
