9c265f35ef6eaf9a67c61c6c59f3c20b05a623be
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-11-21 16:05:03 +0100

    feat: capture, send exceptions to sentry frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -16,6 +16,26 @@ const moduleExports = {
       },
     ];
   },
+  webpack: (config, { isServer }) => {
+    // In `pages/_app.js`, Sentry is imported from @sentry/browser. While
+    // @sentry/node will run in a Node.js environment. @sentry/node will use
+    // Node.js-only APIs to catch even more unhandled exceptions.
+    //
+    // This works well when Next.js is SSRing your page on a server with
+    // Node.js, but it is not what we want when your client-side bundle is being
+    // executed by a browser.
+    //
+    // Luckily, Next.js will call this webpack function twice, once for the
+    // server and once for the client. Read more:
+    // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+    //
+    // So ask Webpack to replace @sentry/node imports with @sentry/browser when
+    // building the browser's bundle
+    if (!isServer) {
+      config.resolve.alias["@sentry/node"] = "@sentry/browser";
+    }
+    return config;
+  },
   sentry: {
     // Disable the Sentry CLI and manually manage releases and source maps
     // To make Sentry work more nicely with CI/CD, as build != release for us.
