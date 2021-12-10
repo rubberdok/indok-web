c6c3756d7629b882c263cdbb64f00fbac6c61c7d
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-12-10 19:43:26 +0100

    fix: fix regex, re-add `styles.ts`

--- frontend/src/components/pages/profile/UserForm/helpers.ts
+++ frontend/src/components/pages/profile/UserForm/helpers.ts
@@ -29,5 +29,5 @@ export const validationSchema = Yup.object().shape({
   phoneNumber: Yup.string()
     .min(8, "Telefonnummeret må være 8 tegn eller lenger.")
     .max(12, "Telefonnummeret kan ikke være mer enn 12 tegn.")
-    .matches(/(0047|\+47|47)?(4|9)\d{7}/, "Må være et gyldig telefonnummer."),
+    .matches(/(0047|\+47|47)?[49]\d{7}/, "Må være et gyldig telefonnummer."),
 });
