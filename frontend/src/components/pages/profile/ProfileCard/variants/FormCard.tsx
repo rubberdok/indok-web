b6bdc548b760e9f28cf6b6274d1879872226d2ff
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-12-10 16:36:17 +0100

    :recycle: refactor: standardize profile card actions

--- frontend/src/components/pages/profile/ProfileCard/variants/FormCard.tsx
+++ frontend/src/components/pages/profile/ProfileCard/variants/FormCard.tsx
@@ -1,21 +1,10 @@
-import { Button, Grid, Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import Form from "@public/illustrations/Form.svg";
 import ProfileCard from "..";
 
 const FormCard: React.VFC = () => {
   return (
-    <ProfileCard
-      title="Verv"
-      cardActions={
-        <Grid container direction="row" justifyContent="flex-end">
-          <Grid item>
-            <Button>Se verv</Button>
-          </Grid>
-        </Grid>
-      }
-      image={Form}
-      alt=""
-    >
+    <ProfileCard title="Verv" actionText="Se verv" actionLink="/listings" image={Form} alt="">
       <Grid container direction="column">
         <Grid item>
           <Typography variant="body2">Her kan du se en oversikt over alle innsendte søknader på verv.</Typography>
--- frontend/src/components/pages/profile/ProfileCard/variants/OrganizationCard.tsx
+++ frontend/src/components/pages/profile/ProfileCard/variants/OrganizationCard.tsx
@@ -1,21 +1,10 @@
-import { Button, Grid, Typography } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import Organization from "@public/illustrations/Organization.svg";
 import ProfileCard from "..";
 
 const OrganizationCard: React.VFC = () => {
   return (
-    <ProfileCard
-      title="Organisasjoner"
-      cardActions={
-        <Grid container direction="row" justifyContent="flex-end">
-          <Grid item>
-            <Button>Se organisasjoner</Button>
-          </Grid>
-        </Grid>
-      }
-      image={Organization}
-      alt=""
-    >
+    <ProfileCard title="Organisasjoner" actionText="Se organisasjoner" actionLink="/orgs" image={Organization} alt="">
       <Grid container direction="column">
         <Grid item>
           <Typography variant="body2">Her kan du se en oversikt over alle organisasjoner der du er medlem.</Typography>
