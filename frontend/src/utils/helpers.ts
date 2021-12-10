66cb03098ae73fd1832cccc4c01a2a6750f7c020
Lars Waage <46653859+larwaa@users.noreply.github.com>
2021-12-10 19:38:01 +0100

    fix: add default step to range util

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -20,5 +20,5 @@ export const allValuesDefined = (obj: Record<string, any>): boolean => {
   return empty;
 };
 
-export const range = (start: number, stop: number, step: number) =>
+export const range = (start: number, stop: number, step: number = 1) =>
   Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step);
