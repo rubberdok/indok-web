12ec406fda4f912281d40e87feedd6244c523164
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-01-24 19:51:51 +0100

    :recycle: refactor: feide integration tests

--- e2e/cypress/integration/pages/index/feide.spec.ts
+++ e2e/cypress/integration/pages/index/feide.spec.ts
@@ -1,30 +1,22 @@
 describe("SSO login", () => {
-  it("should not prompt registration registered", () => {
+  it("should not prompt registration as a registered user", () => {
     cy.session("asbjørn_elevg", () => {
       cy.log("Accessing site");
       cy.visit("/").then(() => {
-        cy.getByTestId("hero-title").should("contain.text", "Industriell Økonomi og Teknologiledelse");
-        cy.getByTestId("login")
+        cy.getByTestId("login").click();
+        cy.log("Logging in");
+        cy.clearCookies();
+        cy.clearCookies();
+        cy.reload();
+        cy.contains("Feide test users")
           .click()
           .then(() => {
-            cy.log("Logging in");
-            cy.clearCookies();
-            cy.clearCookies();
-            cy.reload();
-            cy.contains("Feide test users")
-              .click()
-              .then(() => {
-                cy.get("[id=username]").type("asbjorn_elevg");
-                cy.get("[id=password]").type("1qaz");
-              });
+            cy.get("[id=username]").type("asbjorn_elevg");
+            cy.get("[id=password]").type("1qaz");
           });
-        cy.get("button")
-          .get("[type=submit]")
-          .click()
-          .then(() => {
-            cy.getByTestId("profile-personal-name").should("contain.text", "Asbjørn ElevG Hansen");
-            cy.log("Logged in");
-          });
+        cy.get("button").get("[type=submit]").click();
+        cy.getByTestId("profile-personal-name").should("contain.text", "Asbjørn ElevG Hansen");
+        cy.log("Logged in");
       });
     });
   });
@@ -32,25 +24,18 @@ describe("SSO login", () => {
   it("should prompt registration when not registered", () => {
     cy.session("cecilie_elevvgs", () => {
       cy.log("Accessing site");
-      cy.visit("/").then(() => {
-        cy.getByTestId("login")
-          .click()
-          .then(() => {
-            cy.log("Logging in");
-            cy.contains("Feide test users").click();
-            cy.get("[id=username]").type("cecilie_elevvgs");
-            cy.get("[id=password]").type("3edc");
-            cy.get("button").get("[type=submit]").click();
-          });
-      });
+      cy.visit("/");
+      cy.getByTestId("login").click();
+      cy.log("Logging in");
+      cy.contains("Feide test users").click();
+      cy.get("[id=username]").type("cecilie_elevvgs");
+      cy.get("[id=password]").type("3edc");
+      cy.get("button").get("[type=submit]").click();
       cy.getByTestId("registerUser-title").should("contain.text", "Registrering");
       cy.getByTestId("registerUser-firstNameTextField").within(() => cy.get("input").clear().type("Abba"));
       cy.getByTestId("registerUser-lastNameTextField").within(() => cy.get("input").clear().type("Baab"));
-      cy.getByTestId("registerUser-saveButton")
-        .click()
-        .then(() => {
-          cy.getByTestId("profile-personal-name").should("contain.text", "Abba Baab");
-        });
+      cy.getByTestId("registerUser-saveButton").click();
+      cy.getByTestId("profile-personal-name").should("contain.text", "Abba Baab");
     });
   });
 });
