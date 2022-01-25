ab3a0bab2da97f93fc21395f7683c41c7362ef0d
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-01-25 13:32:32 +0100

    tests: arbitrarily wait for 10 seconds

--- e2e/cypress/integration/pages/index/feide.spec.ts
+++ e2e/cypress/integration/pages/index/feide.spec.ts
@@ -28,7 +28,7 @@ describe("SSO login", () => {
       cy.getByTestId("registerUser-firstNameTextField").within(() => cy.get("input").clear().type("Abba"));
       cy.getByTestId("registerUser-lastNameTextField").within(() => cy.get("input").clear().type("Baab"));
       cy.getByTestId("registerUser-saveButton").click();
-      cy.reload();
+      cy.wait(10000);
       cy.getByTestId("profile-personal-name").should("contain.text", "Abba Baab");
     });
   });
