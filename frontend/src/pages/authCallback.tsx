4e0a4761ef78d7f34ee8e5858e56f767b2ee7ced
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-01-25 13:03:46 +0100

    refactor: rename skeleton -> ProfileSkeleton

--- frontend/src/pages/authCallback.tsx
+++ frontend/src/pages/authCallback.tsx
@@ -1,6 +1,6 @@
 import { useMutation } from "@apollo/client";
 import Layout from "@components/Layout";
-import ProfileSkeleton from "@components/pages/profile/skeleton";
+import ProfileSkeleton from "@components/pages/profile/ProfileSkeleton";
 import { AUTHENTICATE } from "@graphql/users/mutations";
 import { User } from "@interfaces/users";
 import { Button, Container, Grid, Typography, useTheme } from "@material-ui/core";
