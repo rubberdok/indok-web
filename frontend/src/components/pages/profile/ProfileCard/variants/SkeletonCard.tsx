4688910d3778a7e1cd864dd462b69c1c61c2bda1
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-01-25 12:19:16 +0100

    fix: link in skeleton

--- frontend/src/components/pages/profile/ProfileCard/variants/SkeletonCard.tsx
+++ frontend/src/components/pages/profile/ProfileCard/variants/SkeletonCard.tsx
@@ -4,7 +4,7 @@ import ProfileCardBase from "./ProfileCardBase";
 
 const SkeletonCard: React.VFC = () => (
   <Skeleton variant="rect" width="100%" height="100%">
-    <ProfileCardBase title="Loading" actionText="Loading" actionLink="Loading">
+    <ProfileCardBase title="Loading" actionText="Loading" actionLink="/">
       <Typography variant="body2">Loading</Typography>
       <Typography variant="body2">Loading</Typography>
     </ProfileCardBase>
