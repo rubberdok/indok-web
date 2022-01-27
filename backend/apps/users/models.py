51d2b3693649613fce9b9c43a8870a8009b2ffc3
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-01-27 18:26:21 +0100

    feat: add graduation year update restriction

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -20,6 +20,7 @@ class User(AbstractUser):
     first_login = models.BooleanField(default=True)
     graduation_year = models.IntegerField(null=True, blank=True)
     is_indok = models.BooleanField(default=False)
+    year_updated_at = models.DateTimeField()
 
     @property
     def events(self):
