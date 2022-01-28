e94af2ba65e0c2d86024693d40527e3eee92c6f4
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-01-28 14:43:49 +0100

    feat: graduation year validation, show users year always

--- frontend/src/components/pages/profile/UserForm/helpers.ts
+++ frontend/src/components/pages/profile/UserForm/helpers.ts
@@ -3,6 +3,7 @@ import * as Yup from "yup";
 
 const today = dayjs();
 export const maxGraduationYear = today.month() > 7 ? today.year() + 5 : today.year() + 4;
+const minGraduationYear = today.month() > 7 ? today.year() + 1 : today.year();
 
 export const suggestNames = (name: string | undefined): { firstName: string; lastName: string } => {
   if (!name) {
@@ -26,7 +27,10 @@ export const validationSchema = Yup.object().shape({
   lastName: Yup.string().required("Etternavn kan ikke være tomt.").min(2, "Kan ikke være kortere enn to tegn."),
   email: Yup.string().email("Oppgi en gyldig e-postadresse.").notRequired(),
   allergies: Yup.string().notRequired(),
-  graduationYear: Yup.number().required().min(today.year()).max(maxGraduationYear),
+  graduationYear: Yup.number()
+    .required()
+    .min(minGraduationYear, `Kan ikke være før ${minGraduationYear}`)
+    .max(maxGraduationYear, `Kan ikke være etter ${maxGraduationYear}`),
   phoneNumber: Yup.string()
     .min(8, "Telefonnummeret må være 8 tegn eller lenger.")
     .max(12, "Telefonnummeret kan ikke være mer enn 12 tegn.")
