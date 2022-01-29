f392a9d474f37ae21a5fcbfb94df5085279e7beb
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-01-29 16:25:35 +0100

    fix: manually update user cache after authUser

--- frontend/src/graphql/users/queries.ts
+++ frontend/src/graphql/users/queries.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 import { USER_FRAMGENT } from "./fragments";
 
 export const GET_USER_PROFILE = gql`
-  query {
+  query userProfile {
     user {
       username
       firstName
