8b6662e0c449f40fe83dcc0e46304f5365d0e37f
Jørgen Rosager <joergen.rosager@gmail.com>
2022-01-31 19:17:15 +0100

    working celery + django + rabbitmq :D

--- backend/celery_worker/celery.py
+++ backend/celery_worker/celery.py
@@ -1,21 +1,41 @@
-from __future__ import absolute_import
-from celery import Celery
-
-# import os
+# noqa# noqa# noqa# noqa# noqa# noqa# noqa# noqa# noqa# noqa# noqafrom __future__ import absolute_import
+from celery import Celery, shared_task
+import os
+import django
 
 # Set the default Django settings module for the 'celery' program.
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celery_worker.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")  # noqa
+django.setup()  # noqa
+# noqa# noqa# noqa# noqa
+from apps.users.models import User  # noqa: E402
+from django.conf import settings  # noqa
 
+# noqa# noqa# noqa
 app = Celery(
-    "celery_worker", broker="amqp://user:password@rabbitmq:5672/"  # , backend="rpc://"
+    "celery_worker", broker="amqp://user:password@rabbitmq:5672/", backend="rpc://"
 )  # , include=["test_celery.tasks"]
 
-# app.config_from_object("django.conf:settings", namespace="CELERY")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 
-# Load task modules from all registered Django apps.
-app.autodiscover_tasks()
+# Load task modules from all registered Django apps.# noqa
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)  # noqa
 
 
 @app.task(bind=True)
 def debug_task(self):
-    print(f"Request: {self.request!r}")
+    print(f"Request: {self.request!r}")  # noqa# noqa# noqa
+
+
+@shared_task
+def count_users():  # noqa
+    return User.objects.count()  # noqa# noqa# noqa
+
+
+@shared_task
+def edit_user(pk, new_first_name):
+    user = User.objects.get(pk=pk)  # noqa# noqa# # noqa# noqanoqa
+    user.first_name = new_first_name
+    user.save()
+
+
+# noqa# noqa# noqa# noqa# noqa# noqa# noqa# noqa# noqa
--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -54,13 +54,7 @@ DJANGO_APPS = [
     "django.contrib.staticfiles",
 ]
 
-THIRD_PARTY_APPS = [
-    "corsheaders",
-    "graphene_django",
-    "rest_framework",
-    "phonenumber_field",
-    "guardian",
-]
+THIRD_PARTY_APPS = ["corsheaders", "graphene_django", "rest_framework", "phonenumber_field", "guardian", "celery"]
 
 LOCAL_APPS = [
     "apps.archive",
