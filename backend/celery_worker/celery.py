0f959bfd953221ce54be6d4dea157661cefb50f6
Jørgen Rosager <joergen.rosager@gmail.com>
2022-01-31 22:33:44 +0100

    THINGS WORKING :D

--- backend/celery_worker/celery.py
+++ backend/celery_worker/celery.py
@@ -18,7 +18,7 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 
 app.conf.beat_schedule = {
-    "add-every-30-seconds": {"task": "tasks.add", "schedule": 30.0, "args": (16, 16)},
+    "add-every-30-seconds": {"task": "celery_worker.tasks.add", "schedule": 30.0, "args": (16, 16)},
 }
 app.conf.timezone = "UTC"
 
