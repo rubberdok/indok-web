ffad8bbef33853341c2f8b19614fce5a28efdc3d
Herman Holmøy <herman.holmoy12@gmail.com>
2022-02-07 17:47:10 +0100

    add profile page admin link

--- frontend/src/components/pages/profile/ProfileCard/index.tsx
+++ frontend/src/components/pages/profile/ProfileCard/index.tsx
@@ -4,6 +4,7 @@ import OrganizationCard from "./variants/OrganizationCard";
 import PersonalCard from "./variants/PersonalCard";
 import ReportCard from "./variants/ReportCard";
 import OrdersCard from "./variants/OrdersCard";
+import CabinsAdminCard from "./variants/CabinsAdminCard";
 
 export const Event = EventCard;
 export const Form = FormCard;
@@ -11,3 +12,4 @@ export const Organization = OrganizationCard;
 export const Personal = PersonalCard;
 export const Report = ReportCard;
 export const Orders = OrdersCard;
+export const CabinsAdmin = CabinsAdminCard;
