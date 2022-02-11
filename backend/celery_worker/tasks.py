3bf5b429af2ad7c9d8ef4dc78d2d557210a70eaf
Jørgen Rosager <joergen.rosager@gmail.com>
2022-02-11 19:48:14 +0100

    creating periodic tasks from backend working

--- backend/celery_worker/tasks.py
+++ backend/celery_worker/tasks.py
@@ -1,7 +1,18 @@
 from celery import shared_task
 from apps.users.models import User
+from django_celery_beat.models import IntervalSchedule, PeriodicTask
+from django.utils import timezone
 
 
 @shared_task
 def count_users():
     return User.objects.count()
+
+
+@shared_task
+def create_periodic_task():
+    schedule, _ = IntervalSchedule.objects.get_or_create(every=1, period=IntervalSchedule.MINUTES)
+    task = PeriodicTask.objects.create(
+        name="Count users", task="celery_worker.tasks.count_users", interval=schedule, start_time=timezone.now(),
+    )
+    task.save()
