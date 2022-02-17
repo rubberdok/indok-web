00f65020e9b00389d86d74f84bbe17d7860d9802
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-02-17 17:15:03 +0100

    feat: notify users bumped from wait list

--- backend/apps/events/signals.py
+++ backend/apps/events/signals.py
@@ -9,15 +9,16 @@ from .models import Event, SignUp
 @receiver(pre_save, sender=SignUp)
 def send_order_confirmation(sender, instance: SignUp, **kwargs):
     """
-    Send an order confirmation email when an order is captured.
-    An order is captured when the payment status is changed from RESERVED to CAPTURED
+    Send an email to the user who is bumped from the wait list when another user is no longer attending, if any.
+    Prone to race conditions in the case where two users sign off simultaneously and should be resolved.
     """
-    # Check that we are updating an order, not creating
     if not instance._state.adding:
         previous: SignUp = sender.objects.get(pk=instance.pk)
         if previous.is_attending and not instance.is_attending:
             event: Event = previous.event
             signed_up = event.signed_up_users
-            if len(signed_up) > event.available_slots:
+            attending = signed_up[: event.available_slots].filter(user=previous.user).exists()
+            if attending and len(signed_up) > event.available_slots:
                 new_attending_user = signed_up[: event.available_slots + 1].last()
-                send_waitlist_notification_email(new_attending_user, event)
+                if new_attending_user is not None:
+                    send_waitlist_notification_email(new_attending_user, event)
