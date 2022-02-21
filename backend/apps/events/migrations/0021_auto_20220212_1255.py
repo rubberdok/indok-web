2319425ff3db7f43344f1471512a07f48cd63eac
mathildesolberg <mathilde.solberg@hotmail.com>
2022-02-21 18:44:34 +0100

    fixed black

--- backend/apps/events/migrations/0021_auto_20220212_1255.py
+++ backend/apps/events/migrations/0021_auto_20220212_1255.py
@@ -6,41 +6,41 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('events', '0020_auto_20220212_1226'),
+        ("events", "0020_auto_20220212_1226"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='event',
-            name='available_slots',
+            model_name="event",
+            name="available_slots",
         ),
         migrations.RemoveField(
-            model_name='event',
-            name='binding_signup',
+            model_name="event",
+            name="binding_signup",
         ),
         migrations.RemoveField(
-            model_name='event',
-            name='deadline',
+            model_name="event",
+            name="deadline",
         ),
         migrations.RemoveField(
-            model_name='event',
-            name='has_extra_information',
+            model_name="event",
+            name="has_extra_information",
         ),
         migrations.RemoveField(
-            model_name='event',
-            name='is_attendable',
+            model_name="event",
+            name="is_attendable",
         ),
         migrations.RemoveField(
-            model_name='event',
-            name='price',
+            model_name="event",
+            name="price",
         ),
         migrations.RemoveField(
-            model_name='event',
-            name='signup_open_date',
+            model_name="event",
+            name="signup_open_date",
         ),
         migrations.AlterField(
-            model_name='event',
-            name='short_description',
-            field=models.CharField(default='Klikk her for å lese mer', max_length=100),
+            model_name="event",
+            name="short_description",
+            field=models.CharField(default="Klikk her for å lese mer", max_length=100),
         ),
     ]
