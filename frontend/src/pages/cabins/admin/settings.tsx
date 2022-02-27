7d6ffa43c78ecdea07823fb9288fa2b473c3db9b
Herman Holmøy <herman.holmoy12@gmail.com>
2022-02-27 14:58:58 +0100

    resolve frontend

--- frontend/src/pages/cabins/admin/settings.tsx
+++ frontend/src/pages/cabins/admin/settings.tsx
@@ -1,5 +1,6 @@
 import Layout from "@components/Layout";
 import BookingSemesterPicker from "@components/pages/cabins/Admin/BookingSemesterPicker";
+import PermissionRequired from "@components/permissions/PermissionRequired";
 import { Box, Button, Container, Divider, Grid, Paper, Typography, useMediaQuery } from "@material-ui/core";
 import { ArrowBackIos } from "@material-ui/icons";
 import theme from "@styles/theme";
@@ -11,34 +12,36 @@ const SettingsPage: React.VFC = () => {
   return (
     <Layout>
       <Container>
-        <Grid container direction="column" spacing={3}>
-          <Grid item>
-            <Box p={3}>
-              <Typography variant={isMobile ? "h3" : "h1"} align="center">
-                Booking adminside
-              </Typography>
-              <Button startIcon={<ArrowBackIos />} onClick={() => router.back()}>
-                Gå tilbake
-              </Button>
-            </Box>
+        <PermissionRequired permission="cabins.change_bookingsemester">
+          <Grid container direction="column" spacing={3}>
+            <Grid item>
+              <Box p={3}>
+                <Typography variant={isMobile ? "h3" : "h1"} align="center">
+                  Booking adminside
+                </Typography>
+                <Button startIcon={<ArrowBackIos />} onClick={() => router.back()}>
+                  Gå tilbake
+                </Button>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
 
-        <Box marginBottom={5}>
-          <Paper>
-            <Box p={4}>
-              <Typography variant={isMobile ? "h4" : "h3"}>Instillinger</Typography>
-            </Box>
-            <Divider />
-            <Box p={4}>
-              <Typography variant="h5">Velg start og slutt-dato for booking for høst- og vårsemestrene</Typography>
-              <Typography>Det vil kun være mulig for brukere å søke om bookinger i disse periodene.</Typography>
-            </Box>
-            <Box p={4}>
-              <BookingSemesterPicker />
-            </Box>
-          </Paper>
-        </Box>
+          <Box marginBottom={5}>
+            <Paper>
+              <Box p={4}>
+                <Typography variant={isMobile ? "h4" : "h3"}>Instillinger</Typography>
+              </Box>
+              <Divider />
+              <Box p={4}>
+                <Typography variant="h5">Velg start og slutt-dato for booking for høst- og vårsemestrene</Typography>
+                <Typography>Det vil kun være mulig for brukere å søke om bookinger i disse periodene.</Typography>
+              </Box>
+              <Box p={4}>
+                <BookingSemesterPicker />
+              </Box>
+            </Paper>
+          </Box>
+        </PermissionRequired>
       </Container>
     </Layout>
   );
