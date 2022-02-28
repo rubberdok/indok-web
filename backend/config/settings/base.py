b36f2f44bcd35f5da4020c41b3d24f0e20e7189a
Jørgen Rosager <joergen.rosager@gmail.com>
2022-02-28 19:15:54 +0100

    fix formatting

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -163,7 +163,13 @@ LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {"verbose": {"format": "%(levelname)s %(asctime)s %(module)s " "%(process)d %(thread)d %(message)s"}},
-    "handlers": {"console": {"level": "DEBUG", "class": "logging.StreamHandler", "formatter": "verbose"}},
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
     "root": {"level": "INFO", "handlers": ["console"]},
 }
 
