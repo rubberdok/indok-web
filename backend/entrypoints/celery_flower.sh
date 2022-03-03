3946d6a0a4dcd5ea18becf40948f8021a89db5cf
Jørgen Rosager <joergen.rosager@gmail.com>
2022-03-03 17:44:54 +0100

    fix .env.example

--- backend/entrypoints/celery_flower.sh
+++ backend/entrypoints/celery_flower.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source .env
-celery -A celery_worker flower --basic_auth=${CELERY_FLOWER_USER}:${CELERY_FLOWER_PW}
+celery -A celery_worker flower --basic_auth=${CELERY_FLOWER_USER}:${CELERY_FLOWER_PASS}
