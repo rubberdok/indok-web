7114367b62ef2085617780b3e62b3607410d44fc
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-03-03 21:18:23 +0100

    feat: fetch csrf token

--- backend/config/urls/local.py
+++ backend/config/urls/local.py
@@ -8,5 +8,5 @@ from .base import urlpatterns
 urlpatterns += [
     # ok to csrf exempt the graphql endpoint:
     # https://stackoverflow.com/questions/51764452/403-by-graphene-django-dont-use-csrf-exempt
-    path(settings.GRAPHQL_URL, csrf_exempt(GraphQLView.as_view(graphiql=True)))
+    path(settings.GRAPHQL_URL, GraphQLView.as_view(graphiql=True))
 ]
