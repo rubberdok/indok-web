ffbbc042911f0b0090ffa5a8fce0d82e25981a87
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-03-12 00:47:30 +0100

    refactor: remove unused import

--- backend/config/urls/local.py
+++ backend/config/urls/local.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 from .base import urlpatterns
