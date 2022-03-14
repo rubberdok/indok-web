38735f145ba806bc9d0c09cba0d66a9ac5e6fdb2
Jørgen Rosager <joergen.rosager@gmail.com>
2022-03-14 19:06:14 +0100

    fix comment

--- backend/celery_worker/celery.py
+++ backend/celery_worker/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 import os
 from django.conf import settings
 
-# Set default sets the DJANGO_SETTINGS_MODULE only if it hasn't been set already so this will only affect production.
+# Set default sets the DJANGO_SETTINGS_MODULE only if it hasn't been set already so this will only affect development.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 app = Celery(
