29ac50e747d31d9568ba61aaed89af4ca23e4614
hermannm <hermann.morkrid@gmail.com>
2022-03-24 17:44:24 +0100

    add assign_object_permissions util function

---
+++ backend/utils/permissions.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+from apps.permissions.constants import DEFAULT_ORG_PERMISSION_GROUPS
+from guardian.shortcuts import assign_perm
+
+if TYPE_CHECKING:
+    from apps.organizations.models import Organization
+    from django.db.models import Model
+
+
+def assign_object_permissions(app: str, model: str, instance: "Model", organization: "Organization") -> None:
+    """
+    Takes in a newly created instance of a model, and the organization tied to it.
+    Assigns permissions on the instance to the organization's appropriate permission groups.
+    """
+    for permission_group in organization.permission_groups.all():
+        try:
+            perms = DEFAULT_ORG_PERMISSION_GROUPS[permission_group.group_type].formatted_model_permissions(app, model)
+
+            for perm in perms:
+                assign_perm(perm, permission_group.group, instance)
+        except KeyError:
+            continue
