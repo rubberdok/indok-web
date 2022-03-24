41df43978b4da4f2b73e49fe87a0436a07f985c7
hermannm <hermann.morkrid@gmail.com>
2022-03-24 18:02:13 +0100

    add get_organization_for_permissions util function

--- backend/apps/organizations/permissions.py
+++ backend/apps/organizations/permissions.py
@@ -6,3 +6,11 @@ def check_user_membership(user: User, organization: Organization):
     membership = Membership.objects.filter(user=user, organization=organization)
     if not membership:
         raise PermissionError(f"{user.username}({user.id}) is not a member of {organization.name}({organization.id})")
+
+
+def get_organization_for_permissions(context, *args, **kwargs):
+    """
+    Utility function to get an organization instance when using @permission_required.
+    """
+    organization = Organization.objects.get(id=kwargs["id"])
+    return organization
