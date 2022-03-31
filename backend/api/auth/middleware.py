75d1aa6f01cc24cd0a2e08c4bfdb112692f9dfaa
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-03-31 20:39:40 +0200

    Revert "build(python): remove graphql-jwt dependency"
    This reverts commit 2f7c52d1d192b1b1d9f159525dec610d1f577b5a.

--- backend/api/auth/middleware.py
+++ backend/api/auth/middleware.py
@@ -1,15 +1,31 @@
-from typing import TYPE_CHECKING, cast
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.auth import get_user_model
+from django.conf import settings
 
-if TYPE_CHECKING:
-    from apps.users import models
+
+class IndokWebJWTMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+        jwt_token = getattr(request, "set_jwt_cookie", None)
+        if jwt_token:
+            response.set_cookie(
+                "JWT",
+                jwt_token,
+                max_age=24 * 60 * 60,
+                httponly=True,
+                domain=settings.GRAPHQL_JWT.get("JWT_COOKIE_DOMAIN", None),
+            )
+
+        return response
 
 
 class AnonymousUserMiddleware(object):
     def resolve(self, next, root, info, **args):
         if isinstance(info.context.user, AnonymousUser):
-            User = cast("models.User", get_user_model())
+            User = get_user_model()
             anon = User.get_anonymous()
             info.context.user = anon
         return next(root, info, **args)
