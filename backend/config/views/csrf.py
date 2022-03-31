0a9d5f6c7085f31bfaf1e83302a59786c64a1db8
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-03-31 20:45:29 +0200

    fix: remove old cookie

--- backend/config/views/csrf.py
+++ backend/config/views/csrf.py
@@ -3,4 +3,6 @@ from django.middleware.csrf import get_token
 
 
 def csrf(request: HttpRequest):
-    return JsonResponse({"csrfToken": get_token(request)})
+    response = JsonResponse({"csrfToken": get_token(request)})
+    response.delete_cookie("csrftoken", domain="api.indokntnu.no", path="/")
+    return response
