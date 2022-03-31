a679e2105af3511a162afad1d490825032085c54
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-03-31 20:10:31 +0200

    refactor: clean up and simplify decorators

--- backend/decorators/authentication.py
+++ backend/decorators/authentication.py
@@ -19,9 +19,20 @@ def user_passes_test(
     test_fn: Callable[["models.User"], bool],
     exception: Exception = PermissionDenied(PERMISSION_REQUIRED_ERROR),
 ):
+    """
+    Create a decorator for a function that checks if the user passes the given test.
+
+    Parameters
+    ----------
+    test_fn : Callable[[models.User], bool]
+        The test function the user has to pass
+    exception : Exception, optional
+        Raised if the user does not pass the test function, by default PermissionDenied(PERMISSION_REQUIRED_ERROR)
+    """
+
     def decorator(fn: ResolverSignature[P, R]) -> Callable[P, R]:
         @wraps(fn)
-        @context(fn)
+        @context
         def wrapper(context, *args: P.args, **kwargs: P.kwargs) -> R:
             user: "models.User" = context.user
             if test_fn(user):
