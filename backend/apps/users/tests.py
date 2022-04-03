34ac667805d1fc4201b828fe33ce7a4549d0bb52
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-04-03 21:59:06 +0200

    :white_check_mark: tests: mock feide user authentication

--- backend/apps/users/tests.py
+++ backend/apps/users/tests.py
@@ -1,11 +1,67 @@
 import json
 from datetime import datetime
+from typing import Callable
+from unittest.mock import patch
 
+import requests
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from graphene.utils.str_converters import to_snake_case
 from utils.testing.base import ExtendedGraphQLTestCase
 from utils.testing.factories.users import IndokUserFactory, UserFactory
+
+User = get_user_model()
+
+
+def setup_mock_requests(
+    firstName: str = "Test", lastName: str = "Testesen", indok: bool = True, id: str = "123123"
+) -> Callable:
+    def mocked_feide_requests(*args, **kwargs):
+        class MockResponse:
+            def __init__(self, json_data, status_code: int) -> None:
+                self.json_data = json_data
+                self.status_code = status_code
+
+            def json(self):
+                return self.json_data
+
+            def raise_for_status(self):
+                if self.status_code in range(400, 600):
+                    raise requests.exceptions.RequestException("Error")
+
+        if args[0] == "https://auth.dataporten.no/oauth/token":
+            return MockResponse(
+                {
+                    "access_token": "1234567890",
+                    "id_token": "1234567890",
+                    "expires_in": 3600,
+                    "token_type": "Bearer",
+                },
+                200,
+            )
+        if args[0] == "https://auth.dataporten.no/openid/userinfo":
+            return MockResponse(
+                {
+                    "sub": id,
+                    "name": f"{firstName} {lastName}",
+                    "email": f"{firstName.lower()}@stud.ntnu.no",
+                    "dataporten-userid_sec": [f"feide:{firstName.lower()}@ntnu.no"],
+                },
+                200,
+            )
+
+        if args[0] == "https://groups-api.dataporten.no/groups/me/groups/fc:fs:fs:prg:ntnu.no:MTIØT":
+            if indok:
+                return MockResponse(
+                    {
+                        "basic": "member",
+                        "active": True,
+                    },
+                    200,
+                )
+            return MockResponse({}, 200)
+
+    return mocked_feide_requests
 
 
 class UsersBaseTestCase(ExtendedGraphQLTestCase):
@@ -132,10 +188,85 @@ class UsersMutationsTestCase(UsersBaseTestCase):
             }}
         """
         )
+
+        self.auth_user_mutation = """
+        mutation {{
+            authUser(code: "mocked") {{
+                user {{
+                    id
+                    username
+                    firstName
+                    lastName
+                    email
+                    feideEmail
+                    firstLogin
+                    yearUpdatedAt
+                    feideUserid
+                }}
+            }}
+        }}
+        """
+        self.registered_indok_user = IndokUserFactory(feide_userid="a random ID", first_login=False)
         return super().setUp()
 
-    def test_auth_user(self):
-        pass
+    @patch("requests.get", side_effect=setup_mock_requests())
+    @patch("requests.post", side_effect=setup_mock_requests())
+    @patch("api.auth.dataporten_auth.DataportenAuth.validate_response", return_value=None)
+    def test_register_new_indok_user(self, *args, **kwargs):
+        response = self.query(self.auth_user_mutation)
+        self.assertResponseNoErrors(response)
+
+        user = response.json()["data"]["authUser"]["user"]
+        pk = user["id"]
+
+        self.deep_assert_equal(user, current_user := User.objects.get(pk=pk))
+        self.assertTrue(user["firstLogin"], "First login should be set to True")
+        self.assertEqual(user["username"], "test")
+        self.assertEqual(user["firstName"], "Test")
+        self.assertEqual(user["lastName"], "Testesen")
+
+        cookies = response.client.cookies
+        self.assertIn("sessionid", cookies)
+
+        self.assertTrue(current_user.groups.filter(name="Indøk").exists())
+        self.assertTrue(current_user.groups.filter(name="Registered user").exists())
+
+    @patch("requests.get", side_effect=setup_mock_requests(indok=False))
+    @patch("requests.post", side_effect=setup_mock_requests(indok=False))
+    @patch("api.auth.dataporten_auth.DataportenAuth.validate_response", return_value=None)
+    def test_register_new_non_indok_user(self, *args, **kwargs):
+        response = self.query(self.auth_user_mutation)
+        self.assertResponseNoErrors(response)
+
+        user = response.json()["data"]["authUser"]["user"]
+        pk = user["id"]
+
+        self.deep_assert_equal(user, current_user := User.objects.get(pk=pk))
+        self.assertTrue(user["firstLogin"], "First login should be set to True")
+        self.assertEqual(user["username"], "test")
+        self.assertEqual(user["firstName"], "Test")
+        self.assertEqual(user["lastName"], "Testesen")
+
+        cookies = response.client.cookies
+        self.assertIn("sessionid", cookies)
+
+        self.assertTrue(not current_user.groups.filter(name="Indøk").exists())
+        self.assertTrue(current_user.groups.filter(name="Registered user").exists())
+
+    @patch("requests.get", side_effect=setup_mock_requests(id="a random ID"))
+    @patch("requests.post", side_effect=setup_mock_requests(id="a random ID"))
+    @patch("api.auth.dataporten_auth.DataportenAuth.validate_response", return_value=None)
+    def test_registered_user_authentication(self, *args, **kwargs):
+        response = self.query(self.auth_user_mutation)
+        self.assertResponseNoErrors(response)
+
+        user = response.json()["data"]["authUser"]["user"]
+
+        self.deep_assert_equal(user, self.registered_indok_user)
+        self.assertFalse(user["firstLogin"], "First login should be set to False")
+
+        cookies = response.client.cookies
+        self.assertIn("sessionid", cookies)
 
     def test_update_graduation_year(self):
         today = timezone.now().year
