40b6af310f45a56bdeb4e175e2374cb542a2fc3f
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-06-03 15:26:58 +0200

    fix: move wait-for-it to the start of entrypoint

--- backend/entrypoints/integration.sh
+++ backend/entrypoints/integration.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 
+./wait-for-it.sh postgres:5432 -- echo "Postgres is up"
 python manage.py migrate
 python manage.py loadfactories
 python manage.py collectstatic --noinput
-./wait-for-it.sh postgres:5432 -- gunicorn config.wsgi:application --bind 0.0.0.0:8000
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
 exec "$@"
