04c57ed0391074a6f1c78c767d1651bf5e58ca93
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-06-03 23:59:25 +0200

    :label: chore: improve type specificity

--- frontend/src/components/LabeledIcon.tsx
+++ frontend/src/components/LabeledIcon.tsx
@@ -3,27 +3,25 @@ import { ReactElement } from "react";
 
 interface Props extends StackProps {
   icon: ReactElement;
-  value: any;
+  value: ReactElement;
   endIcon?: boolean;
   sx?: SxProps;
 }
 
-const LabeledIcon: React.FC<Props> = ({ icon, value, endIcon = false, sx, ...other }) => {
-  return (
-    <Stack
-      direction="row"
-      alignItems="center"
-      sx={{
-        typography: "body2",
-        ...sx,
-      }}
-      {...other}
-    >
-      {!endIcon && icon}
-      {value}
-      {endIcon && icon}
-    </Stack>
-  );
-};
+const LabeledIcon: React.FC<Props> = ({ icon, value, endIcon = false, sx, ...other }) => (
+  <Stack
+    direction="row"
+    alignItems="center"
+    sx={{
+      typography: "body2",
+      ...sx,
+    }}
+    {...other}
+  >
+    {!endIcon && icon}
+    {value}
+    {endIcon && icon}
+  </Stack>
+);
 
 export default LabeledIcon;
