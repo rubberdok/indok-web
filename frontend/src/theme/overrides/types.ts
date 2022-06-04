21bb44cb28526dc78da7830f645640c8a0b94d17
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-06-04 19:00:10 +0200

    chore: apply some deoderant to the code

--- frontend/src/theme/overrides/types.ts
+++ frontend/src/theme/overrides/types.ts
@@ -4,7 +4,7 @@ import { CSSProperties } from "@mui/styles";
 
 /* Certain components are not of the core theme, so we extend the components here to include them */
 type Overrides = {
-  MuiLoadingButton?: Partial<Record<LoadingButtonClassKey, CSSProperties | (() => CSSProperties)>> | undefined;
+  MuiLoadingButton?: Partial<Record<LoadingButtonClassKey, CSSProperties | (() => CSSProperties)>>;
 } & Theme["components"];
 
 export type ComponentOverride = (theme: Theme) => Overrides;
