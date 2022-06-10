cd14d8ac0b0fd2a3efa96845e8e21c2b805c02c2
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-06-10 19:07:54 +0200

    feat: ssr profile page

--- frontend/src/components/Auth/Logout/index.tsx
+++ frontend/src/components/Auth/Logout/index.tsx
@@ -1,17 +1,19 @@
 import { useMutation } from "@apollo/client";
+import { LogoutDocument } from "@generated/graphql";
+import { LoadingButton } from "@mui/lab";
+import { Alert, AlertTitle, ButtonProps, Snackbar } from "@mui/material";
 import { config } from "@utils/config";
 import { generateQueryString } from "@utils/helpers";
 import { useRouter } from "next/router";
-import { LogoutDocument } from "@generated/graphql";
-import { Button, ButtonProps } from "@mui/material";
-import { LoadingButton } from "@mui/lab";
+import { useState } from "react";
 
-const Logout: React.FC<ButtonProps> = ({ ...props }) => {
+const Logout: React.FC<Omit<ButtonProps, "sx">> = ({ ...props }) => {
   const router = useRouter();
+  const [open, setOpen] = useState(false);
   const [logoutAction, { loading, client }] = useMutation(LogoutDocument, {
-    onCompleted: ({ logout }) => {
+    onCompleted: async ({ logout }) => {
       // reset the apollo store and redirect. See // https://www.apollographql.com/docs/react/networking/authentication/#reset-store-on-logout
-      client.resetStore();
+      await client.resetStore();
       const queryString = generateQueryString({
         post_logout_redirect_uri: config.FRONTEND_URI,
         id_token_hint: logout?.idToken,
@@ -19,20 +21,23 @@ const Logout: React.FC<ButtonProps> = ({ ...props }) => {
       const logOutUrl = "https://auth.dataporten.no/openid/endsession" + queryString;
       router.push(logOutUrl);
     },
+    onError: () => setOpen(true),
   });
 
-  if (loading) {
-    return (
-      <LoadingButton color="warning" {...props}>
+  return (
+    <>
+      {open && (
+        <Snackbar open={open} color="error" anchorOrigin={{ vertical: "bottom", horizontal: "center" }}>
+          <Alert onClose={() => setOpen(false)} severity="error">
+            <AlertTitle>Utloggingen feilet</AlertTitle>
+            Kontakt <a href="mailto:kontakt@rubberdok.no">kontakt@rubberdok.no</a> dersom problemet vedvarer.
+          </Alert>
+        </Snackbar>
+      )}
+      <LoadingButton variant="contained" color="error" loading={loading} {...props} onClick={() => logoutAction()}>
         Logg ut
       </LoadingButton>
-    );
-  }
-
-  return (
-    <Button color="error" variant="contained" {...props} onClick={() => logoutAction()}>
-      Logg ut
-    </Button>
+    </>
   );
 };
 
