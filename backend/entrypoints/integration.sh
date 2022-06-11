d3a8edf9468f3cb0d607faea13300a23867d197f
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-06-12 00:20:20 +0200

    build: collect static during build

--- backend/entrypoints/integration.sh
+++ backend/entrypoints/integration.sh
@@ -4,6 +4,5 @@ set -e
 ./wait-for-it.sh postgres:5432 -- echo "Postgres is up"
 python manage.py migrate
 python manage.py loadfactories
-python manage.py collectstatic --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
 exec "$@"
--- backend/entrypoints/production.sh
+++ backend/entrypoints/production.sh
@@ -2,6 +2,5 @@
 set -e
 
 python manage.py migrate
-python manage.py collectstatic --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
 exec "$@"
