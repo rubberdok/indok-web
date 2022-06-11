65160432fd013909708cff0a830ce351a576427c
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-06-11 02:55:48 +0200

    refactor: landing section

--- frontend/src/components/landing/LandingListings.tsx
+++ frontend/src/components/landing/LandingListings.tsx
@@ -1,8 +1,9 @@
 import useResponsive from "@hooks/useResponsive";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Social from "@public/img/gang.jpg";
 
 const LandingListings: React.FC = () => {
   const isMobile = useResponsive({ query: "down", key: "md" });
@@ -17,24 +18,28 @@ const LandingListings: React.FC = () => {
         }}
       >
         <Box>
-          <Typography variant="overline" sx={{ color: "primary.main", mb: 2, display: "block" }}>
-            Sosialt
-          </Typography>
-          <Typography variant="h2">Delta i et fantastisk studentmiljø</Typography>
-          <br />
-          <Typography variant="body1">
-            Vi har foreninger som tar seg av det meste, og man finner en forening for enhver som har lyst til å
-            engasjere seg!
-          </Typography>
-          <br />
-          <Link href="/listings" passHref>
-            <Button color="primary" variant="contained" size="medium">
-              Søk verv
-            </Button>
-          </Link>
+          <Grid container justifyContent="center">
+            <Grid item sm={8} xs={10}>
+              <Typography variant="overline" sx={{ color: "primary.main", mb: 2, display: "block" }}>
+                Sosialt
+              </Typography>
+              <Typography variant="h2">Delta i et fantastisk studentmiljø</Typography>
+              <br />
+              <Typography variant="body1">
+                Vi har foreninger som tar seg av det meste, og man finner en forening for enhver som har lyst til å
+                engasjere seg!
+              </Typography>
+              <br />
+              <Link href="/listings" passHref>
+                <Button color="primary" variant="contained" size="medium">
+                  Søk verv
+                </Button>
+              </Link>
+            </Grid>
+          </Grid>
         </Box>
         <Box sx={{ position: "relative" }}>
-          <Image src="/img/gang.jpg" layout="fill" objectFit="cover" objectPosition="center" />
+          <Image src={Social} layout="fill" objectFit="cover" objectPosition="center" placeholder="blur" />
         </Box>
       </Box>
     </Container>
