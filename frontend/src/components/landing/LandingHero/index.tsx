ac3a650c94b234e1f5a86d6aeebf0db2eca9dde6
Mathias Raa <skraagen@hotmail.com>
2022-06-12 20:00:59 +0200

    use swiper instead of scrolling on LandingHero

--- frontend/src/components/landing/LandingHero/index.tsx
+++ frontend/src/components/landing/LandingHero/index.tsx
@@ -1,11 +1,34 @@
-import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Divider, Grid, Stack, Typography } from "@mui/material";
 import Hero from "@public/static/landing/hero.jpg";
 import Image from "next/image";
 import Link from "next/link";
-import { ArrowRight } from "phosphor-react";
+import { useEffect, useRef, useState } from "react";
 import Organizations from "./Organizations";
 
 const LandingHero: React.FC = () => {
+  const orgsTitleEl = useRef<HTMLElement>();
+
+  const [sliderActiveIndex, setSliderActiveIndex] = useState(0);
+  const [sliderOffsetX, setSliderOffsetX] = useState(0);
+
+  const onActiveIndexChange = (index: number) => {
+    setSliderActiveIndex(index);
+  };
+
+  const updateSliderOffset = () => {
+    if (orgsTitleEl.current) {
+      setSliderOffsetX(orgsTitleEl.current.offsetWidth + orgsTitleEl.current.offsetLeft);
+    }
+  };
+
+  useEffect(() => {
+    updateSliderOffset();
+    window.addEventListener("resize", updateSliderOffset);
+    return () => {
+      window.removeEventListener("resize", updateSliderOffset);
+    };
+  }, []);
+
   return (
     <>
       <Box
@@ -23,33 +46,32 @@ const LandingHero: React.FC = () => {
               <Stack spacing={4} mt={14} mb={8}>
                 <Grid container gap={4} direction="column" alignItems={{ xs: "center", md: "flex-start" }}>
                   <Grid item>
-                    <Typography variant="overline" sx={{ color: "primary.main" }}>
+                    <Typography
+                      variant="overline"
+                      sx={{ color: (theme) => (theme.palette.mode === "light" ? "primary.main" : "primary.light") }}
+                    >
                       Foreningen for studentene ved
                     </Typography>
                   </Grid>
                   <Grid item xs={8}>
-                    <Typography variant="h2" textAlign={{ xs: "center", md: "left" }}>
+                    <Typography variant="h1" textAlign={{ xs: "center", md: "left" }}>
                       Industriell Økonomi & Teknologiledelse
                     </Typography>
                   </Grid>
                 </Grid>
 
-                <Grid container direction={{ xs: "column", md: "row" }} columnGap={1} rowGap={2} alignItems="center">
-                  <Grid item xs={10} md="auto">
-                    <Link passHref href="/about">
-                      <Button variant="contained" size="medium">
-                        Les om foreningen
-                      </Button>
-                    </Link>
-                  </Grid>
-                  <Grid item xs={10} md="auto">
-                    <Link passHref href="/events">
-                      <Button variant="contained" color="inherit" size="medium">
-                        Se arrangementer
-                      </Button>
-                    </Link>
-                  </Grid>
-                </Grid>
+                <Stack spacing={{ xs: 1.5, md: 2 }} direction={{ xs: "column", md: "row" }}>
+                  <Link passHref href="/about">
+                    <Button variant="contained" size="large">
+                      Les om foreningen
+                    </Button>
+                  </Link>
+                  <Link passHref href="/events">
+                    <Button variant="contained" color="inherit" size="large">
+                      Se arrangementer
+                    </Button>
+                  </Link>
+                </Stack>
               </Stack>
             </Grid>
           </Grid>
@@ -57,7 +79,7 @@ const LandingHero: React.FC = () => {
         <Box
           sx={{
             position: "relative",
-            gridColumn: "7 / -1",
+            gridColumn: "8 / -1",
             gridRow: "1",
             display: { xs: "none", md: "block" },
           }}
@@ -74,25 +96,27 @@ const LandingHero: React.FC = () => {
           px: 1,
           bgcolor: "background.neutral",
           borderTop: "1px solid",
+          borderBottom: "1px solid",
           borderColor: "divider",
-          overflow: "auto",
+          overflow: "hidden",
           position: "relative",
         }}
       >
         <Container>
-          <Stack direction="row" spacing={3} minWidth="max-content" pr={3} alignItems="center">
-            <Box mr={{ xs: 2, md: 6 }}>
-              <Typography variant="h4">
+          <Stack direction="row" minWidth="max-content" alignItems="center">
+            <Box ref={orgsTitleEl}>
+              <Typography
+                variant="h4"
+                sx={{
+                  opacity: sliderActiveIndex == 0 ? 1 : 0,
+                  transition: (theme) => theme.transitions.create("opacity"),
+                }}
+              >
                 Våre <br />
                 Foreninger
               </Typography>
             </Box>
-            <Organizations />
-            <Link passHref href="/about/organization">
-              <Button color="inherit" size="large" endIcon={<ArrowRight />}>
-                Se mer
-              </Button>
-            </Link>
+            <Organizations onActiveIndexChange={onActiveIndexChange} offsetX={sliderOffsetX + 48} />
           </Stack>
         </Container>
       </Box>
