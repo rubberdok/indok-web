474af8d8ba3ef15d0e78a7bb02aa969b8fa288a7
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-06-15 19:09:10 +0200

    fix: full width skeleton in drawer

--- frontend/src/layouts/components/LoginButton.tsx
+++ frontend/src/layouts/components/LoginButton.tsx
@@ -6,11 +6,15 @@ import { Button } from "@mui/material";
 import NextLink from "next/link";
 import { User } from "phosphor-react";
 
-const LoginButton: React.FC = () => {
+type Props = {
+  fullWidth?: boolean;
+};
+
+const LoginButton: React.FC<Props> = ({ fullWidth }) => {
   const { data } = useQuery<{ user: UserInfo | null }>(GET_USER_INFO);
 
   return (
-    <LoginRequired size="medium" color="inherit" data-test-id="login">
+    <LoginRequired size="medium" color="inherit" data-test-id="login" fullWidth={fullWidth}>
       <NextLink href="/profile" passHref>
         <Button endIcon={<User />} variant="outlined" color="inherit" size="medium">
           {data?.user?.firstName}
