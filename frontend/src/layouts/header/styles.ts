e45dd8fc875a7d158426d5c01f07ee442fe1d602
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-06-15 17:50:38 +0200

    fix: header overscroll behavior

--- frontend/src/layouts/header/styles.ts
+++ frontend/src/layouts/header/styles.ts
@@ -1,18 +1,29 @@
-import { Toolbar, ToolbarProps } from "@mui/material";
+import { AppBar as MuiAppBar, AppBarProps as MuiAppBarProps } from "@mui/material";
 import { alpha, styled } from "@mui/material/styles";
 import { HEADER_DESKTOP_HEIGHT, HEADER_MOBILE_HEIGHT } from "../../theme/constants";
 
-interface ToolbarStyleProps extends ToolbarProps {
+interface AppBarProps extends MuiAppBarProps {
   transparent?: boolean;
   scrolling?: boolean;
 }
 
-export const ToolbarStyle = styled(Toolbar, {
+export const AppBar = styled(MuiAppBar, {
   shouldForwardProp: (prop) => prop !== "transparent" && prop !== "scrolling",
-})<ToolbarStyleProps>(({ transparent, scrolling, theme }) => ({
+})<AppBarProps>(({ transparent, scrolling, theme }) => ({
   height: HEADER_MOBILE_HEIGHT,
+  [theme.breakpoints.up("md")]: {
+    height: HEADER_DESKTOP_HEIGHT,
+  },
+
   color: theme.palette.text.primary,
   backgroundColor: theme.palette.background.default,
+
+  ...(transparent && {
+    color: theme.palette.common.white,
+    backgroundColor: "transparent",
+  }),
+
+  /* Transition drop shadow and height smoothly */
   transition: `${theme.transitions.create(["height", "background-color"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.short,
@@ -20,14 +31,13 @@ export const ToolbarStyle = styled(Toolbar, {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.standard,
   })}`,
+
+  /* Browser optimization for smooth animations */
   willChange: "filter",
-  [theme.breakpoints.up("md")]: {
-    height: HEADER_DESKTOP_HEIGHT,
-  },
-  ...(transparent && {
-    color: theme.palette.common.white,
-    backgroundColor: "transparent",
-  }),
+
+  /* Replace default box shadow with our own */
+  boxShadow: "none",
+  /* Drop shadow, hidden when not scrolling to avoid flicker when scrolling starts */
   filter:
     theme.palette.mode === "light"
       ? `drop-shadow(0 25px 25px ${alpha(theme.palette.common.black, 0)})`
@@ -35,6 +45,8 @@ export const ToolbarStyle = styled(Toolbar, {
           theme.palette.common.black,
           0
         )})`,
+
+  /* When scrolling, show drop shadow and shrink the app bar on desktop */
   ...(scrolling && {
     filter:
       theme.palette.mode === "light"
