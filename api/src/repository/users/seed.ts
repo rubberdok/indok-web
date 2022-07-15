fcd6ceb3dc49b900127b1b73bbc8f06f9d7e1c67
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-07-15 22:54:34 +0200

    chore: fix seeds

--- api/src/repository/users/seed.ts
+++ api/src/repository/users/seed.ts
@@ -3,14 +3,29 @@ import { Prisma, PrismaClient } from "@prisma/client";
 export const userData: Prisma.UserCreateInput[] = [
   {
     username: "eva_student",
-    feideId: "asdf",
+    feideId: "evastudent",
     email: "example@example.org",
+    firstName: "Eva",
+    lastName: "Student Aasen",
+  },
+  {
+    username: "rubberdok",
+    feideId: "rubberdok",
+    email: "rubberdok@example.org",
+    firstName: "Rubb",
+    lastName: "Er Dok",
   },
 ];
 
-export const load = async (client: PrismaClient) => {
+export const load = async (db: PrismaClient) => {
   console.log("Seeding users");
-  client.user.createMany({
-    data: userData,
-  });
+  for (const user of userData) {
+    await db.user.upsert({
+      where: {
+        feideId: user.feideId,
+      },
+      update: {},
+      create: user,
+    });
+  }
 };
