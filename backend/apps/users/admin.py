9ab02625d35beb3d56f4fe1ed15cc2db6d9b7e92
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-09-23 13:59:44 +0200

    chore: allow settings passwords in admin

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import User
+from apps.users.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    exclude = ("password",)
     search_fields = ["username", "first_name", "last_name"]
     list_display = ("username", "first_name", "last_name", "last_login")
