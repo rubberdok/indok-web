29f8f8f15f4c62f9d714b0dfa21ff75faa8151bd
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-09-23 13:22:24 +0200

    chore: update cabin images

--- frontend/src/components/pages/cabins/ImageSlider/imageData.ts
+++ frontend/src/components/pages/cabins/ImageSlider/imageData.ts
@@ -1,93 +1,92 @@
+import _00 from "@public/static/cabins/00.jpg";
+import _01 from "@public/static/cabins/01.jpg";
+import _02 from "@public/static/cabins/02.jpg";
+import _03 from "@public/static/cabins/03.jpg";
+import _04 from "@public/static/cabins/04.jpg";
+import _05 from "@public/static/cabins/05.jpg";
+import _06 from "@public/static/cabins/06.jpg";
+import _07 from "@public/static/cabins/07.jpg";
+import _08 from "@public/static/cabins/08.jpg";
+import _09 from "@public/static/cabins/09.jpg";
+import _10 from "@public/static/cabins/10.jpg";
+import _11 from "@public/static/cabins/11.jpg";
+
+import _20 from "@public/static/cabins/20.jpg";
+import _21 from "@public/static/cabins/21.jpg";
+import _22 from "@public/static/cabins/22.jpg";
+
+import { StaticImageData } from "next/future/image";
+
+export interface ImageData {
+  label: string;
+  img: StaticImageData;
+}
+
 /*
 Data with all paths, labels and descriptions for images used in the image slider
 */
-export const outsideImages = [
+export const outsideImages: ImageData[] = [
   {
-    label: "Hytte",
-    description: "",
-    imgPath: "/static/cabins_images/ute1.jpg",
+    label: "Fjell",
+    img: _20,
   },
   {
-    label: "Hytte",
-    description: "",
-    imgPath: "/static/cabins_images/ute2.jpg",
+    label: "Skiløyper",
+    img: _21,
   },
   {
-    label: "Hytte",
-    description: "",
-    imgPath: "/static/cabins_images/ute6.jpg",
+    label: "Sosialt på fjellet",
+    img: _22,
   },
 ];
 
-export const cabinImages = [
+export const cabinImages: ImageData[] = [
   {
-    label: "Stue",
-    description: "",
-    imgPath: "/static/cabins_images/stue1.jpg",
-  },
-  {
-    label: "Kjøkken",
-    description: "",
-    imgPath: "/static/cabins_images/spis1.jpg",
-  },
-  {
-    label: "Kjøkken",
-    description: "",
-    imgPath: "/static/cabins_images/spis3.jpg",
-  },
-  {
-    label: "Gang",
-    description: "",
-    imgPath: "/static/cabins_images/gang1.jpg",
-  },
-  {
-    label: "Bad",
-    description: "",
-    imgPath: "/static/cabins_images/bad1.jpg",
-  },
-  {
-    label: "Bad",
-    description: "",
-    imgPath: "/static/cabins_images/bad2.jpg",
-  },
-  {
-    label: "Badstue",
-    description: "",
-    imgPath: "/static/cabins_images/badstue1.jpg",
-  },
-  {
-    label: "Soverom 1",
-    description: "",
-    imgPath: "/static/cabins_images/sov1.jpg",
-  },
-  {
-    label: "Soverom 2",
-    description: "",
-    imgPath: "/static/cabins_images/sov2.jpg",
-  },
-  {
-    label: "Soverom 3",
-    description: "",
-    imgPath: "/static/cabins_images/sov3.jpg",
-  },
-  {
-    label: "Soverom 4",
-    description: "",
-    imgPath: "/static/cabins_images/sov4.jpg",
-  },
-  {
-    label: "Utenfor",
-    description: "",
-    imgPath: "/static/cabins_images/utenfor1.jpg",
-  },
-  {
-    label: "Utenfor",
-    description: "",
-    imgPath: "/static/cabins_images/utenfor2.jpg",
+    label: "Hytta",
+    img: _00,
   },
   {
     label: "Utsikt",
-    description: "",
-    imgPath: "/static/cabins_images/utsikt1.jpg",
+    img: _01,
+  },
+  {
+    label: "Stue",
+    img: _02,
+  },
+  {
+    label: "Kjøkken",
+    img: _03,
+  },
+  {
+    label: "Kjøkken",
+    img: _04,
+  },
+  {
+    label: "Bad",
+    img: _05,
+  },
+  {
+    label: "Bad",
+    img: _06,
+  },
+  {
+    label: "Badstue",
+    img: _07,
+  },
+  {
+    label: "Soverom",
+    img: _08,
+  },
+  {
+    label: "Soverom",
+    img: _09,
+  },
+  {
+    label: "Soverom",
+    img: _10,
+  },
+  {
+    label: "Soverom",
+    img: _11,
   },
 ];
