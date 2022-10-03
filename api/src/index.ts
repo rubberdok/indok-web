b07fb4072e67c3638664e1a434170358edeade24
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-10-03 22:43:58 +0200

    refactor: lint

--- api/src/index.ts
+++ api/src/index.ts
@@ -11,24 +11,25 @@ import { json } from "body-parser";
 import cors from "cors";
 import express from "express";
 import session from "express-session";
-import { initializeContainer } from "@/container";
+import { container } from "tsyringe";
 
 import { env } from "@/config";
+import { initializeContainer } from "@/container";
 import { resolvers, typeDefs } from "@/graphql";
 import { IContext, IContextProvider, Type as ContextProviderType } from "@/graphql/context";
 import { formatError } from "@/lib/apolloServer";
 import { redisClient, RedisStore } from "@/lib/redis";
-import { container } from "tsyringe";
 
 Sentry.init({
   dsn: env.SENTRY_DSN,
   tracesSampleRate: env.SENTRY_TRACES_SAMPLE_RATE,
 });
 
+initializeContainer();
+
 const start = async () => {
   const app = express();
   const httpServer = http.createServer(app);
-  initializeContainer();
 
   app.use(Sentry.Handlers.requestHandler());
   app.use(
