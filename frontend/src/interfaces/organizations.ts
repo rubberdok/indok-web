a2b9a204fa0468030ebac739324a825243c1290a
hermannm <hermann.morkrid@gmail.com>
2022-10-07 00:14:39 +0200

    remove redundant fiels from org interface

--- frontend/src/interfaces/organizations.ts
+++ frontend/src/interfaces/organizations.ts
@@ -1,14 +1,9 @@
-import { AdminEventFragment } from "@/generated/graphql";
-import { Listing } from "@/interfaces/listings";
-
 export interface Organization {
   id: string;
   name: string;
   description: string;
   color: string | null;
   children?: Organization[];
-  events?: AdminEventFragment[];
-  listings?: Listing[];
   slug: string;
   logoUrl?: string;
 }
