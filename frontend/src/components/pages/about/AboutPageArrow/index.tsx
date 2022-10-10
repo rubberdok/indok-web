2ecf0b9e53cb25e5b81e0ccd87de07f85c9aab7e
hermannm <hermann.morkrid@gmail.com>
2022-10-10 10:26:04 +0200

    refactor s to arrow functions

---
+++ frontend/src/components/pages/about/AboutPageArrow/index.tsx
@@ -0,0 +1,49 @@
+import { ChevronLeft, ChevronRight } from "@mui/icons-material";
+import { Box, Grid } from "@mui/material";
+
+import { PostItem } from "./PostItem";
+
+type AboutPostProps = {
+  slug: string;
+  title: string;
+  cover?: string;
+};
+
+type Props = {
+  prevPost?: AboutPostProps;
+  nextPost?: AboutPostProps;
+};
+
+export const AboutPageArrow: React.FC<Props> = ({ prevPost, nextPost }) => {
+  const nextSlug = nextPost?.slug || "";
+  const prevSlug = prevPost?.slug || "";
+
+  return (
+    <Grid container spacing={5} sx={{ py: 8 }}>
+      <Grid item xs={12} md={6}>
+        {prevPost && (
+          <PostItem
+            href={prevSlug}
+            title={prevPost?.title}
+            coverImg={prevPost?.cover}
+            icon={<Box component={ChevronLeft} sx={{ width: 24, height: 24, color: "text.disabled", flexShrink: 0 }} />}
+          />
+        )}
+      </Grid>
+
+      <Grid item xs={12} md={6}>
+        {nextSlug && (
+          <PostItem
+            href={nextSlug}
+            isNext
+            title={nextPost?.title}
+            coverImg={nextPost?.cover}
+            icon={
+              <Box component={ChevronRight} sx={{ width: 24, height: 24, color: "text.disabled", flexShrink: 0 }} />
+            }
+          />
+        )}
+      </Grid>
+    </Grid>
+  );
+};
--- frontend/src/lib/emotion.ts
+++ frontend/src/lib/emotion.ts
@@ -1,11 +1,11 @@
-import createCache from "@emotion/cache";
+import createCache, { EmotionCache } from "@emotion/cache";
 
 const isBrowser = typeof document !== "undefined";
 
 // On the client side, Create a meta tag at the top of the <head> and set it as insertionPoint.
 // This assures that MUI styles are loaded first.
 // It allows developers to easily override MUI styles with other styling solutions, like CSS modules.
-export function createEmotionCache() {
+export const createEmotionCache = (): EmotionCache => {
   let insertionPoint;
 
   if (isBrowser) {
@@ -14,4 +14,4 @@ export function createEmotionCache() {
   }
 
   return createCache({ key: "mui-style", insertionPoint });
-}
+};
