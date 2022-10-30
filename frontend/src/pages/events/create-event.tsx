066adc84341d02045af0bd7c94c2bf5270c4726f
Lars Waage <46653859+larwaa@users.noreply.github.com>
2022-10-30 19:37:51 +0100

    :recycle: refactor: use new event creator

--- frontend/src/pages/events/create-event.tsx
+++ frontend/src/pages/events/create-event.tsx
@@ -1,18 +1,23 @@
+import { useQuery } from "@apollo/client";
 import { Button, Container } from "@mui/material";
 import Link from "next/link";
 
-import { CreateEvent } from "@/components/pages/events/CreateEvent";
+import { UserOrganizationsDocument } from "@/generated/graphql";
 import { Layout, RootStyle } from "@/layouts/Layout";
 import { NextPageWithLayout } from "@/pages/_app";
 
+import CreateEvent from "../create";
+
 /** Component for showing the create event page. */
 const CreateEventsPage: NextPageWithLayout = () => {
+  const { data } = useQuery(UserOrganizationsDocument);
+  const organizations = data?.user?.organizations ?? [];
   return (
     <Container>
       <Link href="/events" passHref>
         <Button color="primary">Tilbake til arrangementer</Button>
       </Link>
-      <CreateEvent />
+      <CreateEvent organizations={organizations} />
     </Container>
   );
 };
