48cdb7960776a97c28904026a6077f45d80a18fb
MagnusHafstad <114174209+MagnusHafstad@users.noreply.github.com>
2022-11-10 19:34:05 +0100

    Run yarn and graphql generation

--- frontend/src/generated/graphql.tsx
+++ frontend/src/generated/graphql.tsx
@@ -1348,6 +1348,7 @@ export type UserAttendingType = {
   hasBoughtTicket?: Maybe<Scalars["Boolean"]>;
   isOnWaitingList?: Maybe<Scalars["Boolean"]>;
   isSignedUp?: Maybe<Scalars["Boolean"]>;
+  positionOnWaitinglist?: Maybe<Scalars["Int"]>;
 };
 
 export type UserInput = {
@@ -1777,6 +1778,7 @@ export type EventFragment = {
     isSignedUp?: boolean | null;
     isOnWaitingList?: boolean | null;
     hasBoughtTicket?: boolean | null;
+    positionOnWaitinglist?: number | null;
   } | null;
   product?: { __typename?: "ProductType"; id: string } | null;
 };
@@ -1983,6 +1985,7 @@ export type UpdateEventMutation = {
         isSignedUp?: boolean | null;
         isOnWaitingList?: boolean | null;
         hasBoughtTicket?: boolean | null;
+        positionOnWaitinglist?: number | null;
       } | null;
       product?: { __typename?: "ProductType"; id: string } | null;
     } | null;
@@ -3480,6 +3483,7 @@ export const EventFragmentDoc = {
                 { kind: "Field", name: { kind: "Name", value: "isSignedUp" } },
                 { kind: "Field", name: { kind: "Name", value: "isOnWaitingList" } },
                 { kind: "Field", name: { kind: "Name", value: "hasBoughtTicket" } },
+                { kind: "Field", name: { kind: "Name", value: "positionOnWaitinglist" } },
               ],
             },
           },
