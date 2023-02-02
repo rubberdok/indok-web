7508a75c727545a8a353112f3965afeacf9619c3
MagnusHafstad <114174209+MagnusHafstad@users.noreply.github.com>
2023-02-02 18:51:18 +0100

    Add member cards without img and imports

--- frontend/src/pages/janus/info.tsx
+++ frontend/src/pages/janus/info.tsx
@@ -1,25 +1,113 @@
 import { Typography } from "@mui/material";
+import { red } from "@mui/material/colors";
 import Grid from "@mui/material/Grid";
 
-import { MemberCard } from "@/components/pages/about/MemberCard";
-import { BoardMember } from "@/components/pages/about/MemberCard/types";
+import { ContactInfo } from "@/components/pages/Janus/ContactInfo";
 import { Template } from "@/components/pages/Janus/Template";
 import { Layout } from "@/layouts/Layout";
 import { NextPageWithLayout } from "@/pages/_app";
+import Eventsjef from "~/public/img/JanusstyretsMedlemmer/Eventsjef.jpg";
+import Festsjef from "~/public/img/JanusstyretsMedlemmer/Festsjef.jpg";
+import Hyttetursjef from "~/public/img/JanusstyretsMedlemmer/Hyttetursjef.jpg";
+import JanusScriptRedaktør from "~/public/img/JanusstyretsMedlemmer/JanusScriptRedaktør.jpg";
+import Kjellersjef from "~/public/img/JanusstyretsMedlemmer/Kjellersjef.jpg";
+import Nestleder from "~/public/img/JanusstyretsMedlemmer/Nestleder.jpg";
+import President from "~/public/img/JanusstyretsMedlemmer/President.jpg";
+import PRsjef from "~/public/img/JanusstyretsMedlemmer/PRsjef.jpg";
+import Sekretær from "~/public/img/JanusstyretsMedlemmer/Sekretær.jpg";
+import Websjef from "~/public/img/JanusstyretsMedlemmer/Websjef.jpg";
 
 const JanusPage: NextPageWithLayout = () => {
-  const boardMembers: BoardMember[] = [
+  const responsibles = [
     {
-      rank: 1,
+      id: 1,
       name: "Oskar Gåsø",
-      position: "President Janus",
-      email: "president@janulinjeforening.no",
+      position: "President",
+      color: red[800],
+      image: President,
+      phonenumber: "tlf: 979 62 390",
+      email: "oskar.gasoo@gmail.com",
     },
     {
-      rank: 2,
-      name: "Fannar ",
-      position: "Janus web.sjef",
-      email: "president@janulinjeforening.no",
+      id: 2,
+      name: "Markus Kile Søyland",
+      position: "Økonomisjef/Nestleder",
+      color: red[500],
+      image: Nestleder,
+      phonenumber: "tlf: 468 48 580",
+      email: "marsoy2002@gmail.com",
+    },
+    {
+      id: 3,
+      name: "Fannar Steinn Lindal Rafnsson",
+      position: "Websjef",
+      color: red[500],
+      image: Websjef,
+      phonenumber: "tlf: 938 16 614",
+      email: "fannar23@gmail.com",
+    },
+    {
+      id: 4,
+      name: "Didrik Næss",
+      position: "JanusScript-Redaktør",
+      color: red[500],
+      image: JanusScriptRedaktør,
+      phonenumber: "tlf: 476 57 533",
+      email: "didriknaess@hotmail.com",
+    },
+    {
+      id: 5,
+      name: "Martin Nåtedal",
+      position: "Hyttetursjef",
+      color: red[500],
+      image: Hyttetursjef,
+      phonenumber: "tlf: 984 80 743",
+      email: "martin.natedal@gmail.com",
+    },
+    {
+      id: 6,
+      name: "Tobias Jortveit",
+      position: "Kjellersjef",
+      color: red[500],
+      image: Kjellersjef,
+      phonenumber: "tlf: 953 66 176",
+      email: "tobiasjortveit@gmail.com",
+    },
+    {
+      id: 7,
+      name: "Jenny Temmerud",
+      position: "Festsjef",
+      color: red[500],
+      image: Festsjef,
+      phonenumber: "tlf: 413 88 670",
+      email: "jennywtemmerud@gmail.com",
+    },
+    {
+      id: 8,
+      name: "Philip Thomas Jenkins",
+      position: "Eventsjef",
+      color: red[500],
+      image: Eventsjef,
+      phonenumber: "tlf: 955 52 393",
+      email: "philipthomasjenkinkins@hotmail.com",
+    },
+    {
+      id: 9,
+      name: "Mina Myrstøl",
+      position: "PR-sjef",
+      color: red[500],
+      image: PRsjef,
+      phonenumber: "tlf: 477 17 089",
+      email: "myrmina@gmail.com",
+    },
+    {
+      id: 10,
+      name: "Hans Bertil Olsson",
+      position: "Sekretær",
+      color: red[500],
+      image: Sekretær,
+      phonenumber: "tlf: 482 92 265",
+      email: "hansbert2002@gmail.com",
     },
   ];
 
@@ -42,10 +130,24 @@ const JanusPage: NextPageWithLayout = () => {
       <Typography variant="h3" gutterBottom>
         Medlemmer
       </Typography>
-      <Grid container spacing={2} alignItems="stretch" justifyContent="center">
-        {boardMembers.map((member) => (
-          <Grid key={member.rank} item xs={12} md={6}>
-            <MemberCard member={member} />
+
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="stretch"
+        spacing={8}
+        sx={{ mb: (theme) => theme.spacing(8) }}
+      >
+        {responsibles.map((responsible) => (
+          <Grid container item sm={4} xs={8} key={responsible.id}>
+            <ContactInfo
+              name={responsible.name}
+              email={responsible.email}
+              phonenumber={responsible.phonenumber}
+              image={responsible.image}
+              position={responsible.position}
+            />
           </Grid>
         ))}
       </Grid>
