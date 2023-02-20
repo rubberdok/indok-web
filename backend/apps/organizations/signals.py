1f8b1060632f1dfbc4d04c3bf2658694bbcc3d4f
Simen Sandhaug <70633406+simensandhaug@users.noreply.github.com>
2023-02-20 18:31:30 +0100

    Update backend/apps/organizations/signals.py
    remove print statement
    
    Co-authored-by: Lars Waage <46653859+larwaa@users.noreply.github.com>

--- backend/apps/organizations/signals.py
+++ backend/apps/organizations/signals.py
@@ -59,6 +59,5 @@ def create_default_groups(instance: Organization, created, **kwargs):
             organization=instance,
             group_type=ADMIN_GROUP_TYPE,
         )
-        print(admin_group.group, instance)
         assign_perm("forms.add_form", admin_group.group)
         # assign_perm("organizations.manage_organization", admin_group.group, instance)
