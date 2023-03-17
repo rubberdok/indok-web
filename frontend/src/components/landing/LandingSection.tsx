472cd59dc03bd4e4fd2de095f4467a49ba9fb922
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-03-17 01:28:56 +0100

    :bug: fix: overscrolling on landing page

--- frontend/src/components/landing/LandingSection.tsx
+++ frontend/src/components/landing/LandingSection.tsx
@@ -1,10 +1,9 @@
-import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import Image from "next/image";
 
+import { Link } from "@/components";
 import Hovedbygget from "~/public/static/landing/hovedbygget.webp";
-
-import { NextLinkComposed } from "../Link";
 
 export const LandingSection: React.FC = () => {
   const theme = useTheme();
@@ -38,35 +37,27 @@ export const LandingSection: React.FC = () => {
         />
       </Box>
       <Container
-        sx={{ alignSelf: "center", gridColumn: "1 / -1", gridRow: "1", textAlign: { xs: "center", md: "left" } }}
+        sx={{ alignSelf: "center", gridColumn: "2 / -1", gridRow: "1", textAlign: { xs: "center", md: "left" } }}
       >
-        <Grid container direction="row-reverse" justifyContent={{ xs: "center", md: "flex-start" }}>
-          <Grid item md={6} sm={8} xs={12}>
-            <Grid
-              container
-              my={{ xs: 6, md: 12 }}
-              ml={{ xs: 0, md: 8 }}
-              direction="column"
-              alignItems={{ xs: "center", md: "flex-start" }}
-              sx={{ color: "common.white" }}
-            >
-              <Grid item>
-                <Typography variant="overline" sx={{ mb: 3, color: "grey.300" }}>
-                  Samarbeid og kommunikasjon
-                </Typography>
-                <Typography variant="h2">Hovedstyret i foreningen</Typography>
-                <Typography maxWidth={450} textAlign="left" sx={{ mt: 3, mb: 5, opacity: 0.72 }}>
-                  Hovedstyrets fremste oppgave er å sørge for god kommunikasjon og samarbeid mellom de ulike
-                  studentinitiativene, og forvalte og disponere Indøks midler på en forsvarlig måte.
-                </Typography>
+        <Stack
+          direction="column"
+          alignItems={{ xs: "center", md: "flex-start" }}
+          justifyContent="center"
+          sx={{ color: "common.white", py: { xs: 6, md: 12 }, pl: { xs: 0, md: 3 } }}
+        >
+          <Typography variant="overline" sx={{ color: "grey.300" }}>
+            Samarbeid og kommunikasjon
+          </Typography>
+          <Typography variant="h2">Hovedstyret i foreningen</Typography>
+          <Typography maxWidth={450} textAlign="left" sx={{ mt: 3, mb: 5, opacity: 0.72 }}>
+            Hovedstyrets fremste oppgave er å sørge for god kommunikasjon og samarbeid mellom de ulike
+            studentinitiativene, og forvalte og disponere Indøks midler på en forsvarlig måte.
+          </Typography>
 
-                <Button component={NextLinkComposed} to="/about/board" variant="contained" size="medium">
-                  Les mer
-                </Button>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Grid>
+          <Button component={Link} href="/about/board" noLinkStyle variant="contained" size="medium">
+            Les mer
+          </Button>
+        </Stack>
       </Container>
     </Box>
   );
