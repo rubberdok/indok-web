531d3d9f6be522586c526c9484aa6ac9b7a1ad5c
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-03-19 04:50:14 +0100

    :recycle: refactor: switch between popover and dialog based on screen size

--- frontend/src/layouts/components/ColorModeSwitcher.tsx
+++ frontend/src/layouts/components/ColorModeSwitcher.tsx
@@ -8,22 +8,25 @@ import {
   DialogTitle,
   Grow,
   IconButton,
+  Popover,
   Stack,
+  Theme,
   Typography,
   Unstable_Grid2 as Grid,
+  useMediaQuery,
 } from "@mui/material";
 import { useColorScheme } from "@mui/material/styles";
 import { useState } from "react";
 
 export const ColorModeSwitcher: React.FC = () => {
-  const { mode, setMode } = useColorScheme();
-  const [open, setOpen] = useState(false);
+  const { handleOpen, handleClose, dialogOpen, popoverOpen, anchorEl } = useColorSchemePreview();
+
   return (
     <>
       <Stack justifyContent="center" alignItems="center">
         <IconButton
           size="small"
-          onClick={() => setOpen(true)}
+          onClick={handleOpen}
           sx={(theme) => ({
             color: "text.primary",
             position: "absolute",
@@ -37,7 +40,7 @@ export const ColorModeSwitcher: React.FC = () => {
         </IconButton>
         <IconButton
           size="small"
-          onClick={() => setOpen(true)}
+          onClick={handleOpen}
           sx={(theme) => ({
             color: "text.primary",
             position: "absolute",
@@ -50,82 +53,130 @@ export const ColorModeSwitcher: React.FC = () => {
           <DarkMode fontSize="small" />
         </IconButton>
       </Stack>
-      <Dialog open={open} onClose={() => setOpen(false)} fullWidth maxWidth="xs">
+
+      <Dialog open={dialogOpen} onClose={() => handleClose()} fullWidth maxWidth="xs">
         <DialogTitle>Utseende</DialogTitle>
         <DialogContent>
-          <Grid container direction="row" spacing={2} sx={{ aspectRatio: "11/4" }}>
-            <Grid container direction="column" spacing={1} data-color-scheme="light" xs={4}>
-              <Grid xs>
-                <Card
-                  variant="outlined"
-                  sx={{
-                    width: "100%",
-                    height: "100%",
-                    position: "relative",
-                  }}
-                >
-                  <CardActionArea sx={{ width: "100%", height: "100%" }} onClick={() => setMode("light")}>
-                    <Grow in={mode === "light"} appear={false}>
-                      <CheckCircle sx={{ position: "absolute", bottom: 0, right: 0, color: "text.primary" }} />
-                    </Grow>
-                    <CardContentPreview />
-                  </CardActionArea>
-                </Card>
-              </Grid>
-              <Grid>
-                <Typography variant="body2">Lys</Typography>
-              </Grid>
-            </Grid>
-            <Grid container direction="column" spacing={1} data-color-scheme="dark" xs={4}>
-              <Grid xs>
-                <Card
-                  variant="outlined"
-                  sx={{
-                    width: "100%",
-                    height: "100%",
-                    position: "relative",
-                  }}
-                >
-                  <CardActionArea sx={{ width: "100%", height: "100%" }} onClick={() => setMode("dark")}>
-                    <Grow in={mode === "dark"} appear={false}>
-                      <CheckCircle sx={{ position: "absolute", bottom: 0, right: 0, color: "text.primary" }} />
-                    </Grow>
-                    <CardContentPreview />
-                  </CardActionArea>
-                </Card>
-              </Grid>
-              <Grid>
-                <Typography variant="body2">Mørk</Typography>
-              </Grid>
-            </Grid>
-            <Grid container direction="column" spacing={1} xs={4}>
-              <Grid container xs spacing={0}>
-                <Card variant="outlined" sx={{ height: "100%", width: "100%", padding: "none", position: "relative" }}>
-                  <CardActionArea sx={{ height: "100%", width: "100%" }} onClick={() => setMode("system")}>
-                    <Grid container sx={{ height: "100%" }} xs>
-                      <Grid xs={6} data-color-scheme="dark">
-                        <CardContentPreview />
-                      </Grid>
-                      <Grid xs={6} data-color-scheme="light">
-                        <Grow in={mode === "system"} appear={false}>
-                          <CheckCircle sx={{ position: "absolute", bottom: 0, right: 0, color: "text.primary" }} />
-                        </Grow>
-                        <CardContentPreview />
-                      </Grid>
-                    </Grid>
-                  </CardActionArea>
-                </Card>
-              </Grid>
-              <Grid>
-                <Typography variant="body2">Automatisk</Typography>
-              </Grid>
-            </Grid>
-          </Grid>
+          <ColorModePreview />
         </DialogContent>
       </Dialog>
+
+      <Popover
+        open={popoverOpen}
+        anchorEl={anchorEl}
+        onClose={() => handleClose()}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <Card
+          sx={{
+            width: "500px",
+          }}
+        >
+          <CardContent>
+            <ColorModePreview />
+          </CardContent>
+        </Card>
+      </Popover>
     </>
   );
 };
+
+function useColorSchemePreview() {
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+  const isDesktop = useMediaQuery<Theme>((theme) => theme.breakpoints.up("md"));
+  const dialogOpen = Boolean(anchorEl) && !isDesktop;
+  const popoverOpen = Boolean(anchorEl) && isDesktop;
+
+  function handleOpen(e: React.MouseEvent<HTMLButtonElement>) {
+    setAnchorEl(e.currentTarget);
+  }
+
+  function handleClose() {
+    setAnchorEl(null);
+  }
+  return { dialogOpen, popoverOpen, handleOpen, handleClose, anchorEl };
+}
+
+function ColorModePreview() {
+  const { mode, setMode } = useColorScheme();
+  return (
+    <Grid container direction="row" spacing={2} sx={{ aspectRatio: { xs: "11/4", md: "12/4" } }}>
+      <Grid container direction="column" spacing={1} data-color-scheme="light" xs={4}>
+        <Grid xs>
+          <Card
+            variant="outlined"
+            sx={{
+              width: "100%",
+              height: "100%",
+              position: "relative",
+            }}
+          >
+            <CardActionArea sx={{ width: "100%", height: "100%" }} onClick={() => setMode("light")}>
+              <Grow in={mode === "light"} appear={false}>
+                <CheckCircle sx={{ position: "absolute", bottom: 0, right: 0, color: "text.primary" }} />
+              </Grow>
+              <CardContentPreview />
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid>
+          <Typography variant="body2">Lys</Typography>
+        </Grid>
+      </Grid>
+      <Grid container direction="column" spacing={1} data-color-scheme="dark" xs={4}>
+        <Grid xs>
+          <Card
+            variant="outlined"
+            sx={{
+              width: "100%",
+              height: "100%",
+              position: "relative",
+            }}
+          >
+            <CardActionArea sx={{ width: "100%", height: "100%" }} onClick={() => setMode("dark")}>
+              <Grow in={mode === "dark"} appear={false}>
+                <CheckCircle sx={{ position: "absolute", bottom: 0, right: 0, color: "text.primary" }} />
+              </Grow>
+              <CardContentPreview />
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid>
+          <Typography variant="body2">Mørk</Typography>
+        </Grid>
+      </Grid>
+      <Grid container direction="column" spacing={1} xs={4}>
+        <Grid container xs spacing={0}>
+          <Card variant="outlined" sx={{ height: "100%", width: "100%", padding: "none", position: "relative" }}>
+            <CardActionArea sx={{ height: "100%", width: "100%" }} onClick={() => setMode("system")}>
+              <Grid container sx={{ height: "100%" }} xs>
+                <Grid xs={6} data-color-scheme="dark">
+                  <CardContentPreview />
+                </Grid>
+                <Grid xs={6} data-color-scheme="light">
+                  <Grow in={mode === "system"} appear={false}>
+                    <CheckCircle sx={{ position: "absolute", bottom: 0, right: 0, color: "text.primary" }} />
+                  </Grow>
+                  <CardContentPreview />
+                </Grid>
+              </Grid>
+            </CardActionArea>
+          </Card>
+        </Grid>
+        <Grid>
+          <Typography variant="body2">Automatisk</Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
 
 function CardContentPreview() {
   return (
