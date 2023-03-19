9c69fa2f2fa979e455fee15b70c980b69a50c052
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-03-19 01:45:47 +0100

    chore: prevent wrapping for landing buttons

--- frontend/src/components/landing/LandingHero/index.tsx
+++ frontend/src/components/landing/LandingHero/index.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, Container, Grid, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Unstable_Grid2 as Grid, Stack, Typography } from "@mui/material";
 import { useTheme } from "@mui/material/styles";
 import Image from "next/image";
 
-import { NextLinkComposed } from "@/components";
+import { Link, NextLinkComposed } from "@/components";
 import Hero from "~/public/static/landing/hero.webp";
 
 import { OrganizationsSlider } from "./OrganizationsSlider";
@@ -24,36 +24,54 @@ export const LandingHero: React.FC = () => {
       >
         <Container sx={{ alignSelf: "center", gridColumn: "1 / -1", gridRow: "1" }}>
           <Grid container direction="row">
-            <Grid item md={5} xs={12}>
-              <Stack spacing={4} mt={14} mb={8}>
-                <Grid container gap={2} direction="column" alignItems={{ xs: "center", md: "flex-start" }}>
-                  <Grid item>
-                    <Typography
-                      variant="overline"
-                      sx={(theme) => ({
-                        color: "primary.main",
-                        [theme.getColorSchemeSelector("dark")]: { color: "primary.light" },
-                      })}
-                    >
-                      Foreningen for studentene ved
-                    </Typography>
-                  </Grid>
-                  <Grid item xs={8}>
-                    <Typography variant="h1" textAlign={{ xs: "center", md: "left" }}>
-                      Industriell Økonomi & Teknologiledelse
-                    </Typography>
-                  </Grid>
-                </Grid>
-
-                <Stack spacing={{ xs: 1.5, md: 2 }} direction={{ xs: "column", md: "row" }}>
-                  <Button component={NextLinkComposed} to="/about" variant="contained" size="large">
-                    Les om foreningen
-                  </Button>
-                  <Button component={NextLinkComposed} to="/events" variant="contained" color="contrast" size="large">
-                    Se arrangementer
-                  </Button>
+            <Grid container md={6} xs={12} spacing={4} mt={14} mb={8}>
+              <Grid>
+                <Stack spacing={2} alignItems={{ xs: "center", md: "flex-start" }}>
+                  <Typography
+                    variant="overline"
+                    sx={(theme) => ({
+                      color: "primary.main",
+                      [theme.getColorSchemeSelector("dark")]: { color: "primary.light" },
+                    })}
+                  >
+                    Foreningen for studentene ved
+                  </Typography>
+                  <Typography variant="h1" textAlign={{ xs: "center", md: "left" }}>
+                    Industriell Økonomi & Teknologiledelse
+                  </Typography>
                 </Stack>
-              </Stack>
+              </Grid>
+              <Grid
+                container
+                xs={12}
+                justifyContent={{ xs: "center", md: "flex-start" }}
+                alignItems={{ xs: "center", md: "flex-start" }}
+                spacing={2}
+                direction={{ xs: "column", md: "row" }}
+              >
+                <Grid xs={10} md={5}>
+                  <Button fullWidth component={Link} noLinkStyle href="/about" variant="contained" size="large">
+                    <Typography variant="inherit" noWrap>
+                      Les om foreningen
+                    </Typography>
+                  </Button>
+                </Grid>
+                <Grid xs={10} md={5}>
+                  <Button
+                    fullWidth
+                    component={Link}
+                    noLinkStyle
+                    href="/events"
+                    variant="contained"
+                    color="contrast"
+                    size="large"
+                  >
+                    <Typography variant="inherit" noWrap>
+                      Se arrangementer
+                    </Typography>
+                  </Button>
+                </Grid>
+              </Grid>
             </Grid>
           </Grid>
         </Container>
