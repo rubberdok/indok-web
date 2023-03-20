7713f1f715aa440877b75e84526e276646856309
MagnusHafstad <114174209+MagnusHafstad@users.noreply.github.com>
2023-03-20 17:38:04 +0100

    Fix archive (#1597)
    * Add thumbnail field to document model
    
    Co-authored-by: Lars Waage <larwaa@users.noreply.github.com>
    
    * Update types
    
    * Update graphql schema
    
    ---------
    
    Co-authored-by: Lars Waage <larwaa@users.noreply.github.com>

--- backend/apps/archive/types.py
+++ backend/apps/archive/types.py
@@ -1,7 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from .google_drive_api import GoogleDriveAPI
 from .models import ArchiveDocument
 
 
@@ -10,8 +9,3 @@ class ArchiveDocumentType(DjangoObjectType):
 
     class Meta:
         model = ArchiveDocument
-
-    @staticmethod
-    def resolve_thumbnail(root: ArchiveDocument, info):
-        drive = GoogleDriveAPI()
-        return drive.get_thumbnail(root.file_location)
