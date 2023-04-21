6c43f0029585cc00dff95c6b4300848de0acfda4
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-04-21 14:10:14 +0200

    :recycle: refactor: simplify cabin info picker, replace formik

---
+++ frontend/src/components/pages/cabins/Admin/CabinInfoPicker/CabinInfoForm.tsx
@@ -0,0 +1,159 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Unstable_Grid2 as Grid, TextField, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+
+export type CabinInfoForm = {
+  oksen?: {
+    internalPrice: number;
+    externalPrice: number;
+    maxGuests: number;
+  };
+  bjornen?: {
+    internalPrice: number;
+    externalPrice: number;
+    maxGuests: number;
+  };
+};
+
+const validationSchema = yup.object().shape({
+  oksen: yup.object().shape({
+    internalPrice: yup.number().positive().required().label("Internpris"),
+    externalPrice: yup.number().positive().required().label("Eksternpris"),
+    maxGuests: yup.number().positive().required().label("Kapasitet (antall gjester)"),
+  }),
+  bjornen: yup.object().shape({
+    internalPrice: yup.number().positive().required().label("Internpris"),
+    externalPrice: yup.number().positive().required().label("Eksternpris"),
+    maxGuests: yup.number().positive().required().label("Kapasitet (antall gjester)"),
+  }),
+});
+
+type Props = {
+  defaultValues: CabinInfoForm;
+  onSubmit: (data: CabinInfoForm) => void;
+  values?: CabinInfoForm;
+};
+
+export const CabinInfoForm: React.FC<Props> = ({ defaultValues, values, onSubmit }) => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<CabinInfoForm>({
+    resolver: yupResolver(validationSchema),
+    mode: "onTouched",
+    defaultValues,
+    values,
+  });
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Grid container direction="column" spacing={2}>
+        <Grid container direction={{ xs: "column", md: "row" }} justifyContent="space-between">
+          <Grid container direction="column" xs={12} md={5}>
+            <Grid>
+              <Typography variant="subtitle1">Bjørnen</Typography>
+            </Grid>
+            <Grid>
+              <TextField
+                {...register("bjornen.internalPrice")}
+                required
+                helperText={errors.bjornen?.internalPrice?.message}
+                error={Boolean(errors.bjornen?.internalPrice)}
+                type="number"
+                fullWidth
+                label="Internpris"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid>
+              <TextField
+                {...register("bjornen.externalPrice")}
+                required
+                helperText={errors.bjornen?.externalPrice?.message}
+                error={Boolean(errors.bjornen?.externalPrice)}
+                type="number"
+                fullWidth
+                label="Eksternpris"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid>
+              <TextField
+                {...register("bjornen.maxGuests")}
+                required
+                helperText={errors.bjornen?.maxGuests?.message}
+                error={Boolean(errors.bjornen?.maxGuests)}
+                type="number"
+                fullWidth
+                label="Kapasitet (antall gjester)"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+          </Grid>
+          <Grid container direction="column" xs={12} md={5}>
+            <Grid>
+              <Typography variant="subtitle1">Oksen</Typography>
+            </Grid>
+            <Grid>
+              <TextField
+                {...register("oksen.internalPrice")}
+                required
+                helperText={errors.oksen?.internalPrice?.message}
+                error={Boolean(errors.oksen?.internalPrice)}
+                type="number"
+                fullWidth
+                label="Internpris"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid>
+              <TextField
+                {...register("oksen.externalPrice")}
+                required
+                helperText={errors.oksen?.externalPrice?.message}
+                error={Boolean(errors.oksen?.externalPrice)}
+                type="number"
+                fullWidth
+                label="Eksternpris"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+            <Grid>
+              <TextField
+                {...register("oksen.maxGuests")}
+                required
+                helperText={errors.oksen?.maxGuests?.message}
+                error={Boolean(errors.oksen?.maxGuests)}
+                type="number"
+                fullWidth
+                label="Kapasitet (antall gjester)"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container direction="row" justifyContent="flex-end">
+          <Grid>
+            <Button variant="contained" type="submit">
+              Lagre
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
