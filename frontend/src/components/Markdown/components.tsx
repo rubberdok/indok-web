d00c1b8605fcfeb066862cf9a2a0fb0e36af80fa
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-04-25 03:37:01 +0200

    :sparkles: feat: add list components to markdown

--- frontend/src/components/Markdown/components.tsx
+++ frontend/src/components/Markdown/components.tsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, List, ListItemText, ListItem as MuiListItem, Typography } from "@mui/material";
 import { Components } from "react-markdown";
 
 const Heading: Components["h1"] = ({ children, id, level }) => {
@@ -18,6 +18,25 @@ const Image: Components["img"] = (props) => {
   );
 };
 
+const UnorderedList: Components["ul"] = ({ children }) => {
+  return <List>{children}</List>;
+};
+
+const ListItem: Components["li"] = ({ ordered, children }) => {
+  if (ordered) {
+    return (
+      <MuiListItem sx={{ listStyleType: "decimal" }}>
+        <ListItemText sx={{ display: "list-item" }} primary={children} />
+      </MuiListItem>
+    );
+  }
+  return (
+    <MuiListItem sx={{ listStyleType: "disc" }}>
+      <ListItemText sx={{ display: "list-item" }} primary={children} />
+    </MuiListItem>
+  );
+};
+
 export const h1 = Heading;
 export const h2 = Heading;
 export const h3 = Heading;
@@ -25,6 +44,8 @@ export const h4 = Heading;
 export const h5 = Heading;
 export const h6 = Heading;
 export const img = Image;
+export const ul = UnorderedList;
+export const li = ListItem;
 
 export const p: Components["p"] = ({ children }) => (
   <Typography variant="body1" paragraph>
