b9abaec69773edfb9d248046499c462bf1060808
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-04-26 13:21:32 +0200

    chore: add sentry edge config, update to latest with-sentry example

---
+++ frontend/sentry.edge.config.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import { init } from "@sentry/nextjs";
+
+import { config } from "@/utils/config";
+
+const SENTRY_DSN = config.APP_ENV === "production" ? config.SENTRY_DSN : undefined;
+
+init({
+  dsn: SENTRY_DSN,
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  // ...
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+  environment: config.APP_ENV,
+});
