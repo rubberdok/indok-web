603a022d97039d6dfcf374ddb0acae2396ea956e
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-04-26 18:01:20 +0200

    :sparkles: feat: add anchor and code blocks

--- frontend/src/components/Markdown/components.tsx
+++ frontend/src/components/Markdown/components.tsx
@@ -1,6 +1,9 @@
-import { Box, List, ListItemText, ListItem as MuiListItem, Typography } from "@mui/material";
+import { Box, Card, CardContent, List, ListItemText, ListItem as MuiListItem, Paper, Typography } from "@mui/material";
 import { listItemClasses } from "@mui/material/ListItem";
+import { styled } from "@mui/material/styles";
 import { Components } from "react-markdown";
+
+import Link from "../Link";
 
 const Heading: Components["h1"] = ({ children, id, level }) => {
   const variant = `h${Math.max(level, 3)}` as "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
@@ -59,6 +62,33 @@ const ListItem: Components["li"] = ({ children }) => {
   );
 };
 
+const Anchor: Components["a"] = ({ children, href }) => {
+  return <Link href={href ?? ""}>{children}</Link>;
+};
+
+const Code = styled("code")({});
+
+const CodeBlock: Components["code"] = ({ children, inline }) => {
+  if (inline)
+    return (
+      <Code
+        sx={{ bgcolor: "background.elevated", px: 1, py: 0.5, borderRadius: (theme) => theme.shape.borderRadius / 2 }}
+      >
+        {children}
+      </Code>
+    );
+  return (
+    <Card
+      sx={{ display: inline ? "inline" : "block", bgcolor: "background.elevated", overflowX: "scroll" }}
+      elevation={0}
+    >
+      <CardContent>
+        <code>{children}</code>
+      </CardContent>
+    </Card>
+  );
+};
+
 export const h1 = Heading;
 export const h2 = Heading;
 export const h3 = Heading;
@@ -69,6 +99,8 @@ export const img = Image;
 export const ul = UnorderedList;
 export const li = ListItem;
 export const ol = OrderedList;
+export const a = Anchor;
+export const code = CodeBlock;
 
 export const p: Components["p"] = ({ children }) => (
   <Typography variant="body1" paragraph>
