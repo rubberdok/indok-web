faf10f336fb432b376a7d163d51ec68dcfd9aa84
Lars Waage <46653859+larwaa@users.noreply.github.com>
2023-08-22 20:57:09 +0200

    docs: add permission required docstring

--- frontend/src/app/components/PermissionRequired/index.tsx
+++ frontend/src/app/components/PermissionRequired/index.tsx
@@ -4,6 +4,12 @@ import { PropsWithChildren } from "react";
 import { graphql } from "@/gql";
 import { HasPermissionQuery } from "@/gql/graphql";
 
+/**
+ * Wrapper component that renders its children if the user has the given permission.
+ * Permissions can either be passed as strings, or as a query reference if using `useBackgroundQuery`
+ * from @apollo/client. Using the latter can result in better performance, as the permission check can
+ * be done in the background while the rest of the page is rendered.
+ */
 export function PermissionRequired<T extends HasPermissionQuery>(
   props: React.PropsWithChildren<BackgroundQueryProps<T> | PermissionRequiredProps>
 ) {
