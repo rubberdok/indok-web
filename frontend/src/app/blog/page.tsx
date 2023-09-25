f033b587a866ecc28c30b6d29bb44e68c1e987e3
Thomas Falkeid Hagland <thomas.hagland03@gmail.com>
2023-09-25 20:00:39 +0200

    beginning of blog func

--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -1,5 +1,48 @@
 "use client";
 
-export default function blog() {
-  return;
+import { useState } from "react";
+
+function MarkdownEditor() {
+  const [markdown, setMarkdown] = useState<string>("");
+
+  const renderMarkdown = (text: string) => {
+    let renderedText = text;
+
+    // Render bold text: **bold**
+    renderedText = renderedText.replace(/\/b\{(.*?)\}/g, "<strong>$1</strong>");
+
+    // Render italic text: *italic*
+    renderedText = renderedText.replace(/\/i\{(.*?)\}/g, "<em>$1</em>");
+
+    // Render links to photos: ![alt text](url)
+    renderedText = renderedText.replace(/\/img\{(.*?)\}/g, "<img src='$1' />");
+
+    // Render links to websites: [link text](url)
+    renderedText = renderedText.replace(/\[(.*?)\]\((.*?)\)/g, "<a href='$2' target='_blank'>$1</a>");
+
+    return { __html: renderedText };
+  };
+
+  return (
+    <div>
+      <textarea
+        value={markdown}
+        onChange={(e) => setMarkdown(e.target.value)}
+        style={{ width: "100%", minHeight: "100px" }}
+      />
+      <div style={{ marginTop: "20px" }}>
+        <strong>Rendered Markdown:</strong>
+        <div dangerouslySetInnerHTML={renderMarkdown(markdown)} />
+      </div>
+    </div>
+  );
+}
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Markdown Editor</h1>
+      <MarkdownEditor />
+    </div>
+  );
 }
