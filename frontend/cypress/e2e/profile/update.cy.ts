ca7dc731b1a994fd4702a10d167b2449b77cefc0
MagnusHafstad <114174209+MagnusHafstad@users.noreply.github.com>
2023-09-28 17:56:32 +0200

    Remove comment

--- frontend/cypress/e2e/profile/update.cy.ts
+++ frontend/cypress/e2e/profile/update.cy.ts
@@ -20,7 +20,7 @@ describe("On the profile page", () => {
     it("should be able to update personal information", () => {
       cy.log("Accessing site");
       cy.visit("/profile");
-      //cy.getByTestId("profile-personal-name").should("contain.text", "Eva Student Åsen");
+      cy.getByTestId("profile-personal-name").should("contain.text", "Eva Student Åsen");
       cy.getByTestId("profile-personal-link").click();
       cy.getByTestId("editUser-firstNameTextField").within(() => cy.get("input").clear().type("Newname"));
       cy.getByTestId("editUser-saveButton").click();
