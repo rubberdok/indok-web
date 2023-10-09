eddcd1532315bc800411aa488125e29449f9e3b2
Thomas Falkeid Hagland <thomas.hagland03@gmail.com>
2023-10-09 17:59:38 +0200

    add image tag

--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -1,130 +1,112 @@
 "use client";
 
 import { ButtonBase, Container, Stack, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid";
 import { useState } from "react";
-import Grid from '@mui/material/Grid';
 export default function Blog() {
-
-    return (
-        <Container sx={{width: "75%", marginTop: "40px"}}>
-
-            <Editor/>
-
-        </Container>
-    );
-
+  return (
+    <Container sx={{ width: "75%", marginTop: "40px" }}>
+      <Editor />
+    </Container>
+  );
 }
 
 function Toolbar() {
-
-    return (
-        <div
-            style={{width: "100%"}}
-        >
-            <Stack
-            direction="row"
-            sx={{
-                borderWidth: "1px",
-                borderColor: "gray",
-                borderStyle: "dashed"
-            }}
-        >
-            <ToolbarButton
-                insert="/b{}"
-                body={<strong>B</strong>}
-            />
-            <ToolbarButton
-                insert="/i{}"
-                body={<strong><i>I</i></strong>}
-            />
-            <ToolbarButton
-                insert="/28{}"
-                body={<Typography sx={{fontSize: "28px"}}>h1</Typography>}
-            />
-            <ToolbarButton
-                insert="/24{}"
-                body={<Typography sx={{fontSize: "24px"}}>h2</Typography>}
-            />
-            <ToolbarButton
-                insert="/20{}"
-                body={<Typography sx={{fontSize: "20px"}}>p</Typography>}
-            />
-        </Stack>
-        </div>
-    )
-
+  return (
+    <div style={{ width: "100%" }}>
+      <Stack
+        direction="row"
+        sx={{
+          borderWidth: "1px",
+          borderColor: "gray",
+          borderStyle: "dashed",
+        }}
+      >
+        <ToolbarButton insert="/b{}" body={<strong>B</strong>} />
+        <ToolbarButton
+          insert="/i{}"
+          body={
+            <strong>
+              <i>I</i>
+            </strong>
+          }
+        />
+        <ToolbarButton insert="/28{}" body={<Typography sx={{ fontSize: "28px" }}>h1</Typography>} />
+        <ToolbarButton insert="/24{}" body={<Typography sx={{ fontSize: "24px" }}>h2</Typography>} />
+        <ToolbarButton insert="/20{}" body={<Typography sx={{ fontSize: "20px" }}>p</Typography>} />
+        <ToolbarButton insert="/img 50,50{}" body={<Typography sx={{ fontSize: "20px" }}>Img</Typography>} />
+      </Stack>
+    </div>
+  );
 }
 
-function ToolbarButton({insert, body}: {insert: string, body: any}) {
-    return (
-        <ButtonBase
-            sx={{
-                fontSize: "20px",
-                minWidth: "50px",
-                minHeight: "50px"
-            }}
-            onClick={() => {
-                let e = document.getElementById("input") as HTMLTextAreaElement
-                let cursor = e.selectionStart
-                e.value = e.value.substring(0, cursor) + insert + e.value.substring(cursor)
-                e.focus()
-                e.selectionStart = cursor + insert.length - 1
-                e.selectionEnd = cursor + insert.length - 1
-            }}
-        >
-            {body}
-        </ButtonBase>)
+function ToolbarButton({ insert, body }: { insert: string; body: any }) {
+  return (
+    <ButtonBase
+      sx={{
+        fontSize: "20px",
+        minWidth: "50px",
+        minHeight: "50px",
+      }}
+      onClick={() => {
+        const e = document.getElementById("input") as HTMLTextAreaElement;
+        const cursor = e.selectionStart;
+        e.value = e.value.substring(0, cursor) + insert + e.value.substring(cursor);
+        e.focus();
+        e.selectionStart = cursor + insert.length - 1;
+        e.selectionEnd = cursor + insert.length - 1;
+      }}
+    >
+      {body}
+    </ButtonBase>
+  );
 }
 
 function Editor() {
+  const [text, setText] = useState("");
 
-    const [text, setText] = useState("");
+  function rendered() {
+    const outputString = text
+      .replaceAll(/\/b\{(.*?)\}/g, (_, p1) => `<strong>${p1}</strong>`)
+      .replaceAll(/\/i\{(.*?)\}/g, (_, p1) => `<i>${p1}</i>`)
+      .replaceAll("\n", (_) => `<br>`)
+      .replaceAll(" ", (_) => " ")
+      .replaceAll(/\/([0-9]*)\{(.*?)\}/g, (_, p1, p2) => `<span style="font-size: ${p1}px">${p2}</span>`)
+      .replaceAll(/\/img([0-9]*)\{(.*?)\}/g, (_, p2, p1) => `<img src=${p1} width="${p2}" ></img>`);
 
-    function rendered() {
+    return <div dangerouslySetInnerHTML={{ __html: outputString }} />;
+  }
 
-        const outputString = text
-            .replaceAll(/\/b\{(.*?)\}/g, (_, p1) => `<strong>${p1}</strong>`)
-            .replaceAll(/\/i\{(.*?)\}/g, (_, p1) => `<i>${p1}</i>`)
-            .replaceAll("\n", (_) => `<br>`)
-            .replaceAll(" ", (_) => " ")
-            .replaceAll(/\/([0-9]*)\{(.*?)\}/g, (_, p1, p2) => `<span style="font-size: ${p1}px">${p2}</span>`)
+  return (
+    <Grid container columnSpacing={2}>
+      <Grid item md={6}>
+        <Toolbar />
+      </Grid>
 
+      <Grid item md={6}>
+        <h2>Rendered</h2>
+      </Grid>
 
-        return <div
-            dangerouslySetInnerHTML={{__html: outputString}}
+      <Grid item md={6}>
+        <textarea
+          id="input"
+          style={{
+            width: "100%",
+            height: "100vh",
+            fontSize: "100%",
+          }}
+          autoFocus={true}
+          onChange={(e) => {
+            setText(e.target.value);
+            console.log(e.target.value);
+          }}
+          placeholder="Skriv den jævla bloggen din her ..."
         />
-    }
+      </Grid>
 
-    return (
-        <Grid container columnSpacing={2}>
-
-            <Grid item md={6}>
-                <Toolbar/>
-            </Grid>
-
-            <Grid item md={6}>
-                <h2>Rendered</h2>
-            </Grid>
-
-            <Grid item md={6}>
-                <textarea
-                    id="input"
-                    style={{
-                        width: "100%",
-                        height: "100vh",
-                        fontSize: "100%"
-                    }}
-                    autoFocus={true}
-                    onChange={(e) => {setText(e.target.value); console.log(e.target.value)}}
-                    placeholder="Skriv den jævla bloggen din her ..."
-                />
-            </Grid>
-
-            <Grid md={6} item>
-                {rendered()}
-            </Grid>
-
-        </Grid>
-    )
-
+      <Grid md={6} item>
+        {rendered()}
+      </Grid>
+    </Grid>
+  );
 }
