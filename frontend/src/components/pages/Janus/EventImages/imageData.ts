400a365333353d4a34263ee8cde61a82e9f8a0bf
MagnusHafstad <114174209+MagnusHafstad@users.noreply.github.com>
2023-10-12 19:04:15 +0200

    Delete all images

--- frontend/src/components/pages/Janus/EventImages/imageData.ts
+++ frontend/src/components/pages/Janus/EventImages/imageData.ts
@@ -1,6 +1,69 @@
 import { StaticImageData } from "next/image";
 
+import mai1 from "~/public/static/janusArrangement/17mai/17mai1.jpg";
+import mai2 from "~/public/static/janusArrangement/17mai/17mai2.jpg";
+import mai3 from "~/public/static/janusArrangement/17mai/17mai3.jpg";
+import mai4 from "~/public/static/janusArrangement/17mai/17mai4.jpg";
+import mai5 from "~/public/static/janusArrangement/17mai/17mai5.jpg";
+import mai6 from "~/public/static/janusArrangement/17mai/17mai6.jpg";
+import Aretur1 from "~/public/static/janusArrangement/Aretur/Aretur1.jpg";
+import Aretur2 from "~/public/static/janusArrangement/Aretur/Aretur2.jpg";
+import Aretur3 from "~/public/static/janusArrangement/Aretur/Aretur3.jpg";
+import Aretur4 from "~/public/static/janusArrangement/Aretur/Aretur4.jpg";
+import Aretur5 from "~/public/static/janusArrangement/Aretur/Aretur5.jpg";
+import Bors1 from "~/public/static/janusArrangement/Borsfest/Bors1.jpg";
+import Bors10 from "~/public/static/janusArrangement/Borsfest/Bors10.jpg";
+import Bors11 from "~/public/static/janusArrangement/Borsfest/Bors11.jpg";
+import Bors2 from "~/public/static/janusArrangement/Borsfest/Bors2.jpg";
+import Bors3 from "~/public/static/janusArrangement/Borsfest/Bors3.jpg";
+import Bors4 from "~/public/static/janusArrangement/Borsfest/Bors4.jpg";
+import Bors5 from "~/public/static/janusArrangement/Borsfest/Bors5.jpg";
+import Bors6 from "~/public/static/janusArrangement/Borsfest/Bors6.jpg";
+import Bors7 from "~/public/static/janusArrangement/Borsfest/Bors7.jpg";
+import Bors8 from "~/public/static/janusArrangement/Borsfest/Bors8.jpg";
+import Bors9 from "~/public/static/janusArrangement/Borsfest/Bors9.jpg";
+import DSCF5226 from "~/public/static/janusArrangement/Fadderuker/DSCF5226.jpg";
+import DSCF5299 from "~/public/static/janusArrangement/Fadderuker/DSCF5299.jpg";
+import DSCF5301 from "~/public/static/janusArrangement/Fadderuker/DSCF5301.jpg";
 import IMG_0004 from "~/public/static/janusArrangement/Immball/IMG_0004.jpg";
+import IMG_0049 from "~/public/static/janusArrangement/Immball/IMG_0049.jpg";
+import IMG_9288 from "~/public/static/janusArrangement/Immball/IMG_9288.jpg";
+import IMG_9535 from "~/public/static/janusArrangement/Immball/IMG_9535.jpg";
+import IMG_9753 from "~/public/static/janusArrangement/Immball/IMG_9753.jpg";
+import IMG_9787 from "~/public/static/janusArrangement/Immball/IMG_9787.jpg";
+import IMG_9872 from "~/public/static/janusArrangement/Immball/IMG_9872.jpg";
+import IMG_9878 from "~/public/static/janusArrangement/Immball/IMG_9878.jpg";
+import IMG_9901 from "~/public/static/janusArrangement/Immball/IMG_9901.jpg";
+import IMG_9930 from "~/public/static/janusArrangement/Immball/IMG_9930.jpg";
+import IMG_9970 from "~/public/static/janusArrangement/Immball/IMG_9970.jpg";
+import IMG_9989 from "~/public/static/janusArrangement/Immball/IMG_9989.jpg";
+import DSCF5177 from "~/public/static/janusArrangement/janusvalgetImages/DSCF5177.jpg";
+import DSCF5211 from "~/public/static/janusArrangement/janusvalgetImages/DSCF5211.jpg";
+import DSCF5215 from "~/public/static/janusArrangement/janusvalgetImages/DSCF5215.jpg";
+import DSCF52261 from "~/public/static/janusArrangement/janusvalgetImages/DSCF52261.jpg";
+import DSCF5248 from "~/public/static/janusArrangement/janusvalgetImages/DSCF5248.jpg";
+import DSCF5267 from "~/public/static/janusArrangement/janusvalgetImages/DSCF5267.jpg";
+import DSCF5274 from "~/public/static/janusArrangement/janusvalgetImages/DSCF5274.jpg";
+import DSCF5302 from "~/public/static/janusArrangement/janusvalgetImages/DSCF5302.jpg";
+import DSCF5360 from "~/public/static/janusArrangement/janusvalgetImages/DSCF5360.jpg";
+import DSCF5407 from "~/public/static/janusArrangement/janusvalgetImages/DSCF5407.jpg";
+import DSC00823 from "~/public/static/janusArrangement/WinterGames/DSC00823.jpg";
+import DSC00928 from "~/public/static/janusArrangement/WinterGames/DSC00928.jpg";
+import DSC00964 from "~/public/static/janusArrangement/WinterGames/DSC00964.jpg";
+import DSC01059 from "~/public/static/janusArrangement/WinterGames/DSC01059.jpg";
+import DSC01065 from "~/public/static/janusArrangement/WinterGames/DSC01065.jpg";
+import DSC01072 from "~/public/static/janusArrangement/WinterGames/DSC01072.jpg";
+import DSC01074 from "~/public/static/janusArrangement/WinterGames/DSC01074.jpg";
+import DSC01209 from "~/public/static/janusArrangement/WinterGames/DSC01209.jpg";
+import IMG_8675 from "~/public/static/janusArrangement/WinterGames/IMG_8675.jpg";
+import IMG_8704 from "~/public/static/janusArrangement/WinterGames/IMG_8704.jpg";
+import IMG_8746 from "~/public/static/janusArrangement/WinterGames/IMG_8746.jpg";
+import IMG_8854 from "~/public/static/janusArrangement/WinterGames/IMG_8854.jpg";
+import IMG_8995 from "~/public/static/janusArrangement/WinterGames/IMG_8995.jpg";
+import IMG_9030 from "~/public/static/janusArrangement/WinterGames/IMG_9030.jpg";
+import IMG_9346 from "~/public/static/janusArrangement/WinterGames/IMG_9346.jpg";
+import IMG_9374 from "~/public/static/janusArrangement/WinterGames/IMG_9374.jpg";
+import IMG_9406 from "~/public/static/janusArrangement/WinterGames/IMG_9406.jpg";
 
 export type ImageData = {
   label: string;
@@ -13,7 +76,43 @@ export type ImageData = {
 export const immballImages: ImageData[] = [
   {
     label: "",
-    img: IMG_0004,
+    img: IMG_9878,
+  },
+  {
+    label: "",
+    img: IMG_9989,
+  },
+  {
+    label: "",
+    img: IMG_0049,
+  },
+  {
+    label: "",
+    img: IMG_9288,
+  },
+  {
+    label: "",
+    img: IMG_9753,
+  },
+  {
+    label: "",
+    img: IMG_9787,
+  },
+  {
+    label: "",
+    img: IMG_9872,
+  },
+  {
+    label: "",
+    img: IMG_9901,
+  },
+  {
+    label: "",
+    img: IMG_9930,
+  },
+  {
+    label: "",
+    img: IMG_9970,
   },
   {
     label: "",
@@ -21,114 +120,78 @@ export const immballImages: ImageData[] = [
   },
   {
     label: "",
-    img: IMG_0004,
-  },
-  {
-    label: "",
-    img: IMG_0004,
-  },
-  {
-    label: "",
-    img: IMG_0004,
-  },
-  {
-    label: "",
-    img: IMG_0004,
-  },
-  {
-    label: "",
-    img: IMG_0004,
-  },
-  {
-    label: "",
-    img: IMG_0004,
-  },
-  {
-    label: "",
-    img: IMG_0004,
-  },
-  {
-    label: "",
-    img: IMG_0004,
-  },
-  {
-    label: "",
-    img: IMG_0004,
-  },
-  {
-    label: "",
-    img: IMG_0004,
+    img: IMG_9535,
   },
 ];
 
 export const winterGamesImages: ImageData[] = [
   {
     label: "",
-    img: IMG_0004,
+    img: IMG_8746,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: IMG_8854,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: IMG_8995,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: IMG_9030,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: IMG_9346,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSC01059,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSC00928,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSC00964,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSC01065,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSC01074,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSC01209,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: IMG_9374,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: IMG_9406,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSC00823,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSC01072,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: IMG_8675,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: IMG_8704,
   },
 ];
 export const oktoberfestImages: ImageData[] = [
@@ -141,153 +204,153 @@ export const oktoberfestImages: ImageData[] = [
 export const aareturImages: ImageData[] = [
   {
     label: "",
-    img: IMG_0004,
+    img: Aretur1,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Aretur2,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Aretur3,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Aretur4,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Aretur5,
   },
 ];
 export const maiImages: ImageData[] = [
   {
     label: "",
-    img: IMG_0004,
+    img: mai1,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: mai2,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: mai3,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: mai4,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: mai5,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: mai6,
   },
 ];
 
 export const borsfestImages: ImageData[] = [
   {
     label: "",
-    img: IMG_0004,
+    img: Bors1,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Bors2,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Bors3,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Bors4,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Bors5,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Bors6,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Bors7,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Bors8,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Bors9,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Bors10,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: Bors11,
   },
 ];
 
 export const fadderukeImages: ImageData[] = [
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5226,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5299,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5301,
   },
 ];
 
 export const janusvalgetImages: ImageData[] = [
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5248,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5267,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5274,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5360,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5211,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5177,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5215,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5302,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF5407,
   },
   {
     label: "",
-    img: IMG_0004,
+    img: DSCF52261,
   },
 ];
