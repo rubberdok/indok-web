1d5cd07e21595f16ef922927f2cb978b98ae557c
Hannah Köberle <hannah.koeberleger@gmail.com>
2023-10-26 16:24:59 +0200

    edit boxes

--- frontend/src/components/pages/Janus/Shop/ShopItem.tsx
+++ frontend/src/components/pages/Janus/Shop/ShopItem.tsx
@@ -1,13 +1,14 @@
 import Image,{ StaticImageData } from "next/image";
-import { Box, ButtonBase, Stack } from "@mui/material";
-import { shadows } from "@/lib/mui/theme/shadows";
+import {Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
 
 type Props = {
   name: string;
   image?: StaticImageData;
+  price: number
 };
 import cabin from "~/public/static/cabins/00.jpg";
 
+/*
 export const ShopItem: React.VFC<Props> = ({name, image}) => {
   return (
     <Box sx={{
@@ -25,12 +26,35 @@ export const ShopItem: React.VFC<Props> = ({name, image}) => {
           <Stack sx={{ alignItems: 'start'}}>
             <Box>
               {cabin && (
-              <Image src={cabin} style={{ objectFit: "contain", width: "100%", height: "100%",} }/>
+              <Image src={cabin} style={{ objectFit: "contain", width: "100%", height: "100%", }} alt={""}/>
               )}
             </Box>
             <Box>{name}</Box>
           </Stack>
         </ButtonBase>
     </Box>
+  );
+};
+
+
+*/
+
+export const ShopItem: React.VFC<Props> = ({name, image, price}) => {
+  return (
+    <Card sx={{ width: "100%", maxWidth: 345 }}>
+      <CardActionArea href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley">
+          <CardMedia >
+              <Image src={cabin} style={{ objectFit: "contain", width: "100%", height: "100%", }} alt={""}/>
+          </CardMedia>
+        <CardContent>
+          <Typography variant="h5" textAlign="left">
+            {name}
+          </Typography>
+          <Typography variant="body2" textAlign="left" color="text.secondary">
+            -, {price}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
   );
 };
