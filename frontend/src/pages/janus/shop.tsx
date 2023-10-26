4bc58795bd54da37271adaf805317b0c0958b7d0
Hannah Köberle <hannah.koeberleger@gmail.com>
2023-10-26 16:24:37 +0200

    edit boxes in shop
    Co-authored-by: MagnusHafstad <MagnusHafstad@users.noreply.github.com>

--- frontend/src/pages/janus/shop.tsx
+++ frontend/src/pages/janus/shop.tsx
@@ -4,20 +4,28 @@ import { Template } from "@/components/pages/Janus/Template";
 import { ShopItem } from '@/components/pages/Janus/Shop/ShopItem';
 import { Layout } from "@/layouts/Layout";
 import { NextPageWithLayout } from "@/lib/next";
+import { useQuery } from "@apollo/client";
+import { ProductDocument } from "@/generated/graphql";
+
+
 
 const ShopPage: NextPageWithLayout = () => {
+const res = useQuery(//Products)
+console.log("Below")
+console.log(res)
+
   return (
     <Template title="Janus Nettbutikk" description="">
       <Typography variant="h3" gutterBottom>
         Butikk
         <Stack spacing={2}>
           <Stack direction={"row"} spacing={2}>
-            <ShopItem name="hytte"></ShopItem>
-            <ShopItem name="hytte"></ShopItem>
+            <ShopItem name="hytte" price={0}></ShopItem>
+            <ShopItem name="hytte" price={0}></ShopItem>
           </Stack>
           <Stack direction={"row"} spacing={2}>
-            <ShopItem name="hytte"></ShopItem>
-            <ShopItem name="hytte"></ShopItem>
+            <ShopItem name="hytte" price={0}></ShopItem>
+            <ShopItem name="hytte" price={0}></ShopItem>
           </Stack>
         </Stack>
 
