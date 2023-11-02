685b9df404850e548d3c70d194679bb39b713c92
MagnusHafstad <114174209+MagnusHafstad@users.noreply.github.com>
2023-11-02 18:58:12 +0100

    template for product page

--- frontend/src/pages/janus/[productID]/index.tsx
+++ frontend/src/pages/janus/[productID]/index.tsx
@@ -1,5 +1,17 @@
-const ProductPage = () => {
-    return <>Hello World</>
-}
+import { Typography } from "@mui/material";
+import { useRouter } from "next/router";
 
-export default ProductPage
+import { Template } from "@/components/pages/Janus/Template";
+import { NextPageWithLayout } from "@/lib/next";
+
+const ProductPage: NextPageWithLayout = () => {
+  //console.log("Hello World");
+
+  return (
+    <Template title="Genser" description="">
+      hei
+    </Template>
+  );
+};
+
+export default ProductPage;
