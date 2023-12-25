ca981d39f50903007d21a7ce5049984af7749eb0
MagnusHafstad <114174209+MagnusHafstad@users.noreply.github.com>
2023-12-25 16:01:08 +0100

    Add nights type finder

--- frontend/src/components/pages/cabins/booking/Steps/calculatePrice.ts
+++ frontend/src/components/pages/cabins/booking/Steps/calculatePrice.ts
@@ -8,13 +8,32 @@ export function calculatePrice(
 ): number | undefined {
   if (!contactInfo) return NaN;
   const internalPrice = contactInfo.internalParticipants >= contactInfo.externalParticipants;
-  const pricePerNight = chosenCabins
-    .map((cabin) => (internalPrice ? cabin.internalPrice : cabin.externalPrice))
-    .reduce((sum, currentPrice) => sum + currentPrice);
-
-  if (startDate && endDate) {
-    const duration = endDate.diff(startDate, "day");
-    return pricePerNight * duration;
+  let currentDate = dayjs(startDate);
+  const finalDate = dayjs(endDate);
+  let weekdayNights = 0;
+  let weekendNights = 0;
+  while (currentDate.isBefore(finalDate, "day")) {
+    // Last day has no night.
+    if (currentDate.day() === 5 || currentDate.day() === 6) {
+      // 5 = Friday to sat, 6 = Saturday to sunday
+      weekendNights++;
+    } else {
+      weekdayNights++;
+    }
+    currentDate = currentDate.add(1, "day");
   }
+  console.log("Weekday nights", weekdayNights);
+  console.log("Weekend nights", weekendNights);
+
+  /*
+  if internalPrice {
+    return weekdayNights*pricePerWeekdayNight + weekendNights*pricePerWeekendNight;
+  }
+  else {
+    // TODO: calculate price for external participants
+    return weekdayNights*pricePerWeekdayNight + weekendNights*pricePerWeekendNight;
+  }
+  */
+
   return NaN;
 }
