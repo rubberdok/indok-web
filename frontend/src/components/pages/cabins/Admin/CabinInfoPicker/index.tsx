ded3736595befb8a16dc04505d5936f4cda8b540
MagnusHafstad <114174209+MagnusHafstad@users.noreply.github.com>
2024-01-15 17:07:38 +0100

    Linting

--- frontend/src/components/pages/cabins/Admin/CabinInfoPicker/index.tsx
+++ frontend/src/components/pages/cabins/Admin/CabinInfoPicker/index.tsx
@@ -2,10 +2,10 @@ import { useMutation, useQuery } from "@apollo/client";
 import { Alert, AlertTitle, Snackbar } from "@mui/material";
 import { useState } from "react";
 
-import { CabinInfoForm } from "./CabinInfoForm";
-
 import { Link } from "@/components";
 import { CabinsDocument, UpdateCabinDocument, UpdateCabinInput } from "@/generated/graphql";
+
+import { CabinInfoForm } from "./CabinInfoForm";
 
 function useCabins(): CabinInfoForm | undefined {
   const { data } = useQuery(CabinsDocument);
