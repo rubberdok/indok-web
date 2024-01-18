f6b63b9e121402c9fdaec1edc3f7c8405e7fa962
Fleli <frederikedvardsen@gmail.com>
2024-01-18 16:25:37 +0100

    Added blog page

--- frontend/src/app/blog/[id]/page.tsx
+++ frontend/src/app/blog/[id]/page.tsx
@@ -1,28 +1,40 @@
-import { Grid } from "@mui/material"
+
+import { Box, Card, Container, Grid, Typography } from "@mui/material"
 
 
-function BlogPage({ params }: { params: { id: number} }) {
+function BlogPage({ params }: { params: { id: number } }) {
 
+    const title = "Tittel"
     const content = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, corporis repudiandae sunt doloremque tenetur distinctio voluptatum nemo quis. Commodi, eligendi officiis repudiandae optio veniam reprehenderit culpa quos quidem rerum saepe."
     const date = "6969-69-69"
     const author = "Stephen Hawking"
-    const img = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.nationalgeographic.com%2Fanimals%2Fmammals%2Ffacts%2Fdomestic-cat&psig=AOvVaw1K9tP4tlgA8Rw30SRwIqGK&ust=1705419919852000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKiN5rXe34MDFQAAAAAdAAAAABAI"
+    const img = "https://i.natgeofe.com/n/548467d8-c5f1-4551-9f58-6817a8d2c45e/NationalGeographic_2572187_square.jpg"
 
     const id = params.id
 
-    return (<div>
+    return (<Container sx={{ width: "900px", maxWidth: "90vw" }}>
 
         <Grid container direction={"row"}>
-
-            <p>
+            
+            <Grid display="flex" justifyContent={"center"} sx={{width: "100%"}}>
+                <Box sx={{ width: "70%", borderRadius: "8px" }}>
+                <Typography variant="h2" textAlign={"center"}>
+                    {title}
+                </Typography>
+                </Box>
+            </Grid>
+            
+            <Grid display="flex" sx={{ alignItems: "center", justifyContent: "center" }}>
+                <Box component={"img"} alt={"T"} src={img} sx={{ width: "70%", borderRadius: "8px" }} />
+            </Grid>
+            
+            <Grid>
                 {content}
-
-                {content}
-            </p>
+            </Grid>
 
         </Grid>
 
-    </div>)
+    </Container>)
 
 }
 
