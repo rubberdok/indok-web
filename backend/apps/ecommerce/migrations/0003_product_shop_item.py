11558c9eccc59ced968abf7e501ca6bbd913e181
Hannah Köberle <hannah.koeberleger@gmail.com>
2024-02-19 18:36:21 +0100

    attempt format fix

--- backend/apps/ecommerce/migrations/0003_product_shop_item.py
+++ backend/apps/ecommerce/migrations/0003_product_shop_item.py
@@ -4,15 +4,14 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('ecommerce', '0002_auto_20220120_1911'),
+        ("ecommerce", "0002_auto_20220120_1911"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='product',
-            name='shop_item',
+            model_name="product",
+            name="shop_item",
             field=models.BooleanField(default=False),
         ),
     ]
