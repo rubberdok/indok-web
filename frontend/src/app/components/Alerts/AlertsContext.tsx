d099a9c790d35243a7a271d6751fc620d3216d09
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-02-19 14:16:26 +0100

    chore: add page-wide alerts

---
+++ frontend/src/app/components/Alerts/AlertsContext.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { Alert, AlertTitle, Snackbar } from "@mui/material";
+import { createContext, useContext, useState } from "react";
+
+type Alert = {
+  message?: string;
+  type: "error" | "info" | "success";
+  title?: string;
+};
+
+const AlertsContext = createContext<{
+  alerts: Alert[];
+  notify: (alert: Alert) => void;
+}>({ alerts: [], notify: () => {} });
+
+function AlertsWrapper({ children }: React.PropsWithChildren) {
+  const [alerts, setAlerts] = useState<(Alert & { timestamp: number })[]>([]);
+  let currentAlert: (Alert & { timestamp: number }) | undefined = alerts[0];
+
+  const notify = (alert: Alert) => {
+    setAlerts((previousAlerts) => {
+      const newAlerts = previousAlerts.concat({
+        ...alert,
+        timestamp: Date.now(),
+      });
+      return newAlerts;
+    });
+  };
+
+  function removeAlert() {
+    setAlerts((previousAlerts) => {
+      return previousAlerts.slice(1);
+    });
+  }
+
+  return (
+    <AlertsContext.Provider value={{ alerts, notify }}>
+      {children}
+      <Snackbar
+        autoHideDuration={10_000}
+        key={currentAlert?.timestamp}
+        open={Boolean(currentAlert)}
+        onClose={() => removeAlert()}
+      >
+        <Alert severity={currentAlert?.type} onClose={() => removeAlert()}>
+          {currentAlert?.title && <AlertTitle>{currentAlert.title}</AlertTitle>}
+          {currentAlert?.message}
+        </Alert>
+      </Snackbar>
+    </AlertsContext.Provider>
+  );
+}
+
+function useAlerts(): { alerts: Alert[]; notify: (alert: Alert) => void } {
+  return useContext(AlertsContext);
+}
+
+export { AlertsWrapper, useAlerts };
---
+++ frontend/src/app/components/Alerts/index.tsx
@@ -0,0 +1,1 @@
+export { AlertsWrapper, useAlerts } from "./AlertsContext";
