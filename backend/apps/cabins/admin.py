39c4b5a9fd50992ee422136546574fde5f63aa70
AndreasTauge <andreas@tauge.no>
2024-02-22 18:18:11 +0100

    feat(backend/apps/cabins): add car admin site

--- backend/apps/cabins/admin.py
+++ backend/apps/cabins/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from apps.cabins.models import Cabin, Booking, BookingResponsible, BookingSemester
+from apps.cabins.models import Cabin, Booking, BookingResponsible, BookingSemester, Car
 
 # Register your models here.
 
 admin.site.register(Cabin)
+admin.site.register(Car)
 admin.site.register(Booking)
 admin.site.register(BookingResponsible)
 admin.site.register(BookingSemester)
