9f01973e497d2742adf89af497df383189985500
MagnusHafstad <114174209+MagnusHafstad@users.noreply.github.com>
2024-02-26 19:36:57 +0100

    Feat: visual for shop

--- frontend/src/components/pages/organization/OrgShop.tsx
+++ frontend/src/components/pages/organization/OrgShop.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import { Grid, Stack } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 
 import { ShopSale } from "../orgs/ShopSale";
 
@@ -25,24 +25,46 @@ export const OrgProducts: React.FC<Props> = ({ organization }) => {
     return <p>Ingen ordre</p>;
   }
   return (
-    <Grid container spacing={0}>
-      {data?.userOrders?.map((order) => {
-        if (order.product.shopItem === true) {
-          return (
-            <Grid key={order.id} item xs={12} sm={12} md={12}>
-              <Stack border={1}>
-                <ShopSale
-                  name={order.user.firstName + " " + order.user.lastName}
-                  product_name={order.product.name}
-                  quantity={order.quantity}
-                  has_paid={order.paymentStatus === "CAPTURED"}
-                  is_delivered={order.delivered}
-                />
-              </Stack>
-            </Grid>
-          );
-        }
-      })}
-    </Grid>
+    <>
+      <Stack direction={"row"} padding={2} border={3}>
+        <Box display="flex" alignItems="left" justifyContent="left" width={"15%"} padding={1}>
+          <Typography variant="body1">Navn på kunde</Typography>
+        </Box>
+        <Box display="flex" alignItems="left" justifyContent="left" width={"15%"} padding={1}>
+          <Typography variant="body1">Kjøpt produkt</Typography>
+        </Box>
+        <Box display="flex" alignItems="left" justifyContent="left" width={"15%"} padding={1}>
+          <Typography variant="body1">Antall bestilt</Typography>
+        </Box>
+        <Box display="flex" alignItems="left" justifyContent="left" width={"15%"} padding={1}>
+          <Typography variant="body1"> Betalt status</Typography>
+        </Box>
+        <Box display="flex" alignItems="left" justifyContent="left" width={"25%"} padding={1}>
+          Mulige handlinger
+        </Box>
+        <Box display="flex" alignItems="left" justifyContent="left" width={"15%"} padding={1}>
+          <Typography variant="body1"> Har vi levert varen</Typography>
+        </Box>
+      </Stack>
+      <Grid container spacing={0}>
+        {data?.userOrders?.map((order) => {
+          if (order.product.shopItem === true) {
+            return (
+              <Grid key={order.id} item xs={12} sm={12} md={12}>
+                <Stack border={1}>
+                  <ShopSale
+                    name={order.user.firstName + " " + order.user.lastName}
+                    product_name={order.product.name}
+                    quantity={order.quantity}
+                    has_paid={order.paymentStatus === "CAPTURED"}
+                    is_delivered={order.delivered}
+                  />
+                </Stack>
+              </Grid>
+            );
+          }
+        })}
+      </Grid>
+    </>
   );
 };
--- frontend/src/components/pages/orgs/ShopSale.tsx
+++ frontend/src/components/pages/orgs/ShopSale.tsx
@@ -1,4 +1,6 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Check, Clear } from "@mui/icons-material";
+import { Box, IconButton, Stack, Tooltip, Typography } from "@mui/material";
+import { useState } from "react";
 
 type Props = {
   name: string;
@@ -9,22 +11,42 @@ type Props = {
 };
 
 export const ShopSale: React.FC<Props> = ({ name, product_name, quantity, has_paid, is_delivered }) => {
+  const [delivered, setdelivered] = useState(is_delivered);
+
   return (
-    <Stack direction={"row"} padding={2}>
-      <Box width={"20%"} padding={1}>
+    <Stack direction={"row"} padding={2} spacing={1}>
+      <Box display="flex" alignItems="left" justifyContent="left" width={"15%"} padding={1}>
         <Typography variant="body1">{name}</Typography>
       </Box>
-      <Box width={"20%"} padding={1}>
+      <Box display="flex" alignItems="left" justifyContent="left" width={"15%"} padding={1}>
         <Typography variant="body1">{product_name}</Typography>
       </Box>
-      <Box width={"20%"} padding={1}>
+      <Box display="flex" alignItems="left" justifyContent="left" width={"15%"} padding={1}>
         <Typography variant="body1">Antall: {quantity}</Typography>
       </Box>
-      <Box width={"20%"} padding={1}>
+      <Box display="flex" alignItems="left" justifyContent="left" width={"15%"} padding={1}>
         <Typography variant="body1">Betalt: {has_paid ? "Ja" : "Nei"}</Typography>
       </Box>
-      <Box width={"20%"} padding={1}>
-        <Typography variant="body1">Levert: {is_delivered ? "Ja" : "Nei"}</Typography>
+      <Box display="flex" alignItems="left" justifyContent="left" width={"25%"} padding={1}>
+        <Stack direction={"row"} spacing={1}>
+          <Tooltip title="Levert">
+            <Box display="inline" component="span">
+              <IconButton color="success" size="large" disabled={delivered} onClick={() => setdelivered(true)}>
+                <Check />
+              </IconButton>
+            </Box>
+          </Tooltip>
+          <Tooltip title="Angre levert status Levert">
+            <Box display="inline" component="span">
+              <IconButton color="error" size="large" disabled={!delivered} onClick={() => setdelivered(false)}>
+                <Clear />
+              </IconButton>
+            </Box>
+          </Tooltip>
+        </Stack>
+      </Box>
+      <Box display="flex" alignItems="left" justifyContent="left" width={"15%"} padding={1}>
+        <Typography variant="body1">Varen er {delivered ? "levert" : "ikke levert"}</Typography>
       </Box>
     </Stack>
   );
