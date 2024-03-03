ff8330de2944efb7b162ad84e99f0fd106d41323
Lars Waage <46653859+larwaa@users.noreply.github.com>
2024-03-03 17:14:59 +0100

    chore: update metadata on landing page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import { LandingListings, LandingPromo, LandingSection } from "./_components";
 import { LandingHero } from "./_components/LandingHero";
 
 export const metadata: Metadata = {
-  title: "Indøk NTNU - Foreningen for Industriell Økonomi og teknologiledelse",
+  title: "Indøk NTNU | Foreningen for Industriell Økonomi og teknologiledelse",
   description:
     "Janus linjeforening er den øverste instansen for all studentfrivillighet på masterstudiet Industriell Økonomi og Teknologiledelse ved NTNU. Arrangementer, verv, og oversikt over det sosiale på Indøk.",
   openGraph: {
