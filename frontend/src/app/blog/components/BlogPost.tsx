346b27b53b1a766e417ac4730c22c82926e8bd56
Fleli <frederikedvardsen@gmail.com>
2024-03-04 18:34:54 +0100

    Set up BlogPostType and blog grid

--- frontend/src/app/blog/components/BlogPost.tsx
+++ frontend/src/app/blog/components/BlogPost.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { BlogPostType } from "../BlogPost";
+import { Box } from "@mui/material";
+import { Grid } from "@mui/material"
 
-const BlogPost = () => {
-  return <></>;
+const BlogPost = (props: {post: BlogPostType}) => {
+
+  return <Grid item>
+    <Box bgcolor={"blue"} width={"100px"} height={"100px"} margin={"10px"}>
+
+    </Box>
+  </Grid>
+
 };
 
 export default BlogPost;
--- frontend/src/app/blog/page.tsx
+++ frontend/src/app/blog/page.tsx
@@ -1,7 +1,21 @@
 "use client";
 
+import { Grid } from "@mui/material";
+import BlogPost from "./components/BlogPost";
+import { BlogPostType } from "./BlogPost";
+
 const Blog = () => {
-  return <></>;
+
+  const blogPosts: BlogPostType[] = [ {}, {} ];
+
+  return <div>
+
+    <Grid container direction={"column"}>
+      {blogPosts.map( post => <BlogPost post={post} /> )}
+    </Grid>
+
+  </div>;
+
 };
 
 export default Blog;
