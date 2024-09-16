d09e310b0abf4618c7d58a52885e450af11dc0f0
MagnusHafstad <hafstadmagnus@gmail.com>
2024-09-16 18:12:51 +0200

    feat: tictactoe

--- frontend/src/components/pages/ticTacToe/Board.tsx
+++ frontend/src/components/pages/ticTacToe/Board.tsx
@@ -1,26 +1,26 @@
-import { Box, ButtonBase, Stack } from "@mui/material";
-import { useState } from "react";
+import { Box, Stack } from "@mui/material";
+import { useRef, useState } from "react";
 import Square from "./Square";
 
-type Props = {};
+type Props = { squares: string[]; handleMove: (squareIndex: number) => void };
 
-export const Board: React.VFC<Props> = ({}) => {
+export const Board: React.VFC<Props> = ({ squares, handleMove }) => {
   return (
     <Box>
       <Stack direction={"row"}>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} handleMove={() => handleMove(0)} />
+        <Square value={squares[1]} handleMove={() => handleMove(1)} />
+        <Square value={squares[2]} handleMove={() => handleMove(2)} />
       </Stack>
       <Stack direction={"row"}>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} handleMove={() => handleMove(3)} />
+        <Square value={squares[4]} handleMove={() => handleMove(4)} />
+        <Square value={squares[5]} handleMove={() => handleMove(5)} />
       </Stack>
       <Stack direction={"row"}>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} handleMove={() => handleMove(6)} />
+        <Square value={squares[7]} handleMove={() => handleMove(7)} />
+        <Square value={squares[8]} handleMove={() => handleMove(8)} />
       </Stack>
     </Box>
   );
--- frontend/src/pages/ticTacToe/index.tsx
+++ frontend/src/pages/ticTacToe/index.tsx
@@ -1,13 +1,51 @@
 "use client";
 
 import Board from "@/components/pages/ticTacToe/Board";
-import Square from "@/components/pages/ticTacToe/Square";
 import { NextPageWithLayout } from "@/lib/next";
 import { Box, Stack, Typography } from "@mui/material";
-import { useRouter } from "next/router";
+import { useRef, useState } from "react";
 
 const TicTacToe: NextPageWithLayout = () => {
-  const router = useRouter();
+  const [history, setHistory] = useState<string[][]>([Array(9).fill("")]);
+  const [turn, setTurn] = useState<string>("X");
+  const [winner, setWinner] = useState<string>("");
+  const [step, setStep] = useState<number>(0);
+  const moveNumber = useRef(0);
+
+  function handleMove(squareIndex: number) {
+    if (history[history.length - 1][squareIndex] === "" && moveNumber.current < 9) {
+      const newSquares = history[history.length - 1].slice();
+      newSquares[squareIndex] = turn;
+      setHistory([...history, newSquares]);
+      if (turn === "X") {
+        setTurn("O");
+      } else {
+        setTurn("X");
+      }
+      gameOver(newSquares);
+      setStep(step + 1);
+      moveNumber.current += 1;
+    }
+  }
+
+  function gameOver(squares: string[]) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] !== "" && squares[a] === squares[b] && squares[a] === squares[c]) {
+        setWinner(squares[a]);
+      }
+    }
+  }
 
   return (
     <Stack direction={"column"} alignItems={"center"}>
@@ -15,9 +53,22 @@ const TicTacToe: NextPageWithLayout = () => {
       <Typography variant="h1" align="center">
         Tic Tac Toe
       </Typography>
-      <Board />
+      <Typography variant="h3" align="center">
+        {generateGameOverString()}
+      </Typography>
+      <Board squares={history[history.length - 1]} handleMove={handleMove} />
     </Stack>
   );
+
+  function generateGameOverString() {
+    if (winner !== "") {
+      return "The winner is " + winner;
+    } else if (moveNumber.current === 9) {
+      return "It's a Draw!";
+    } else {
+      return "";
+    }
+  }
 };
 
 export default TicTacToe;
